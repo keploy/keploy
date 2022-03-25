99e745604a57b8ea32e469c2af5f35a2887121cb
Sarthak160 <rocksarthak45@gmail.com>
2022-03-25 20:25:14 +0530

    test(mongo): increase context timeout to fix failing tests

--- pkg/platform/mgo/mongo.go
+++ pkg/platform/mgo/mongo.go
@@ -9,7 +9,7 @@ import (
 
 func New(uri string) (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(uri)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 65*time.Second)
 	// defer cancel()
 	return mongo.Connect(ctx, clientOptions)
 }
