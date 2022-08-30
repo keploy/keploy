56fc039d15729b618227de8b53404cc19fd7bb44
re-Tick <jain.ritik.1001@gmail.com>
2022-08-30 10:16:54 +0530

    refactor(logger): adds env config to set logger to debug level
    By default the logger level is set to info level. But using env variable level can be set to debug.
    
    Signed-off-by: re-Tick <jain.ritik.1001@gmail.com>

--- server/server.go
+++ server/server.go
@@ -45,6 +45,7 @@ type config struct {
 	APIKey          string `envconfig:"API_KEY"`
 	EnableDeDup     bool   `envconfig:"ENABLE_DEDUP" default:"false"`
 	EnableTelemetry bool   `envconfig:"ENABLE_TELEMETRY" default:"true"`
+	EnableDebugger  bool   `envconfig:"ENABLE_DEBUG" default:"false"`
 }
 
 func Server() *chi.Mux {
@@ -63,6 +64,10 @@ func Server() *chi.Mux {
 	err = envconfig.Process("keploy", &conf)
 	if err != nil {
 		logger.Error("failed to read/process configuration", zap.Error(err))
+	}
+
+	if conf.EnableDebugger {
+		logConf.Level.SetLevel(zap.DebugLevel)
 	}
 
 	cl, err := mgo.New(conf.MongoURI)
