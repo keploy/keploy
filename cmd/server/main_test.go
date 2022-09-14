2543b0f8f13e95bc8edeb0fb2bbea4b59338c992
re-Tick <jain.ritik.1001@gmail.com>
2022-09-15 00:01:28 +0530

    refactor: removes the debug statement in main_test
    Signed-off-by: re-Tick <jain.ritik.1001@gmail.com>

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/keploy/go-sdk/keploy"
-	"os"
 	"testing"
 )
 
 func TestKeploy(t *testing.T) {
-	os.Setenv("KEPLOY_APP_NAME", "Keploy-Test-App-2")
 	keploy.SetTestMode()
 	go main()
 	keploy.AssertTests(t)
