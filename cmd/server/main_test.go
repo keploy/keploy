b8d3965be75db90c2744f4b574ac1b207721bf62
re-Tick <jain.ritik.1001@gmail.com>
2022-09-14 23:39:15 +0530

    fix: merges the coverage analysis into a txt file
    Signed-off-by: re-Tick <jain.ritik.1001@gmail.com>

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/keploy/go-sdk/keploy"
+	"os"
 	"testing"
 )
 
 func TestKeploy(t *testing.T) {
+	os.Setenv("KEPLOY_APP_NAME", "Keploy-Test-App-2")
 	keploy.SetTestMode()
 	go main()
 	keploy.AssertTests(t)
