8525cd64583025a922aaacaa43425f52a62973dc
Pranshu Srivastava <iampranshu24@gmail.com>
2023-01-12 12:54:52 +0530

    added a default version for starting the server
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,9 +6,8 @@ import (
 
 // version is the version of the server and will be injected during build by ldflags
 // see https://goreleaser.com/customization/build/
-var version = "0.1.0-dev"
 
 func main() {
 	// main method to start Keploy server
-	server.Server(version)
+	server.Server(server.DefaultVersion)
 }
