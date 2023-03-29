c8671acc62f4becff8a84d89caf5f89205ac8efb
re-Tick <jain.ritik.1001@gmail.com>
2023-03-29 11:56:30 +0530

    feat(cli): adds a cli cmd to start the api server
    a cmd is added for starting the api server
    
    Signed-off-by: re-Tick <jain.ritik.1001@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	v "github.com/hashicorp/go-version"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	"go.keploy.io/server/server"
+	v "github.com/hashicorp/go-version"
+	"go.keploy.io/server/config"
+	"go.keploy.io/server/keploycli"
+	"go.keploy.io/server/pkg/service"
+	"go.uber.org/zap"
 )
 
 // version is the version of the server and will be injected during build by ldflags
@@ -18,7 +24,48 @@ func main() {
 	if version == "" {
 		version = getKeployVersion()
 	}
-	server.Server(version)
+
+	conf := config.NewConfig()
+
+	// create a new logger
+	logger := newLogger(conf)
+
+	// default resultPath is current directory from which keploy binary is running
+	if conf.ReportPath == "" {
+		curr, err := os.Getwd()
+		if err != nil {
+			logger.Error("failed to get path of current directory from which keploy binary is running", zap.Error(err))
+		}
+		conf.ReportPath = curr
+	} else if conf.ReportPath[0] != '/' {
+		path, err := filepath.Abs(conf.ReportPath)
+		if err != nil {
+			logger.Error("Failed to get the absolute path from relative conf.path", zap.Error(err))
+		}
+		conf.ReportPath = path
+	}
+	conf.ReportPath += "/test-reports"
+
+	kServices := service.NewServices(version, conf, logger)
+
+	// run the cli
+	keploycli.CLI(version, conf, kServices, logger)
+}
+
+// newLogger returns a new logger at info level
+func newLogger(conf *config.Config) *zap.Logger {
+	logConf := zap.NewDevelopmentConfig()
+	logConf.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	logger, err := logConf.Build()
+	if err != nil {
+		log.Fatalf("failed to initialize logger. error: %v", err)
+	}
+
+	// Set logger on debug level when "ENABLE_DEBUG" env variable is true
+	if conf.EnableDebugger {
+		logConf.Level.SetLevel(zap.DebugLevel)
+	}
+	return logger
 }
 
 func getKeployVersion() string {
