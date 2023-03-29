31227b6a2899f27035d66b5557f732b0fb67bb0b
re-Tick <jain.ritik.1001@gmail.com>
2023-03-29 14:16:04 +0530

    feat(cli): adds simulate method for captured request simulation
    Signed-off-by: re-Tick <jain.ritik.1001@gmail.com>
    
    Former-commit-id: bdd0c365e1cf19613e147912c3a888e98b7bf901

--- keploycli/keploycli.go
+++ keploycli/keploycli.go
@@ -3,6 +3,7 @@ package keploycli
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"go.keploy.io/server/config"
@@ -34,7 +35,72 @@ func CLI(version string, conf *config.Config, kServices *service.KServices, logg
 		},
 	}
 
+	// cmd for running the SDK recorded tests
+	var test = &cobra.Command{
+		// Use:   "test tcsPath mockPath host",
+		Use:   "test",
+		Short: "run the keploy tests",
+	}
+
+	var testExportedTcs = &cobra.Command{
+		Use:   "local tcsPath mockPath host",
+		Short: "run the exported yaml testcases",
+		Args:  cobra.ExactArgs(3),
+		Run: func(cmd *cobra.Command, args []string) {
+			tcsPath := args[0]
+			if tcsPath[0] != '/' {
+				path, err := filepath.Abs(tcsPath)
+				if err != nil {
+					logger.Error("Failed to get the absolute path from relative tcsPath", zap.Error(err))
+				}
+				tcsPath = path
+			}
+			mockPath := args[1]
+			if mockPath[0] != '/' {
+				path, err := filepath.Abs(mockPath)
+				if err != nil {
+					logger.Error("Failed to get the absolute path from relative mockPath", zap.Error(err))
+				}
+				mockPath = path
+			}
+
+			startTest(testInput{
+				ctx:        cmd.Context(),
+				conf:       conf,
+				testExport: conf.EnableTestExport,
+				appId:      "",
+				tcsPath:    tcsPath,
+				mockPath:   mockPath,
+				host:       args[2],
+				kServices:  kServices,
+				logger:     logger,
+			})
+		},
+	}
+
+	var testMongoTcs = &cobra.Command{
+		Use:   "mongo appId host",
+		Short: "run the testcases from MongoDB",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			startTest(testInput{
+				ctx:        cmd.Context(),
+				conf:       conf,
+				testExport: conf.EnableTestExport,
+				appId:      args[0],
+				tcsPath:    "",
+				mockPath:   "",
+				host:       args[1],
+				kServices:  kServices,
+				logger:     logger,
+			})
+		},
+	}
+	test.AddCommand(testExportedTcs)
+	test.AddCommand(testMongoTcs)
+
 	rootCmd.AddCommand(startKeploy)
+	rootCmd.AddCommand(test)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
