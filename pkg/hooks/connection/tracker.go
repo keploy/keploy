703b9c6880b2bce55fa204ed19f9d2105fcd61b0
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2023-07-27 16:59:03 +0530

    fix:add timeout when Connection:keep-alive is enabled in request  (#636)
    * fix:add timeout when Connection:keep-alive is enabled in request to capture test-case
    
    * docs(tracker):added caveat for adding timeout in IsComplete()
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    ---------
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- pkg/hooks/connection/tracker.go
+++ pkg/hooks/connection/tracker.go
@@ -59,6 +59,23 @@ func (conn *Tracker) IsComplete() bool {
 	conn.mutex.RLock()
 	defer conn.mutex.RUnlock()
 	// log.Printf("IsComplete() called: Successfully reading the data...")
+
+	// Get the current timestamp in nanoseconds.
+	currentTimestamp := uint64(time.Now().UnixNano())
+
+	// Calculate the time elapsed since the last activity in nanoseconds.
+	elapsedTime := currentTimestamp - conn.lastActivityTimestamp
+
+	//Caveat: Added a timeout of 2 seconds, after this duration we assume that all the data events would have come.
+	// This will ensure that we capture the requests responses where Connection:keep-alive is enabled.
+
+	// Check if 1 second has passed since the last activity.
+	if elapsedTime >= uint64(time.Second*2) {
+		conn.logger.Debug("Either connection is alive or request is a mutlipart/file-upload")
+		return true
+	}
+
+	// Check if other conditions for completeness are met.
 	return conn.closeTimestamp != 0 &&
 		conn.totalReadBytes == conn.recvBytes &&
 		conn.totalWrittenBytes == conn.sentBytes
