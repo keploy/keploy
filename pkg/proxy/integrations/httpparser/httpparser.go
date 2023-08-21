33b48174beba5afe3854a5221363c90a048db6d2
Pranshu Srivastava <iampranshu24@gmail.com>
2023-08-21 07:48:16 +0000

    fix(http-parser): handled chunked responses in the record mode and gzip compression in test mode
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- pkg/proxy/integrations/httpparser/httpparser.go
+++ pkg/proxy/integrations/httpparser/httpparser.go
@@ -21,8 +21,6 @@ import (
 
 var Emoji = "\U0001F430" + " Keploy:"
 
-type Response struct {
-}
 
 // IsOutgoingHTTP function determines if the outgoing network call is HTTP by comparing the
 // message format with that of an HTTP text message.
@@ -51,6 +49,135 @@ func ProcessOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, h 
 
 }
 
+func contentLengthRequest(finalReq *[]byte, clientConn, destConn net.Conn, logger *zap.Logger, contentLengthHeader string){
+	contentLength, err := strconv.Atoi(contentLengthHeader)
+	if err != nil {
+		logger.Error(Emoji+"failed to get the content-length header", zap.Error(err))
+		return
+	}
+	for contentLength > 0 {
+		clientConn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		requestChunked, err := util.ReadBytes(clientConn)
+		if err != nil {
+			if err == io.EOF {
+				logger.Error(Emoji+"connection closed by the user client", zap.Error(err))
+				break
+			} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				logger.Info(Emoji+"Stopped getting data from the connection", zap.Error(err))
+				break
+			} else {
+				logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
+				return
+			}
+		}
+		*finalReq = append(*finalReq, requestChunked...)
+		contentLength -= len(requestChunked)
+		_, err = destConn.Write(requestChunked)
+		if err != nil {
+			logger.Error(Emoji+"failed to write request message to the destination server", zap.Error(err))
+			return
+		}
+}
+}
+
+func chunkedRequest(finalReq *[]byte, clientConn, destConn net.Conn, logger *zap.Logger, transferEncodingHeader string) {
+	if transferEncodingHeader == "chunked" {
+		for {
+			clientConn.SetReadDeadline(time.Now().Add(5 * time.Second))
+			requestChunked, err := util.ReadBytes(clientConn)
+			if err != nil {
+				if err == io.EOF {
+					logger.Error(Emoji+"connection closed by the user client", zap.Error(err))
+					break
+				} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+					break
+				} else {
+					logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
+					return
+				}
+			}
+			*finalReq = append(*finalReq, requestChunked...)
+			_, err = destConn.Write(requestChunked)
+			if err != nil {
+				logger.Error(Emoji+"failed to write request message to the destination server", zap.Error(err))
+				return
+			}
+			if string(requestChunked) == "0\r\n\r\n" {
+				break
+			}
+		}
+	}
+}
+
+func contentLengthResponse(finalResp *[]byte, clientConn, destConn net.Conn, logger *zap.Logger, contentLengthHeader string) {
+	contentLength, err := strconv.Atoi(contentLengthHeader)
+	if err != nil {
+		logger.Error(Emoji+"failed to get the content-length header", zap.Error(err))
+		return
+	}
+	for contentLength > 0 {
+		//Set deadline of 5 seconds
+		destConn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		resp, err := util.ReadBytes(destConn)
+		if err != nil {
+			//Check if the connection closed.
+			if err == io.EOF {
+				logger.Error(Emoji+"connection closed by the destination server", zap.Error(err))
+				break
+			} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				logger.Info(Emoji+"Stopped getting data from the connection", zap.Error(err))
+				break
+			} else {
+				logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
+				return
+			}
+		}
+		*finalResp = append(*finalResp, resp...)
+		contentLength -= len(resp)
+		// write the response message to the user client
+		_, err = clientConn.Write(resp)
+		if err != nil {
+			logger.Error(Emoji+"failed to write response message to the user client", zap.Error(err))
+			return
+		}
+	}
+}
+
+func chunkedResponse(finalResp *[]byte, clientConn, destConn net.Conn, logger *zap.Logger, transferEncodingHeader string) {
+			//If the transfer-encoding header is chunked
+			if transferEncodingHeader == "chunked" {
+				for {
+					//Set deadline of 5 seconds
+					destConn.SetReadDeadline(time.Now().Add(5 * time.Second))
+					resp, err := util.ReadBytes(destConn)
+					if err != nil {
+						//Check if the connection closed.
+						if err == io.EOF {
+							logger.Error(Emoji+"connection closed by the destination server", zap.Error(err))
+							break
+						} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+							//Check if the deadline is reached.
+							logger.Info(Emoji + "Stopped getting buffer from the destination server")
+							break
+						} else {
+							logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
+							return
+						}
+					}
+					*finalResp = append(*finalResp, resp...)
+					// write the response message to the user client
+					_, err = clientConn.Write(resp)
+					if err != nil {
+						logger.Error(Emoji+"failed to write response message to the user client", zap.Error(err))
+						return
+					}
+					if string(resp) == "0\r\n\r\n" {
+						break
+					}
+				}
+			}
+}
+
 // decodeOutgoingHttp
 func decodeOutgoingHttp(requestBuffer []byte, clienConn, destConn net.Conn, h *hooks.Hook, logger *zap.Logger) {
 	// if len(deps) == 0 {
@@ -67,13 +194,13 @@ func decodeOutgoingHttp(requestBuffer []byte, clienConn, destConn net.Conn, h *h
 	// 	return
 	// }
 	// httpSpec := deps[0]
-	httpSpec := h.FetchDep(0)
-	// fmt.Println("http mock in test: ", httpSpec)
+	stub := h.FetchDep(0)
+	// fmt.Println("http mock in test: ", stub)
 
-	statusLine := fmt.Sprintf("HTTP/%d.%d %d %s\r\n", httpSpec.Spec.HttpReq.ProtoMajor, httpSpec.Spec.HttpReq.ProtoMinor, httpSpec.Spec.HttpResp.StatusCode, http.StatusText(int(httpSpec.Spec.HttpResp.StatusCode)))
+	statusLine := fmt.Sprintf("HTTP/%d.%d %d %s\r\n", stub.Spec.HttpReq.ProtoMajor, stub.Spec.HttpReq.ProtoMinor, stub.Spec.HttpResp.StatusCode, http.StatusText(int(stub.Spec.HttpResp.StatusCode)))
 
 	// Generate the response headers
-	header := pkg.ToHttpHeader(httpSpec.Spec.HttpResp.Header)
+	header := pkg.ToHttpHeader(stub.Spec.HttpResp.Header)
 	var headers string
 	for key, values := range header {
 		for _, value := range values {
@@ -81,22 +208,28 @@ func decodeOutgoingHttp(requestBuffer []byte, clienConn, destConn net.Conn, h *h
 			headers += headerLine
 		}
 	}
+	body := stub.Spec.HttpResp.Body
+	var responseString string
 
-	body := httpSpec.Spec.HttpResp.Body
-	var compressedBuffer bytes.Buffer
-	gw := gzip.NewWriter(&compressedBuffer)
-	_, err := gw.Write([]byte(body))
-	if err != nil {
-		logger.Error(Emoji+"failed to compress the response body", zap.Error(err))
-		return
+	//Check if the gzip encoding is present in the header
+	if header["Content-Encoding"] != nil && header["Content-Encoding"][0] == "gzip" {
+		var compressedBuffer bytes.Buffer
+		gw := gzip.NewWriter(&compressedBuffer)
+		_, err := gw.Write([]byte(body))
+		if err != nil {
+			logger.Error(Emoji+"failed to compress the response body", zap.Error(err))
+			return
+		}
+		err = gw.Close()
+		if err != nil {
+			logger.Error(Emoji+"failed to close the gzip writer", zap.Error(err))
+			return
+		}
+		responseString = statusLine + headers + "\r\n" + compressedBuffer.String()
+	}else{
+		responseString = statusLine + headers + "\r\n" + body
 	}
-	err = gw.Close()
-	if err != nil {
-		logger.Error(Emoji+"failed to close the gzip writer", zap.Error(err))
-		return
-	}
-	responseString := statusLine + headers + "\r\n" + compressedBuffer.String()
-	_, err = clienConn.Write([]byte(responseString))
+	_, err := clienConn.Write([]byte(responseString))
 	if err != nil {
 		logger.Error(Emoji+"failed to write the mock output to the user application", zap.Error(err))
 		return
@@ -109,10 +242,9 @@ func decodeOutgoingHttp(requestBuffer []byte, clienConn, destConn net.Conn, h *h
 // encodeOutgoingHttp function parses the HTTP request and response text messages to capture outgoing network calls as mocks.
 func encodeOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, logger *zap.Logger) *models.Mock {
 	defer destConn.Close()
-
-	var respBuffer []byte
-	var finalRespBuffer []byte
-	var finalRequestBuffer []byte
+	var resp []byte
+	var finalResp []byte
+	var finalReq []byte
 	var err error
 	// write the request message to the actual destination server
 	_, err = destConn.Write(requestBuffer)
@@ -120,7 +252,7 @@ func encodeOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, log
 		logger.Error(Emoji+"failed to write request message to the destination server", zap.Error(err))
 		return nil
 	}
-	finalRequestBuffer = append(finalRequestBuffer, requestBuffer...)
+	finalReq = append(finalReq, requestBuffer...)
 	lines := strings.Split(string(requestBuffer), "\n")
 	var contentLengthHeader string
 	var transferEncodingHeader string
@@ -135,76 +267,26 @@ func encodeOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, log
 	}
 	//Handle chunked requests
 	if contentLengthHeader != "" {
-		contentLength, err := strconv.Atoi(contentLengthHeader)
-		if err != nil {
-			logger.Error(Emoji+"failed to get the content-length header", zap.Error(err))
-			return nil
-		}
-		for contentLength > 0 {
-			clientConn.SetReadDeadline(time.Now().Add(5 * time.Second))
-			requestBufferChunked, err := util.ReadBytes(clientConn)
-			if err != nil {
-				if err == io.EOF {
-					logger.Error(Emoji+"connection closed by the user client", zap.Error(err))
-					break
-				} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-					logger.Info(Emoji+"Stopped getting data from the connection", zap.Error(err))
-					break
-				} else {
-					logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
-					return nil
-				}
-			}
-			finalRequestBuffer = append(finalRequestBuffer, requestBufferChunked...)
-			contentLength -= len(requestBufferChunked)
-			_, err = destConn.Write(requestBufferChunked)
-			if err != nil {
-				logger.Error(Emoji+"failed to write request message to the destination server", zap.Error(err))
-				return nil
-			}
-		}
+		contentLengthRequest(&finalReq, clientConn, destConn, logger, contentLengthHeader)
 	} else if transferEncodingHeader != "" {
-		for {
-			clientConn.SetReadDeadline(time.Now().Add(5 * time.Second))
-			requestBufferChunked, err := util.ReadBytes(clientConn)
-			if err != nil {
-				if err == io.EOF {
-					logger.Error(Emoji+"connection closed by the user client", zap.Error(err))
-					break
-				} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-					break
-				} else {
-					logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
-					return nil
-				}
-			}
-			finalRequestBuffer = append(finalRequestBuffer, requestBufferChunked...)
-			_, err = destConn.Write(requestBufferChunked)
-			if err != nil {
-				logger.Error(Emoji+"failed to write request message to the destination server", zap.Error(err))
-				return nil
-			}
-			if string(requestBufferChunked) == "0\r\n\r\n" {
-				break
-			}
-		}
+		chunkedRequest(&finalReq, clientConn, destConn, logger, transferEncodingHeader)
 	}
 
 	// read the response from the actual server
-	respBuffer, err = util.ReadBytes(destConn)
+	resp, err = util.ReadBytes(destConn)
 	if err != nil {
 		logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
 		return nil
 	}
 	// write the response message to the user client
-	_, err = clientConn.Write(respBuffer)
+	_, err = clientConn.Write(resp)
 	if err != nil {
 		logger.Error(Emoji+"failed to write response message to the user client", zap.Error(err))
 		return nil
 	}
-	finalRespBuffer = append(finalRespBuffer, respBuffer...)
+	finalResp = append(finalResp, resp...)
 	//Getting the content-length or the transfer-encoding header
-	lines = strings.Split(string(respBuffer), "\n")
+	lines = strings.Split(string(resp), "\n")
 	for _, line := range lines {
 		if strings.HasPrefix(line, "Content-Length:") {
 			contentLengthHeader = strings.TrimSpace(strings.TrimPrefix(line, "Content-Length:"))
@@ -215,75 +297,13 @@ func encodeOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, log
 		}
 	}
 	if contentLengthHeader != "" {
-		contentLength, err := strconv.Atoi(contentLengthHeader)
-		if err != nil {
-			logger.Error(Emoji+"failed to get the content-length header", zap.Error(err))
-			return nil
-		}
-		for contentLength > 0 {
-			//Set deadline of 5 seconds
-			destConn.SetReadDeadline(time.Now().Add(5 * time.Second))
-			respBuffer, err = util.ReadBytes(destConn)
-			if err != nil {
-				//Check if the connection closed.
-				if err == io.EOF {
-					logger.Error(Emoji+"connection closed by the destination server", zap.Error(err))
-					break
-				} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-					logger.Info(Emoji+"Stopped getting data from the connection", zap.Error(err))
-					break
-				} else {
-					logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
-					return nil
-				}
-			}
-			finalRespBuffer = append(finalRespBuffer, respBuffer...)
-			contentLength -= len(respBuffer)
-			// write the response message to the user client
-			_, err = clientConn.Write(respBuffer)
-			if err != nil {
-				logger.Error(Emoji+"failed to write response message to the user client", zap.Error(err))
-				return nil
-			}
-		}
+		contentLengthResponse(&finalResp, clientConn, destConn, logger, contentLengthHeader)
 	} else if transferEncodingHeader != "" {
-		//If the transfer-encoding header is chunked
-		if transferEncodingHeader == "chunked" {
-			for {
-				//Set deadline of 5 seconds
-				destConn.SetReadDeadline(time.Now().Add(5 * time.Second))
-				respBuffer, err = util.ReadBytes(destConn)
-				if err != nil {
-					//Check if the connection closed.
-					if err == io.EOF {
-						logger.Error(Emoji+"connection closed by the destination server", zap.Error(err))
-						break
-					} else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-						//Check if the deadline is reached.
-						logger.Info(Emoji + "Stopped getting buffer from the destination server")
-						break
-					} else {
-						logger.Error(Emoji+"failed to read the response message from the destination server", zap.Error(err))
-						return nil
-					}
-				}
-				finalRespBuffer = append(finalRespBuffer, respBuffer...)
-				// write the response message to the user client
-				_, err = clientConn.Write(respBuffer)
-				if err != nil {
-					logger.Error(Emoji+"failed to write response message to the user client", zap.Error(err))
-					return nil
-				}
-				if string(respBuffer) == "0\r\n\r\n" {
-					break
-				}
-			}
-		}
-
+		chunkedResponse(&finalResp, clientConn, destConn, logger, transferEncodingHeader)
 	}
 	var req *http.Request
 	// converts the request message buffer to http request
-	req, err = http.ReadRequest(bufio.NewReader(bytes.NewReader(finalRequestBuffer)))
+	req, err = http.ReadRequest(bufio.NewReader(bytes.NewReader(finalReq)))
 	if err != nil {
 		logger.Error(Emoji+"failed to parse the http request message", zap.Error(err))
 		return nil
@@ -299,21 +319,21 @@ func encodeOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, log
 		}
 	}
 	// converts the response message buffer to http response
-	resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(finalRespBuffer)), req)
+	respParsed, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(finalResp)), req)
 	if err != nil {
 		logger.Error(Emoji+"failed to parse the http response message", zap.Error(err))
 		return nil
 	}
 	var respBody []byte
-	if resp.Body != nil { // Read
-		if resp.Header.Get("Content-Encoding") == "gzip" {
-			resp.Body, err = gzip.NewReader(resp.Body)
+	if respParsed.Body != nil { // Read
+		if respParsed.Header.Get("Content-Encoding") == "gzip" {
+			respParsed.Body, err = gzip.NewReader(respParsed.Body)
 			if err != nil {
 				logger.Error(Emoji+"failed to read the the http response body", zap.Error(err))
 				return nil
 			}
 		}
-		respBody, err = io.ReadAll(resp.Body)
+		respBody, err = io.ReadAll(respParsed.Body)
 		if err != nil {
 			logger.Error(Emoji+"failed to read the the http repsonse body", zap.Error(err))
 			return nil
@@ -372,8 +392,8 @@ func encodeOutgoingHttp(requestBuffer []byte, clientConn, destConn net.Conn, log
 				URLParams:  pkg.UrlParams(req),
 			},
 			HttpResp: &models.HttpResp{
-				StatusCode: resp.StatusCode,
-				Header:     pkg.ToYamlHttpHeader(resp.Header),
+				StatusCode: respParsed.StatusCode,
+				Header:     pkg.ToYamlHttpHeader(respParsed.Header),
 				Body:       string(respBody),
 			},
 			Created: time.Now().Unix(),
