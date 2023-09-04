d0daa37e0db590991173987df2640ce4d8302900
Hermione Dadheech <hermionedadheech@gmail.com>
2023-09-04 19:07:09 +0530

    Adding timedelay
    Signed-off-by: Hermione Dadheech <hermionedadheech@gmail.com>

--- pkg/proxy/integrations/mysqlparser/mysqlparser.go
+++ pkg/proxy/integrations/mysqlparser/mysqlparser.go
@@ -226,7 +226,9 @@ func decodeOutgoingMySQL(clientConnId, destConnId int, requestBuffer []byte, cli
 			responseBinary, err := encodeToBinary(&handshakeResponseFromConfig, opr2, mockResponseRead+1)
 
 			_, err = clientConn.Write(responseBinary)
-
+			if opr2 == "RESULT_SET_PACKET" {
+				time.Sleep(1000 * time.Millisecond)
+			}
 			mockResponseRead++
 		}
 
