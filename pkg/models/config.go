885f097f1eac64a5da61eb6a9e4c13b4dd0adc2a
Akash Kumar <meakash7902@gmail.com>
2023-10-07 16:19:38 +0530

    feat: create a keploy config file
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

---
+++ pkg/models/config.go
@@ -0,0 +1,25 @@
+package models
+
+type Config struct {
+	Record  Record    `json:"record" yaml:"record"`
+	Test    Test      `json:"test" yaml:"test"`
+}
+
+type Record struct {
+	Path string  `json:"path" yaml:"path"`
+	Command string `json:"command" yaml:"command"`
+	ContainerName string `json:"containerName" yaml:"containerName"`
+	NetworkName string `json:"networkName" yaml:"networkName"`
+	Delay uint64 `json:"delay" yaml:"delay"`
+	PassThroughPorts []uint `json:"passThroughPorts" yaml:"passThroughPorts"`
+}
+
+type Test struct {
+	Path string  `json:"path" yaml:"path"`
+	Command string `json:"command" yaml:"command"`
+	ContainerName string `json:"containerName" yaml:"containerName"`
+	NetworkName string `json:"networkName" yaml:"networkName"`
+	Delay uint64 `json:"delay" yaml:"delay"`
+	ApiTimeout uint64 `json:"apiTimeout" yaml:"apiTimeout"`
+	PassThroughPorts []uint `json:"passThroughPorts" yaml:"passThroughPorts"`
+}
