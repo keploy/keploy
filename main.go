920a302743c43469e79b5efde340e70875bb5195
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2023-10-12 16:24:27 +0530

    feat: Added support for sentry and improved the logging (#935)
    * feat: added sentry for telemetry
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * feat: added all events for telemetry
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * feat: added all telemetry events except mocktest
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * style: merged main
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * feat: almost completed the integration
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * feat: added cmd field in the events and updated the README for the docker command
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: removed unnecessary changes
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: added tests and records in one ping
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * feat: added env variables for docker and sentry
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * feat: added support for arch
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: checking for the value of Dsn
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: update goreleaser
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: update approach
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: addressed all the comments on the PR
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: fixed goreleaser and docker build issues
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: fix dsn
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: updated sentry logging mechanism
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: made changes according to the finalisation
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: added if checks before sending events and changed the logger
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: added additional mount directory for docker
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    ---------
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"fmt"
 	_ "net/http/pprof"
+	"os"
+	"time"
 
+	sentry "github.com/getsentry/sentry-go"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/cloudflare/cfssl/log"
 	v "github.com/hashicorp/go-version"
 	"go.keploy.io/server/cmd"
+	"go.keploy.io/server/utils"
 )
 
 // version is the version of the server and will be injected during build by ldflags
 // see https://goreleaser.com/customization/build/
 
 var version string
+var Dsn string
 
 const logo string = `
        ▓██▓▄
@@ -38,7 +44,6 @@ func getKeployVersion() string {
 	if err != nil {
 		return "v0.1.0-dev"
 	}
-
 	var latestTag string
 	var latestTagVersion *v.Version
 
@@ -61,12 +66,27 @@ func getKeployVersion() string {
 	return latestTag + "-dev"
 }
 
+
 func main() {
 	if version == "" {
 		version = getKeployVersion()
 	}
-
 	fmt.Println(logo, " ")
 	fmt.Printf("%v\n\n", version)
+	isDocker := os.Getenv("IS_DOCKER_CMD")
+	if isDocker != "" {
+		Dsn = os.Getenv("Dsn")
+	}
+	//Initialise sentry.
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              Dsn,
+		TracesSampleRate: 1.0,
+	})
+	log.Level = 0
+	if err != nil {
+		log.Debug("Could not initialise sentry.", err)
+	}
+	defer utils.HandlePanic()
+	defer sentry.Flush(2 * time.Second)
 	cmd.Execute()
 }
