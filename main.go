02ddbc1b47aeef09e29909a65eff94409f7a20dd
Pranshu Srivastava <iampranshu24@gmail.com>
2023-10-16 20:38:31 +0000

    fix: checking
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	v "github.com/hashicorp/go-version"
 	"go.keploy.io/server/cmd"
 	"go.keploy.io/server/utils"
+	"sort"
 )
 
 // version is the version of the server and will be injected during build by ldflags
@@ -34,37 +35,44 @@ const logo string = `
 `
 
 func getKeployVersion() string {
-
 	repo, err := git.PlainOpen(".")
 	if err != nil {
+		fmt.Println("Error opening the repo:", err)
 		return "v0.1.0-dev"
 	}
 
 	tagIter, err := repo.Tags()
 	if err != nil {
+		fmt.Println("Error getting the tags:", err)
 		return "v0.1.0-dev"
 	}
-	var latestTag string
-	var latestTagVersion *v.Version
+
+	var versions v.Collection
 
 	err = tagIter.ForEach(func(tagRef *plumbing.Reference) error {
 		tagName := tagRef.Name().Short()
 		tagVersion, err := v.NewVersion(tagName)
-		fmt.Printf("This is the name%s and this is the tag version %s\n", tagName, tagVersion)
+		fmt.Println("This is the tag name and this is the tag version:", tagName, tagVersion)
 		if err == nil {
-			if latestTagVersion == nil || latestTagVersion.LessThan(tagVersion) {
-				latestTagVersion = tagVersion
-				latestTag = tagName
-			}
+			versions = append(versions, tagVersion)
+		} else {
+			fmt.Printf("Error parsing version from tag %s: %v\n", tagName, err)
 		}
 		return nil
 	})
 
 	if err != nil {
+		fmt.Println("Error iterating through the tags:", err)
 		return "v0.1.0-dev"
 	}
 
-	return latestTag + "-dev"
+	if len(versions) == 0 {
+		return "v0.1.0-dev"
+	}
+
+	sort.Sort(versions)
+	latestVersion := versions[len(versions)-1]
+	return latestVersion.String() + "-dev"
 }
 
 
