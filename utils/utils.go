b48190732c731ee93d1c4e127d37940d402e6c11
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2023-10-31 13:12:18 +0530

    fix: add version for sentry (#1050)
    * fix: removed unnecessary log
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: testing version problem change
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: updated path of keploy logs and added function to delete them
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: testing sentry
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: checking
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    * fix: made changes according to the comments
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>
    
    ---------
    
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -10,6 +10,8 @@ import (
 	sentry "github.com/getsentry/sentry-go"
 )
 
+var KeployVersion string
+
 func attachLogFileToSentry(logFilePath string) {
 	file, err := os.Open(logFilePath)
 	if err != nil {
@@ -23,13 +25,12 @@ func attachLogFileToSentry(logFilePath string) {
 	sentry.ConfigureScope(func(scope *sentry.Scope) {
 		scope.SetExtra("logfile", string(content))
 	})
-	fmt.Println("Sent the event to sentry.")
 	sentry.Flush(time.Second * 5)
 }
 
 func HandlePanic() {
 	if r := recover(); r != nil {
-		attachLogFileToSentry("/tmp/keploy-logs.txt")
+		attachLogFileToSentry("./keploy-logs.txt")
 		sentry.CaptureException(errors.New(fmt.Sprint(r)))
 		sentry.Flush(time.Second * 2)
 	}
