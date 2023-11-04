ba469c1ebffd86a453b42c47ae41bcc349a7196a
shivamsouravjha <shivamsouravjha@gmail.com>
2023-11-04 14:03:22 +0000

    refactor:remove fetch hook command

--- pkg/hooks/loader.go
+++ pkg/hooks/loader.go
@@ -322,9 +322,6 @@ func (h *Hook) findAndCollectChildProcesses(parentPID string, pids *[]int) {
 		}
 	}
 }
-func (h *Hook) FetchHookAppCmd() *exec.Cmd {
-	return h.userAppCmd
-}
 
 // StopUserApplication stops the user application
 func (h *Hook) StopUserApplication() {
