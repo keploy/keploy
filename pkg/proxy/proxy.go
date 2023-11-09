4048abbeb291be5f6de88785e2fe2d91e7009b30
EraKin575 <tejaskumar574@gmail.com>
2023-11-09 21:00:14 +0530

    improved code indentation
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -356,7 +356,7 @@ func BootProxy(logger *zap.Logger, opt Option, appCmd, appContainer string, pid 
 		for i := 1024; i <= 65535 && attemptsDone < maxAttempts; i++ {
 			if isPortAvailable(uint32(i)) {
 				opt.Port = uint32(i)
-				logger.Info(Emoji+"Port %v is already in use, switching to next available port");
+				logger.Info("Port %v is already in use, switching to next available port");
 
 				break
 			}
@@ -364,14 +364,14 @@ func BootProxy(logger *zap.Logger, opt Option, appCmd, appContainer string, pid 
 		}
 	}
 	
-	if maxAttempts<=attemptsDone{
-		logger.Error(Emoji+"Failed to find an available port to start proxy server")
+	if maxAttempts <= attemptsDone{
+		logger.Error("Failed to find an available port to start proxy server")
 		return nil
 	}
 	//IPv4
 	localIp4, err := util.GetLocalIPv4()
 	if err != nil {
-		log.Fatalln(Emoji+"Failed to get the local Ip4 address", err)
+		log.Fatalln("Failed to get the local Ip4 address", err)
 	}
 
 	proxyAddr4, ok := util.ConvertToIPV4(localIp4)
