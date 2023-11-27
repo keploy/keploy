5b82104059c5c196017c19c6f9c02b9745fbc11c
gouravkrosx <gouravgreatkr@gmail.com>
2023-11-27 10:04:20 +0000

    feat: add support to filter some ports from intercepting
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- pkg/service/test/util.go
+++ pkg/service/test/util.go
@@ -50,7 +50,7 @@ type TestConfig struct {
 	AppContainer     string
 	AppNetwork       string
 	Delay            uint64
-	PassThorughPorts []uint
+	PassThroughPorts []uint
 	ApiTimeout       uint64
 }
 
@@ -201,13 +201,13 @@ func LeftJoinNoise(globalNoise models.GlobalNoise, tsNoise models.GlobalNoise) m
 }
 
 func MatchesAnyRegex(str string, regexArray []string) (bool, string) {
-    for _, pattern := range regexArray {
-        re := regexp.MustCompile(pattern)
-        if re.MatchString(str) {
-            return true, pattern
-        }
-    }
-    return false, ""
+	for _, pattern := range regexArray {
+		re := regexp.MustCompile(pattern)
+		if re.MatchString(str) {
+			return true, pattern
+		}
+	}
+	return false, ""
 }
 
 func MapToArray(mp map[string][]string) []string {
