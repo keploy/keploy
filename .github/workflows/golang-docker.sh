780c2834c4cbb1d9bf2b6f9d26c876bd9be6a1ac
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-04 12:00:58 +0000

    fix: checking if the file already exists or not
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/golang-docker.sh
+++ .github/workflows/golang-docker.sh
@@ -10,6 +10,8 @@ sudo docker run --name mongoDb --rm --net keploy-network -p 27017:27017 -d mongo
 
 # Generate the keploy-config file.
 docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 generate-config
+expect "Config file already exists. Do you want to override it? [y/n]:"
+send "y\r"
 
 # Update the global noise to ts.
 config_file="./keploy-config.yaml"
