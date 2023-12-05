b8ddca4b5abc760c90ca2b886e09ee494b022ae6
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-05 08:11:16 +0000

    fix: checking logs
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/python-docker.sh
+++ .github/workflows/python-docker.sh
@@ -3,6 +3,7 @@
 # Start the postgres database.
 docker network create django-postgres-network
 docker run -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d --network django-postgres-network --name mypostgres -v $(pwd)/sql:/docker-entrypoint-initdb.d postgres
+docker logs mypostgres
 
 # Change the database configuration.
 sed -i "s/'HOST': '.*'/'HOST': 'mypostgres'/g" django_postgres/settings.py
