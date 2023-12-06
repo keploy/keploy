aae14aa6b849bfccf4615ff550cbba9fc1cb7f85
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-06 07:42:23 +0000

    fix: add docker tests
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

---
+++ .github/workflows/java-docker.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+
+# Checkout a different branch
+git fetch origin
+git checkout add_keploy
+
+# Start postgres instance.
+docker run -d -e POSTGRES_USER=petclinic -e POSTGRES_PASSWORD=petclinic -e POSTGRES_DB=petclinic -p 5432:5432 postgres:15.2
+
+# Update the java version
+source ./../.github/workflows/update-java.sh
+
+# Remove any existing test and mocks by keploy.
+sudo rm -rf keploy/
+
+# Start keploy in record mode.
+docker network create keploy-network
+docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c './mvnw spring-boot:run'
+
+# Wait for the application to start.
+app_started=false
+while [ "$app_started" = false ]; do
+    if curl -X GET http://localhost:9966/petclinic/api/pettypes; then
+        app_started=true
+    fi
+    sleep 3 # wait for 3 seconds before checking again.
+done
+
+# Get the pid of the application.
+pid=$(pgrep keploy)
+
+# Start making curl calls to record the testcases and mocks.
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+curl --request POST \
+--url http://localhost:9966/petclinic/api/pettypes \
+   --header 'content-type: application/json' \
+   --data '{
+    "name":"John Doe"}'
+
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+curl --request POST \
+--url http://localhost:9966/petclinic/api/pettypes \
+   --header 'content-type: application/json' \
+   --data '{
+    "name":"Alice Green"}'
+
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+ curl --request DELETE \
+--url http://localhost:9966/petclinic/api/pettypes/1
+
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+# Wait for 5 seconds for keploy to record the tcs and mocks.
+sleep 5
+
+# Stop keploy.
+sudo kill $pid
+
+# Start keploy in test mode.
+sudo docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 test -c './mvnw spring-boot:run' --delay 20
+
+# Get the test results from the testReport file.
+report_file="./keploy/testReports/report-1.yaml"
+test_status=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
+
+# Return the exit code according to the status.
+if [ "$test_status" = "PASSED" ]; then
+    exit 0
+else
+    exit 1
+fi
