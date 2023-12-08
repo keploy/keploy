7508736f2794dd797b3ffef4e698cd98ac5fc5c4
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-08 07:29:49 +0000

    fix: checking network
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/python-docker.sh
+++ .github/workflows/python-docker.sh
@@ -3,7 +3,7 @@
 # Start the postgres database.
 docker network create django-postgres-network
 docker run -p 5432:5432 -d -e POSTGRES_PASSWORD=postgres  --network django-postgres-network --name mypostgres -v $(pwd)/sql:/docker-entrypoint-initdb.d postgres
-docker ps
+sudo docker inspect network django-postgres-network
 
 # Remove old keploy tests and mocks.
 sudo rm -rf keploy/
