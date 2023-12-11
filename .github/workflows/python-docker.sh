f7779ca4385f72b63f31a9c30f1f76e7a5087fa0
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-11 15:00:01 +0000

    fix: removing djangoapp
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/python-docker.sh
+++ .github/workflows/python-docker.sh
@@ -44,6 +44,7 @@ sleep 5
 
 # Stop the keploy container and the application container.
 docker rm -f keploy-v2
+docker rm -f DjangoApp
 done
 
 # Checking the testcases and mocks before starting the test.
