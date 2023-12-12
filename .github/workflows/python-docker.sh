75c3945f63936925334ea325d05a9a2a630913a4
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-12 12:14:29 +0000

    fix: updated python workflow
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/python-docker.sh
+++ .github/workflows/python-docker.sh
@@ -1,19 +1,13 @@
 #! /bin/bash
 
 # Start the postgres database.
-echo "checking other apps"
-docker network create django-postgres-network
-docker run -p 5432:5432 -d -e POSTGRES_PASSWORD=postgres  --network django-postgres-network --name mypostgres -v $(pwd)/sql:/docker-entrypoint-initdb.d postgres
+docker network create backend
 
 # Remove old keploy tests and mocks.
 sudo rm -rf keploy/
 
-# Change the database configuration.
-sed -i "s/'HOST': '.*'/'HOST': 'mypostgres'/g" django_postgres/settings.py
-sed -i "s/'PORT': '.*'/'PORT': '5432'/g" django_postgres/settings.py
-
 # Generate the keploy-config file.
-sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host  -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm ghcr.io/keploy/keploy generate-config
+docker run --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 generate-config
 
 # Update the global noise to ignore the Allow header.
 config_file="./keploy-config.yaml"
@@ -23,46 +17,35 @@ sed -i 's/"header": {}/"header":{"Allow":[]}/' "$config_file"
 sleep 5
 
 # Start the django-postgres app in record mode and record testcases and mocks.
-docker build -t django-app:1.0 .
+docker build -t flask-app:1.0 .
 
 for i in {1..2}; do
-sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host  -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm ghcr.io/keploy/keploy record -c "docker run -p 8000:8000 --name DjangoApp --network django-postgres-network django-app:1.0" &
+docker run --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c "docker compose up" --buildDelay 40s  &
 
 # Wait for the application to start.
 app_started=false
 while [ "$app_started" = false ]; do
-    if curl --location 'http://127.0.0.1:8000/'; then
+    if curl http://localhost:6000/students; then
         app_started=true
     fi
     sleep 3 # wait for 3 seconds before checking again.
 done
 
 # Start making curl calls to record the testcases and mocks.
-curl --location 'http://127.0.0.1:8000/user/'
-
-# Wait for 5 seconds for keploy to record the tcs and mocks.
-sleep 5
+curl -X POST -H "Content-Type: application/json" -d '{"student_id": "12345", "name": "John Doe", "age": 20}' http://localhost:6000/students
+curl -X POST -H "Content-Type: application/json" -d '{"student_id": "12346", "name": "Alice Green", "age": 22}' http://localhost:6000/students
+curl http://localhost:6000/students
+curl -X PUT -H "Content-Type: application/json" -d '{"name": "Jane Smith", "age": 21}' http://localhost:6000/students/12345
+curl http://localhost:6000/students
+curl -X DELETE http://localhost:6000/students/12345
 
 # Stop the keploy container and the application container.
 docker rm -f keploy-v2
-docker rm -f DjangoApp
+docker rm -f flask-app
 done
-
-# Checking the testcases and mocks before starting the test.
-echo "now the mocks"
-cat ./keploy/test-set-0/mocks.yaml
-echo "checking one of the tests"
-cat ./keploy/test-set-0/tests/test-1.yaml
 
 # Start the app in test mode.
-sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host  -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm ghcr.io/keploy/keploy test -c "docker run -p 8000:8000 --name DjangoApp --network django-postgres-network django-app:1.0" --delay 20 &
-for i in {1..20}; do
-  # Check port 8000.
-    if curl --location 'http://127.0.0.1:8000/user/'; then
-        break
-    fi
-    sleep 3 # wait for 3 seconds before checking again.
-done
+docker run --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 test -c "docker compose up" --buildDelay 40s --delay 15
 
 # Get the test results from the testReport file.
 report_file="./keploy/testReports/report-1.yaml"
