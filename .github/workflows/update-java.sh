34f97b4e326527a6987e7032b16cf464d3dffe85
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-12 05:55:54 +0000

    fix: update java version
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/update-java.sh
+++ .github/workflows/update-java.sh
@@ -2,5 +2,5 @@
 
 # Update the java version
 sudo apt update
-sudo apt install openjdk-17-jre -y
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+sudo apt install openjdk-22-jre -y
+export JAVA_HOME=/usr/lib/jvm/java-22-openjdk-amd64
