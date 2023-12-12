44660c69954fa1f1d0cfb19cefdeef6ccbcb3f00
Akash Kumar <meakash7902@gmail.com>
2023-12-12 12:55:20 +0530

    check config
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- .github/workflows/golang-linux.sh
+++ .github/workflows/golang-linux.sh
@@ -18,6 +18,7 @@ fi
 # Update the global noise to ts.
 config_file="./keploy-config.yaml"
 sed -i 's/body: {}/body: {"ts":[]}/' "$config_file"
+cat $config_file
 
 # Remove any preexisting keploy tests and mocks.
 sudo rm -rf keploy/
