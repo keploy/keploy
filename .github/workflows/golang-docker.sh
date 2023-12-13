890d04689c12fa4eff4a124046b6ee641733a8a3
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-13 06:59:15 +0000

    fix: check
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/golang-docker.sh
+++ .github/workflows/golang-docker.sh
@@ -20,6 +20,8 @@ docker logs mongoDb &
 
 # Start keploy in record mode.
 sudo docker build -t gin-mongo .
+echo "checking the installation id"
+cat ./../.github/workflows/installation-id.yaml
 for i in {1..2}; do
 docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c 'docker run -p8080:8080 --net keploy-network --rm --name ginApp gin-mongo' &
 
