c871447a9ac6c004c8ddeefce1f96cbf5e63cd44
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-13 09:14:11 +0000

    fix: update again
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/fake-iid.sh
+++ .github/workflows/fake-iid.sh
@@ -1,4 +1,4 @@
 # Add fake installation-id to the config file.
 sudo mkdir ~/.keploy-config
-sudo cat ~/.keploy-config/installation-id.yaml
+sudo touch ~/.keploy-config/installation-id.yaml
 echo "ObjectID('123456789')" > ~/.keploy-config/installation-id.yaml
