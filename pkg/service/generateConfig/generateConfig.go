58e26a147e9ae36dd7092b27d210c7fcacdaae29
Asish Kumar <87874775+officialasishkumar@users.noreply.github.com>
2023-12-14 17:02:40 +0530

    fix: generate config returns after logging error (#1154)
    * fix: generate config returns after logging error

--- pkg/service/generateConfig/generateConfig.go
+++ pkg/service/generateConfig/generateConfig.go
@@ -108,6 +108,7 @@ func (g *generatorConfig) GenerateConfig(filePath string) {
 	err = cmd.Run()
 	if err != nil {
 		g.logger.Error("failed to set the permission of config file", zap.Error(err))
+                return
 	}
 
 	g.logger.Info("Config file generated successfully")
