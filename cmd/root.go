eea91dd23a028e2fd250c264972ac5a4bddfdae4
Shubhrant05 <shubhrantyadav34366@gmail.com>
2023-12-14 23:38:40 +0530

    refactor: keploy record commands

--- cmd/root.go
+++ cmd/root.go
@@ -171,10 +171,10 @@ Use "{{.CommandPath}} [command] --help" for more information about a command.{{e
 
 var rootExamples = `
   Record:
-	keploy record -c "docker run -p 8080:8080 --name myContainerName --network keploy-network <applicationImage>" --containerName "<containerName>" --delay 1 --buildDelay 1m
+	keploy record -c "docker run -p 8080:8080 --name <containerName> --network keploy-network <applicationImage>" --containerName "<containerName>" --delay 1 --buildDelay 1m
 
   Test:
-	keploy test --c "docker run -p 8080:8080 --name myContainerName --network keploy-network <applicationImage>" --delay 1 --buildDelay 1m
+	keploy test --c "docker run -p 8080:8080 --name <containerName> --network keploy-network <applicationImage>" --delay 1 --buildDelay 1m
 
   Generate-Config:
 	keploy generate-config -p "/path/to/localdir"
