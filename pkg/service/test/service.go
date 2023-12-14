f0bec142c71661670d55fed11ea662a2084c0494
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-14 11:52:48 +0000

    fix: fix merge conflicts
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- pkg/service/test/service.go
+++ pkg/service/test/service.go
@@ -11,7 +11,7 @@ import (
 
 type Tester interface {
 	Test(path string, testReportPath string, appCmd string, options TestOptions, disableTele bool) bool
-	RunTestSet(testSet, path, testReportPath, appCmd, appContainer, appNetwork string, delay uint64, buildDelay time.Duration, pid uint32, ys platform.TestCaseDB, loadedHook *hooks.Hook, testReportfs yaml.TestReportFS, testRunChan chan string, apiTimeout uint64, ctx context.Context, testcases map[string]bool, noiseConfig models.GlobalNoise, serveTest bool)models.TestRunStatus
+	RunTestSet(testSet, path, testReportPath, appCmd, appContainer, appNetwork string, delay uint64, buildDelay time.Duration, pid uint32, ys platform.TestCaseDB, loadedHook *hooks.Hook, testReportfs platform.TestReportDB, testRunChan chan string, apiTimeout uint64, ctx context.Context, testcases map[string]bool, noiseConfig models.GlobalNoise, serveTest bool)models.TestRunStatus
 	InitialiseTest(cfg *TestConfig) (InitialiseTestReturn, error)
 	InitialiseRunTestSet(cfg *RunTestSetConfig) InitialiseRunTestSetReturn
 	SimulateRequest(cfg *SimulateRequestConfig)
