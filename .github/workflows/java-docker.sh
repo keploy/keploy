52ed5a7799924af7f5b6fc7cdb0464e5627848ef
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-15 10:05:15 +0000

    fix: changed docker app
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/java-docker.sh
+++ .github/workflows/java-docker.sh
@@ -4,16 +4,16 @@ source ./../.github/workflows/fake-iid.sh
 
 # Start postgres instance.
 docker network create keploy-network
-docker run -d -e POSTGRES_USER=petclinic -e POSTGRES_PASSWORD=petclinic -e POSTGRES_DB=petclinic -p 5432:5432 --name mypostgres --network keploy-network postgres:15.2
+docker compose up -d
 
 # Checkout the add-petclinic branch.
 git fetch origin
-git checkout add-petclinic
+git checkout employeem-docker
 
 # Update the java version
 source ./../.github/workflows/update-java.sh
 mvn --version
-cd ./spring-petclinic/spring-petclinic-rest
+cd ./employee-manager
 
 # Remove any existing test and mocks by keploy.
 sudo rm -rf keploy/
@@ -27,15 +27,15 @@ sudo docker build -t java-app:1.0 .
 pwd
 echo "checking the connections again"
 cat src/main/resources/application-postgresql.properties
-docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c 'docker run -p 9966:9966 --name javaApp --network keploy-network java-app:1.0' --debug  &
+docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c 'docker run -p 8080 --name javaApp --network keploy-network java-app:1.0' --debug  &
 sleep 3
-docker cp ./src/main/resources/db/postgresql/initDB.sql mypostgres:/initDB.sql
-docker exec mypostgres psql -U petclinic -d petclinic -f /initDB.sql
+# docker cp ./src/main/resources/db/postgresql/initDB.sql mypostgres:/initDB.sql
+# docker exec mypostgres psql -U petclinic -d petclinic -f /initDB.sql
 
 # Wait for the application to start.
 app_started=false
 while [ "$app_started" = false ]; do
-    if curl -X GET http://localhost:9966/petclinic/api/pettypes; then
+    if curl -X GET http://localhost:8080/; then
         app_started=true
     fi
     sleep 3 # wait for 3 seconds before checking again.
@@ -45,28 +45,18 @@ done
 pid=$(pgrep keploy)
 
 # Start making curl calls to record the testcases and mocks.
-curl -X GET http://localhost:9966/petclinic/api/pettypes
+curl -X GET http://localhost:8080/
 
-curl --request POST \
---url http://localhost:9966/petclinic/api/pettypes \
-   --header 'content-type: application/json' \
-   --data '{
-    "name":"John Doe"}'
+curl -X GET http://localhost:8080/
+curl -X GET http://localhost:8080/
 
-curl -X GET http://localhost:9966/petclinic/api/pettypes
+curl -X GET http://localhost:8080/
 
-curl --request POST \
---url http://localhost:9966/petclinic/api/pettypes \
-   --header 'content-type: application/json' \
-   --data '{
-    "name":"Alice Green"}'
+curl -X GET http://localhost:8080/
 
-curl -X GET http://localhost:9966/petclinic/api/pettypes
+ curl -X GET http://localhost:8080/
 
- curl --request DELETE \
---url http://localhost:9966/petclinic/api/pettypes/1
-
-curl -X GET http://localhost:9966/petclinic/api/pettypes
+curl -X GET http://localhost:8080/
 
 # Wait for 5 seconds for keploy to record the tcs and mocks.
 sleep 5
