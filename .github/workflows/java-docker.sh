3c6d756cd297a44a8101d17503f3d44c6bd833f4
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-15 10:41:41 +0000

    fix: fix port
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/java-docker.sh
+++ .github/workflows/java-docker.sh
@@ -28,7 +28,7 @@ mvn clean install -Dmaven.test.skip=true
 ls target/
 sudo docker build -t java-app:1.0 .
 docker inspect keploy-network
-docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c 'docker run -p 8080 --name javaApp --network keploy-network java-app:1.0' --debug  &
+docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c 'docker run -p8080:8080 --name javaApp --net keploy-network java-app:1.0'   &
 sleep 3
 # docker cp ./src/main/resources/db/postgresql/initDB.sql mypostgres:/initDB.sql
 # docker exec mypostgres psql -U petclinic -d petclinic -f /initDB.sql
