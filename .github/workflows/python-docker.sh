b6db7623aa1c33af1faa01831cf871872edd772b
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-18 09:48:22 +0000

    fix: checking again
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- .github/workflows/python-docker.sh
+++ .github/workflows/python-docker.sh
@@ -50,7 +50,7 @@ docker rm -f flask-app
 done
 
 # Start the app in test mode.
-docker run --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 test -c "docker compose up" --containerName flask-app --buildDelay 40s --delay 15
+docker run --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 test -c "docker compose up" --containerName flask-app --buildDelay 40s --delay 20
 
 # Get the test results from the testReport file.
 report_file="./keploy/testReports/report-1.yaml"
