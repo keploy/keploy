b7aa0a4ac0f4f90a5e38e8b5e15bdcc0055191ae
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-18 08:02:16 +0000

    fix: updted time
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- pkg/proxy/integrations/postgresParser/postgres_parser.go
+++ pkg/proxy/integrations/postgresParser/postgres_parser.go
@@ -455,7 +455,7 @@ func decodePostgresOutgoing(requestBuffer []byte, clientConn, destConn net.Conn,
 	for {
 		// Since protocol packets have to be parsed for checking stream end,
 		// clientConnection have deadline for read to determine the end of stream.
-		err := clientConn.SetReadDeadline(time.Now().Add(1 * time.Second))
+		err := clientConn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		if err != nil {
 			logger.Error(hooks.Emoji+"failed to set the read deadline for the pg client connection", zap.Error(err))
 			return err
