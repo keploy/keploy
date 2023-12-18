2b3c3e0ad866dc858baaabc6d73a1b6793bc215b
shivamsouravjha <shivamsouravjha@gmail.com>
2023-12-18 10:17:30 +0000

    refactor:testing support for individual testcases

--- .github/workflows/node-linux.sh
+++ .github/workflows/node-linux.sh
@@ -66,7 +66,18 @@ sudo -E env PATH=$PATH ./../../keployv2 test -c 'npm start' --delay 10
 sudo -E env PATH=$PATH ./../../keployv2 serve -c "npm test" --delay 5
 
 sudo -E env PATH=$PATH ./../../keployv2 test -c 'npm start' --delay 10 --testsets test-set-0
-  
+
+sed -i "s/mongoDb:27017/localhost:27017/" "$file_path"
+
+# Generate the keploy-config file.
+./../../keployv2 generate-config
+
+# Update the global noise to ts.
+config_file="./keploy-config.yaml"
+sed -i '/tests:/a \        "test-set-0": ["test-1", "test-2", "test-3"]' "keploy-config.yaml"
+
+sudo -E env PATH=$PATH ./../../keployv2 test -c 'npm start' --delay 10
+
 # Get the test results from the testReport file.
 report_file="./keploy/testReports/report-1.yaml"
 test_status1=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
@@ -78,9 +89,13 @@ report_file4="./keploy/testReports/report-4.yaml"
 test_status4=$(grep 'status:' "$report_file4" | head -n 1 | awk '{print $2}')
 report_file5="./keploy/testReports/report-5.yaml"
 test_status5=$(grep 'status:' "$report_file5" | head -n 1 | awk '{print $2}')
+report_file6="./keploy/testReports/report-6.yaml"
+test_status6=$(grep 'status:' "$report_file6" | head -n 1 | awk '{print $2}')
+report_file7="./keploy/testReports/report-7.yaml"
+test_status7=$(grep 'status:' "$report_file7" | head -n 1 | awk '{print $2}')
 
 # Return the exit code according to the status.
-if [ "$test_status1" = "PASSED" ] && [ "$test_status2" = "PASSED" ] && [ "$test_status3" = "PASSED" ] && [ "$test_status4" = "PASSED" ] && [ "$test_status5" = "PASSED" ]; then
+if [ "$test_status1" = "PASSED" ] && [ "$test_status2" = "PASSED" ] && [ "$test_status3" = "PASSED" ] && [ "$test_status4" = "PASSED" ] && [ "$test_status5" = "PASSED" ] && [ "$test_status6" = "PASSED" ] && [ "$test_status7" = "PASSED" ] ; then
     exit 0
 else
     exit 1
