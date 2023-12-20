e1b5848424f381f9bffe49f9c08ae2795456bcaf
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-20 06:39:57 +0000

    feat: added pipeline test for docker
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

---
+++ .github/workflows/workflow_scripts/golang-docker.sh
@@ -0,0 +1,72 @@
+
+#!/bin/bash
+
+source ./../../.github/workflows/workflow_scripts/fake-iid.sh
+
+# Start mongo before starting keploy.
+docker network create keploy-network
+docker run --name mongoDb --rm --net keploy-network -p 27017:27017 -d mongo
+
+# Generate the keploy-config file.
+docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 generate-config
+
+# Update the global noise to ts.
+config_file="./keploy-config.yaml"
+sed -i 's/body: {}/body: {"ts":[]}/' "$config_file"
+
+# Remove any preexisting keploy tests and mocks.
+rm -rf keploy/
+docker logs mongoDb &
+
+# Start keploy in record mode.
+docker build -t gin-mongo .
+for i in {1..2}; do
+docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 record -c 'docker run -p8080:8080 --net keploy-network --rm --name ginApp gin-mongo' &
+
+# Wait for the application to start.
+app_started=false
+while [ "$app_started" = false ]; do
+    if curl -X GET http://localhost:8080/CJBKJd92; then
+        app_started=true
+    fi
+    sleep 3 # wait for 3 seconds before checking again.
+done
+
+# Start making curl calls to record the testcases and mocks.
+curl --request POST \
+  --url http://localhost:8080/url \
+  --header 'content-type: application/json' \
+  --data '{
+  "url": "https://google.com"
+}'
+
+curl --request POST \
+  --url http://localhost:8080/url \
+  --header 'content-type: application/json' \
+  --data '{
+  "url": "https://facebook.com"
+}'
+
+curl -X GET http://localhost:8080/CJBKJd92
+
+# Wait for 5 seconds for keploy to record the tcs and mocks.
+sleep 5
+
+# Stop keploy.
+docker stop keploy-v2
+docker stop ginApp
+done
+
+# Start the keploy in test mode.
+docker run  --name keploy-v2 -p 16789:16789 --privileged --pid=host -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm keployv2 test -c 'docker run -p8080:8080 --net keploy-network --name ginApp gin-mongo' --delay 10
+
+# Get the test results from the testReport file.
+report_file="./keploy/testReports/report-1.yaml"
+test_status=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
+
+# Return the exit code according to the status.
+if [ "$test_status" = "PASSED" ]; then
+    exit 0
+else
+    exit 1
+fi
---
+++ .github/workflows/workflow_scripts/java-linux.sh
@@ -0,0 +1,84 @@
+#! /bin/bash
+
+source ./../../../.github/workflows/workflow_scripts/fake-iid.sh
+
+# Checkout a different branch
+git fetch origin
+git checkout native-linux
+
+# Start postgres instance.
+docker run -d -e POSTGRES_USER=petclinic -e POSTGRES_PASSWORD=petclinic -e POSTGRES_DB=petclinic -p 5432:5432 postgres:15.2
+
+# Update the java version
+source ./../../../.github/workflows/workflow_scripts/update-java.sh
+
+# Remove any existing test and mocks by keploy.
+rm -rf keploy/
+
+for i in {1..2}; do
+# Start keploy in record mode.
+mvn clean install -Dmaven.test.skip=true
+sudo -E env PATH=$PATH ./../../../keployv2 record -c 'java -jar target/spring-petclinic-rest-3.0.2.jar' &
+
+# Wait for the application to start.
+app_started=false
+while [ "$app_started" = false ]; do
+    if curl -X GET http://localhost:9966/petclinic/api/pettypes; then
+        app_started=true
+    fi
+    sleep 3 # wait for 3 seconds before checking again.
+done
+
+# Get the pid of the application.
+pid=$(pgrep keploy)
+
+# Start making curl calls to record the testcases and mocks.
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+curl --request POST \
+--url http://localhost:9966/petclinic/api/pettypes \
+   --header 'content-type: application/json' \
+   --data '{
+    "name":"John Doe"}'
+
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+curl --request POST \
+--url http://localhost:9966/petclinic/api/pettypes \
+   --header 'content-type: application/json' \
+   --data '{
+    "name":"Alice Green"}'
+
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+ curl --request DELETE \
+--url http://localhost:9966/petclinic/api/pettypes/1
+
+curl -X GET http://localhost:9966/petclinic/api/pettypes
+
+# Wait for 5 seconds for keploy to record the tcs and mocks.
+sleep 5
+
+# Stop keploy.
+sudo kill $pid
+
+# Wait for 5 seconds for keploy to stop.
+sleep 5
+
+done
+
+# Start keploy in test mode.
+sudo -E env PATH=$PATH ./../../../keployv2 test -c 'java -jar target/spring-petclinic-rest-3.0.2.jar' --delay 20
+
+# Get the test results from the testReport file.
+report_file="./keploy/testReports/report-1.yaml"
+test_status1=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
+report_file2="./keploy/testReports/report-2.yaml"
+test_status2=$(grep 'status:' "$report_file2" | head -n 1 | awk '{print $2}')
+
+# Return the exit code according to the status.
+if [ "$test_status1" = "PASSED" ] && [ "$test_status2" = "PASSED" ]; then
+    exit 0
+else
+    exit 1
+fi
