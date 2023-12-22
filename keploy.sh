d47828c2ad985cf71e4727f5178e843c5f4df33a
ananupam <2019024@iiitdmj.ac.in>
2023-12-22 11:37:05 +0000

    refactor: checker for docker volume
    Signed-off-by: ananupam <2019024@iiitdmj.ac.in>

--- keploy.sh
+++ keploy.sh
@@ -72,7 +72,9 @@ installKeploy (){
         fi
 
         if [ "$OS_NAME" = "Darwin" ]; then
-            docker volume create --driver local --opt type=debugfs --opt device=debugfs debugfs
+            if ! docker volume inspect debugfs &>/dev/null; then
+                docker volume create --driver local --opt type=debugfs --opt device=debugfs debugfs
+            fi
             alias keploy='sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v debugfs:/sys/kernel/debug:rw -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v '"$HOME"'/.keploy-config:/root/.keploy-config -v '"$HOME"'/.keploy:/root/.keploy --rm ghcr.io/keploy/keploy'
         else
             alias keploy='sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v '"$HOME"'/.keploy-config:/root/.keploy-config -v '"$HOME"'/.keploy:/root/.keploy --rm ghcr.io/keploy/keploy'
