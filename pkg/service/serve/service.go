b480d68a0637c7f3ea311811da0c3534e9a70184
Charan Kamarapu <kamarapucharan@gmail.com>
2024-01-09 11:56:07 +0530

    Revert " feat: add coverage flag to implement serve within test itself (#1300)" (#1319)
    This reverts commit 0347c1d7d54744c1ed2261df23340fc92aa488d7 [formerly 013e6e266b7b277b2543d02c109dd2b3170c1338].
    
    Co-authored-by: Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
    Former-commit-id: 3b54e8011e34c8e418e96fefadfec47328917453

--- pkg/service/serve/service.go
+++ pkg/service/serve/service.go
@@ -1,10 +1,5 @@
-package graph
+package serve
 
-import (
-	"net/http"
-)
-
-type graphInterface interface {
+type Server interface {
 	Serve(path string, proxyPort uint32, testReportPath string, Delay uint64, pid, port uint32, lang string, passThroughPorts []uint, apiTimeout uint64, appCmd string, enableTele bool)
-    stopGraphqlServer(http *http.Server)
 }
