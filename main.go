21eda4ad720fad7b809ffdf47f1abe1f2f7b48bd
Akash <akashsingh2210670@gmail.com>
2024-01-10 10:17:39 +0530

    Feature: Added Version Check to notify users about new release
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	_ "net/http/pprof"
+	"strings"
 	"time"
 
 	"github.com/cloudflare/cfssl/log"
@@ -36,6 +39,22 @@ func main() {
 	utils.KeployVersion = version
 	fmt.Println(logo, " ")
 	fmt.Printf("version: %v\n\n", version)
+	latestVersion, err2 := getLatestGitHubRelease()
+	if err2 != nil {
+		log.Debug("Failed to fetch latest release version", err2)
+	}
+	const msg string = `
+	   ╭─────────────────────────────────────────────────────╮
+	   │ New version available:                              │		
+	   │ %v  ---->   %v                       │
+	   │ Run 'keploy update' to update to the latest version.│
+	   ╰─────────────────────────────────────────────────────╯
+	`
+	versionmsg := fmt.Sprintf(msg, strings.TrimSpace(version), strings.TrimSpace(latestVersion))
+	if latestVersion != version {
+		fmt.Printf(versionmsg)
+	}
+
 	//Initialise sentry.
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn:              dsn,
@@ -50,4 +69,43 @@ func main() {
 	defer utils.HandlePanic()
 	defer sentry.Flush(2 * time.Second)
 	cmd.Execute()
+}
+
+type GitHubRelease struct {
+	TagName string `json:"tag_name"`
+}
+
+// getLatestGitHubRelease fetches the latest version from GitHub releases.
+
+func getLatestGitHubRelease() (string, error) {
+	// GitHub repository details
+	repoOwner := "keploy"
+	repoName := "keploy"
+
+	// GitHub API URL for latest release
+	apiURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", repoOwner, repoName)
+
+	// Create an HTTP client
+	client := http.Client{}
+
+	// Create a GET request to the GitHub API
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Decode the response JSON
+	var release GitHubRelease
+	if err := json.NewDecoder(resp.Body).Decode(&release); err != nil {
+		return "", err
+	}
+
+	return release.TagName, nil
 }
