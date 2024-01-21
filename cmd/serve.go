1babd3beb3dcd9a16fa065a2e7dc684f60582b49
EraKin575 <tejaskumar574@gmail.com>
2024-01-21 12:41:45 +0530

    updated file structure for test reports
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    Former-commit-id: d0bfeea8b2e782f2a0f10d52564f8bf3615de26c

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"go.keploy.io/server/pkg"
 	"go.keploy.io/server/pkg/service/serve"
 	"go.uber.org/zap"
 )
@@ -56,6 +57,13 @@ func (s *Serve) GetCmd() *cobra.Command {
 			path += "/keploy"
 
 			testReportPath := path + "/testReports"
+			subDirPrefix := "test-report-"
+
+			testReportPath, err = pkg.GetNextTestReportDir(testReportPath, subDirPrefix)
+			if err != nil {
+				s.logger.Error("failed to get the next test report directory", zap.Error(err))
+				return
+			}
 
 			s.logger.Info("", zap.Any("keploy test and mock path", path), zap.Any("keploy testReport path", testReportPath))
 
