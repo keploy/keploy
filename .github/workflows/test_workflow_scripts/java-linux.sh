25b08ffb62616ea4d0329f59521783ae37283939
EraKin575 <tejaskumar574@gmail.com>
2024-01-25 14:31:35 +0530

    fixed workflows and removed serve package
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

--- .github/workflows/test_workflow_scripts/java-linux.sh
+++ .github/workflows/test_workflow_scripts/java-linux.sh
@@ -1,28 +1,17 @@
 #! /bin/bash
 
-source ./../../../.github/workflows/workflow_scripts/fake-iid.sh
-
-# Checkout a different branch
-git fetch origin
-git checkout native-linux
-
 # Start postgres instance.
 docker run -d -e POSTGRES_USER=petclinic -e POSTGRES_PASSWORD=petclinic -e POSTGRES_DB=petclinic -p 5432:5432 postgres:15.2
 
 # Update the java version
-source ./../../../.github/workflows/workflow_scripts/update-java.sh
+source ./../../../.github/workflows/update-java.sh
 
 # Remove any existing test and mocks by keploy.
 sudo rm -rf keploy/
 
-# Update the postgres database.
-docker cp ./src/main/resources/db/postgresql/initDB.sql mypostgres:/initDB.sql
-docker exec mypostgres psql -U petclinic -d petclinic -f /initDB.sql
-
 for i in {1..2}; do
 # Start keploy in record mode.
-mvn clean install -Dmaven.test.skip=true
-sudo -E env PATH=$PATH ./../../../keployv2 record -c 'java -jar target/spring-petclinic-rest-3.0.2.jar' &
+sudo -E env PATH=$PATH ./../../../keployv2 record -c './mvnw spring-boot:run' &
 
 # Wait for the application to start.
 app_started=false
@@ -72,7 +61,7 @@ sleep 5
 done
 
 # Start keploy in test mode.
-sudo -E env PATH=$PATH ./../../../keployv2 test -c 'java -jar target/spring-petclinic-rest-3.0.2.jar' --delay 20
+sudo -E env PATH=$PATH ./../../../keployv2 test -c './mvnw spring-boot:run' --delay 20
 
 # Get the test results from the testReport file.
 report_file="./keploy/testReports/test-run-1/report-1.yaml"
