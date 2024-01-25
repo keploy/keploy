5694c730bd1cde222f43ce719559c72599d1c62a
EraKin575 <tejaskumar574@gmail.com>
2024-01-25 11:59:03 +0530

    updated PR
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

---
+++ pkg/graph/service.go
@@ -0,0 +1,10 @@
+package graph
+
+import (
+	"net/http"
+)
+
+type graphInterface interface {
+	Serve(path string, proxyPort uint32, testReportPath string, Delay uint64, pid, port uint32, lang string, passThroughPorts []uint, apiTimeout uint64, appCmd string, enableTele bool)
+    stopGraphqlServer(http *http.Server)
+}
