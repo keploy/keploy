7df47b047acc4942ae978a89a0be034533b4b26e
Tejas Kumar <100225222+EraKin575@users.noreply.github.com>
2024-01-28 14:51:27 +0530

    refactor: remove redudant workflows and removed serve package (#1432)
    * fixed workflows and removed serve package
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed workflows and removed serve package
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed workflows
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed workflows
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * fixed java workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * changed java workflows
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * changed docker container name in workflows
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * changed docker container name in workflows
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * changed docker container name in workflows
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    * changed python workflow
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    ---------
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    Signed-off-by: Yaxhveer <yaxhcod@gmail.com>

--- .github/workflows/test_workflow_scripts/python-linux.sh
+++ .github/workflows/test_workflow_scripts/python-linux.sh
@@ -23,7 +23,7 @@ python3 manage.py migrate
 ./../../../keployv2 generate-config
 
 #Clean any keploy folders.
-rm -rf keploy/
+sudo rm -rf keploy/
 
 # Update the global noise to ignore the Allow header.
 config_file="./keploy-config.yaml"
@@ -39,7 +39,7 @@ sudo -E env PATH="$PATH" ./../../../keployv2 record -c "python3 manage.py runser
 # Wait for the application to start.
 app_started=false
 while [ "$app_started" = false ]; do
-    if curl --location 'http://127.0.0.1:8000/user'; then
+    if curl --location 'http://127.0.0.1:8000/'; then
         app_started=true
     fi
     sleep 3 # wait for 3 seconds before checking again.
@@ -72,10 +72,10 @@ curl --location 'http://127.0.0.1:8000/user/'
 curl --location 'http://127.0.0.1:8000/user/' \
 --header 'Content-Type: application/json' \
 --data-raw '    {
-        "name": "Alice green",
-        "email": "alice.green@example.com",
-        "password": "alice789",
-        "website": "www.alicegreen.com"
+        "name": "John Doe",
+        "email": "john.doe@example.com",
+        "password": "john567",
+        "website": "www.johndoe.com"
     }'
 
 # Wait for 5 seconds for keploy to record the tcs and mocks.
@@ -89,7 +89,7 @@ sleep 5
 done
 
 # Start the app in test mode.
-sudo -E env PATH="$PATH" ./../../../keployv2 test -c "python3 manage.py runserver" --apiTimeout 30 --delay 20
+sudo -E env PATH="$PATH" ./../../../keployv2 test -c "python3 manage.py runserver" --delay 10
 
 # Get the test results from the testReport file.
 report_file="./keploy/testReports/test-run-1/report-1.yaml"
