c0dca99aa5abfbd250e9355def8a329550ae9f3d
EraKin575 <tejaskumar574@gmail.com>
2024-01-29 19:59:33 +0530

    mysql workflow added
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

--- .github/workflows/test_workflow_scripts/mysql-linux.sh
+++ .github/workflows/test_workflow_scripts/mysql-linux.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-# Source the test-iid.sh script
+# Source the test-iid.sh script for setting up the testing environment
 source ./../../.github/workflows/test_workflow_scripts/test-iid.sh
 
-# Checkout the 'native-linux' branch
+# Checkout the 'native-linux' branch from the git repository
 git fetch origin
 git checkout native-linux
 
-# Start MySQL container
+# Start a MySQL container using Docker
 docker run -p 3306:3306 --rm --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:latest
 
-# Check and remove existing keploy-config file
+# Export the database connection string as an environment variable
+export ConnectionString="root:my-secret-pw@tcp(localhost:3306)/mysql"
+
+# Remove the existing Keploy configuration file if it exists
 if [ -f "./keploy-config.yaml" ]; then
     rm ./keploy-config.yaml
 fi
 
-# Generate the keploy-config file
+# Generate a new Keploy configuration file
 ./../../keployv2 generate-config
 
-# Update the global noise to ts in the keploy-config file
+# Update the Keploy configuration file to include a specific body structure
 config_file="./keploy-config.yaml"
 sed -i 's/body: {}/body: {"ts":[]}/' "$config_file"
 
-# Remove any preexisting keploy tests and mocks
+# Remove any preexisting Keploy tests and mocks
 rm -rf keploy/
 
 # Build the Golang binary
 go build -o main
 
-# Loop for recording test cases and mocks
+# Loop for recording test cases and mocks with Keploy
 for i in {1..2}; do
-    # Start the app in record mode with keploy
+    # Start the application in record mode using Keploy
     sudo -E env PATH="$PATH" ./../../keployv2 record -c "./main" &
 
     # Wait for the application to start
@@ -42,34 +45,33 @@ for i in {1..2}; do
         sleep 3
     done
 
-    # Get the PID of the keploy process
-    pid=$(pgrep keploy)
+    # Get the PID of the Keploy process
+    pid=$(pgrep -f keployv2)
 
-    # Making curl calls to record the test cases and mocks
+    # Make curl calls to record the test cases and mocks
     curl -X POST localhost:8080/create -d '{"link":"https://google.com"}'
     curl -X POST localhost:8080/create -d '{"link":"https://facebook.com"}'
     curl localhost:8080/all
 
-    # Wait for keploy to record the test cases and mocks
+    # Wait for Keploy to record the test cases and mocks
     sleep 5
 
-    # Stop the app
+    # Stop the application and Keploy process
     sudo kill $pid
 
-    # Wait for keploy to stop
+    # Wait for Keploy to stop completely
     sleep 5
 done
 
-# Start the app in test mode
+# Start the application in test mode using Keploy
 sudo -E env PATH="$PATH" ./../../keployv2 test -c "./main" --delay 7
 
-# Get the test results from the test report file
+# Retrieve the test results from the Keploy test report files
 report_file="./keploy/testReports/test-run-1/report-1.yaml"
 test_status1=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
-report_file2="./keploy/testReports/test-run-1/report-1.yaml"
-test_status2=$(grep 'status:' "$report_file2" | head -n 1 | awk '{print $2}')
+test_status2=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
 
-# Exit with code 0 if tests passed, otherwise exit with code 1
+# Exit with code 0 if all tests passed, otherwise exit with code 1
 if [ "$test_status1" = "PASSED" ] && [ "$test_status2" = "PASSED" ]; then
     exit 0
 else
