496bf174d2e096c0c30a1ae2ae0ff0aebe82783a
Akash <akashsingh2210670@gmail.com>
2024-02-05 01:38:22 +0530

    Updated Docker Installation Alias for linux
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- keploy.sh
+++ keploy.sh
@@ -71,20 +71,28 @@ installKeploy (){
     }
 
     install_docker() {
-        if ! docker network ls | grep -q 'keploy-network'; then
-            docker network create keploy-network
-        fi
+    if ! docker network ls | grep -q 'keploy-network'; then
+        docker network create keploy-network
+    fi
 
-        if [ "$OS_NAME" = "Darwin" ]; then
-            if ! docker volume inspect debugfs &>/dev/null; then
-                docker volume create --driver local --opt type=debugfs --opt device=debugfs debugfs
-            fi
+    if [ "$OS_NAME" = "Darwin" ]; then
+        if ! docker volume inspect debugfs &>/dev/null; then
+            docker volume create --driver local --opt type=debugfs --opt device=debugfs debugfs
+        fi
+        if docker ps &>/dev/null; then
+            alias keploy='docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v $(pwd):$(pwd) -w $(pwd) -v /sys/fs/cgroup:/sys/fs/cgroup -v debugfs:/sys/kernel/debug:rw -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v '"$HOME"'/.keploy-config:/root/.keploy-config -v '"$HOME"'/.keploy:/root/.keploy --rm ghcr.io/keploy/keploy'
+        else
             alias keploy='sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v $(pwd):$(pwd) -w $(pwd) -v /sys/fs/cgroup:/sys/fs/cgroup -v debugfs:/sys/kernel/debug:rw -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v '"$HOME"'/.keploy-config:/root/.keploy-config -v '"$HOME"'/.keploy:/root/.keploy --rm ghcr.io/keploy/keploy'
+        fi
+    else
+        if docker ps &>/dev/null; then
+            alias keploy='docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v $(pwd):$(pwd) -w $(pwd) -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v '"$HOME"'/.keploy-config:/root/.keploy-config -v '"$HOME"'/.keploy:/root/.keploy --rm ghcr.io/keploy/keploy'
         else
             alias keploy='sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v $(pwd):$(pwd) -w $(pwd) -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v '"$HOME"'/.keploy-config:/root/.keploy-config -v '"$HOME"'/.keploy:/root/.keploy --rm ghcr.io/keploy/keploy'
         fi
+    fi
+}
 
-    }
 
 
     ARCH=$(uname -m)
