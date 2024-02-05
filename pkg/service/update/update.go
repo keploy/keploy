b84f6a61977f8d37909417f6577858a7a6f2fef4
Akash Singh <114267538+Akash-Singh04@users.noreply.github.com>
2024-02-05 11:01:21 +0530

    Feature: Added Keploy Update Command to CLI (#1325)
    * Feature: Added Keploy Update to CLI
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Added Version Check to notify users about new release
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature:AddedChangelog, Refactoring, Update Version message
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Update Version message
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * requested changes
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * Feature: Added Keploy Update to CLI
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Added Version Check to notify users about new release
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Added Update Command to CLI
    
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    
    * Feature: Added Keploy Update to CLI
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Added Version Check to notify users about new release
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature:AddedChangelog, Refactoring, Update Version message
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Update Version message
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * requested changes
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * Feature: Added Version Check to notify users about new release
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Feature: Added Update Command to CLI
    
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    
    * Updated Review Comments and Formatted Markdown
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * remove comments
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * Resolved comments and added silent to curl command
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * added formatted markdown
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * Added Cache File Functionality To Cache and Store User Input
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Merged with upstream
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Code refactoring and adressing comments
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Refactored to include --version flag
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * implemented binary update logic
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * bug fix
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * Changed file names and refactoring
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Addressed Review Comments and Refactoring
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Refactored Update Message and Added Message for docker users
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Updated Branch With Main
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Updated Branch With Main branch
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Merged with main
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Refactored code to remove update message and addressed comments
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Refactored Code and Addressed comments
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Changed default alias to keploybin
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Refactored error handling
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Added Check for -dev version
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    ---------
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    Co-authored-by: Akash Kumar <91385321+AkashKumar7902@users.noreply.github.com>
    Co-authored-by: Akash Kumar <meakash7902@gmail.com>

---
+++ pkg/service/update/update.go
@@ -0,0 +1,152 @@
+package Update
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/charmbracelet/glamour"
+	"go.keploy.io/server/utils"
+	"go.uber.org/zap"
+)
+
+// NewUpdater initializes a new updater instance.
+func NewUpdater(logger *zap.Logger) Updater {
+	return &updater{
+		logger: logger,
+	}
+}
+
+// updater manages the updating process of Keploy .
+type updater struct {
+	logger *zap.Logger
+}
+
+var ErrGitHubAPIUnresponsive = errors.New("GitHub API is unresponsive")
+
+// Update initiates the update process for the Keploy binary file.
+func (u *updater) Update() {
+	currentVersion := utils.Version
+
+	isDockerCmd := len(os.Getenv("IS_DOCKER_CMD")) > 0
+	if isDockerCmd {
+		u.logger.Info("Please Pull the latest Docker image of Keploy")
+		return
+	}
+	if !strings.HasSuffix(currentVersion, "-dev") {
+		u.logger.Info("You are using a development version of Keploy. Skipping update check.")
+		return
+	}
+
+	releaseInfo, err := utils.GetLatestGitHubRelease()
+	if err != nil {
+		if err == ErrGitHubAPIUnresponsive {
+			u.logger.Error("GitHub API is unresponsive. Update process cannot continue.")
+		} else {
+			u.logger.Error("Failed to fetch latest GitHub release version", zap.Error(err))
+		}
+		return
+	}
+
+	latestVersion := releaseInfo.TagName
+	changelog := releaseInfo.Body
+
+	if currentVersion == latestVersion {
+		u.logger.Info("You are on the latest version of Keploy: " + latestVersion)
+		return
+	}
+
+	u.logger.Info("Updating to Version: " + latestVersion)
+
+	arch := runtime.GOARCH
+	downloadUrl := ""
+	if arch == "amd64" {
+		downloadUrl = "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz"
+	} else {
+		downloadUrl = "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_arm64.tar.gz"
+	}
+	err = u.downloadAndUpdate(downloadUrl)
+	if err != nil {
+		u.logger.Error("Update failed!", zap.Error(err))
+		return
+	}
+
+	u.logger.Info("Update Successful!")
+
+	changelog = "\n" + string(changelog)
+	var renderer *glamour.TermRenderer
+
+	var termRendererOpts []glamour.TermRendererOption
+	termRendererOpts = append(termRendererOpts, glamour.WithAutoStyle(), glamour.WithWordWrap(0))
+
+	renderer, err = glamour.NewTermRenderer(termRendererOpts...)
+	if err != nil {
+		u.logger.Error("Failed to initialize renderer", zap.Error(err))
+		return
+	}
+	changelog, err = renderer.Render(changelog)
+	if err != nil {
+		u.logger.Error("Failed to render release notes", zap.Error(err))
+		return
+	}
+	fmt.Println(changelog)
+}
+
+func (u *updater) downloadAndUpdate(downloadUrl string) error {
+	curlPath, err := exec.LookPath("curl")
+	if err != nil {
+		return errors.New("curl command not found on the system")
+	}
+
+	// Determine the path based on the alias "keploy"
+	aliasPath := "/usr/local/bin/keploybin" // Default path
+	aliasCmd := exec.Command("which", "keploy")
+	aliasOutput, err := aliasCmd.Output()
+	if err == nil && len(aliasOutput) > 0 {
+		aliasPath = strings.TrimSpace(string(aliasOutput))
+	}
+	// Check if the aliasPath is a valid path
+	_, err = os.Stat(aliasPath)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("alias path %s does not exist", aliasPath)
+	}
+
+	// Check if the aliasPath is a directory
+	if fileInfo, err := os.Stat(aliasPath); err == nil && fileInfo.IsDir() {
+		return fmt.Errorf("alias path %s is a directory, not a file", aliasPath)
+
+	}
+
+	downloadCmd := exec.Command(curlPath, "--silent", "--location", downloadUrl)
+	untarCmd := exec.Command("tar", "xz", "-C", "/tmp")
+
+	// Pipe the output of the first command to the second command
+	untarCmd.Stdin, _ = downloadCmd.StdoutPipe()
+
+	if err := downloadCmd.Start(); err != nil {
+		return fmt.Errorf("Failed to start download command: %v", err)
+	}
+	if err := untarCmd.Start(); err != nil {
+		return fmt.Errorf("Failed to start untar command: %v", err)
+	}
+
+	if err := downloadCmd.Wait(); err != nil {
+		return fmt.Errorf("Failed to wait download command: %v", err)
+
+	}
+	if err := untarCmd.Wait(); err != nil {
+		return fmt.Errorf("Failed to wait untar command: %v", err)
+
+	}
+
+	moveCmd := exec.Command("sudo", "mv", "/tmp/keploy", aliasPath)
+	if err := moveCmd.Run(); err != nil {
+		return fmt.Errorf("Failed to move keploy binary to %s: %v", aliasPath, err)
+
+	}
+
+	return nil
+}
