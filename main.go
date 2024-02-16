fa59742997f0ed1d2d92db66666d9316e631da30
Asish Kumar <officialasishkumar@gmail.com>
2024-02-16 18:48:46 +0530

    fix: changed color of keploy logo
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "net/http/pprof"
 	"os"
 	"time"
+	"strings"
 
 	"github.com/cloudflare/cfssl/log"
 	sentry "github.com/getsentry/sentry-go"
@@ -17,6 +18,14 @@ import (
 
 var version string
 var dsn string
+
+var gradientColors = []string{
+	"\033[38;5;202m", // Red
+	"\033[38;5;202m", // Orange-Red
+	"\033[38;5;208m", // Orange
+	"\033[38;5;214m", // Yellow-Orange
+	"\033[38;5;220m", // Yellow
+}
 
 const logo string = `
        ▓██▓▄
@@ -36,7 +45,14 @@ func main() {
 	}
 	utils.Version = version
 	if binaryToDocker := os.Getenv("BINARY_TO_DOCKER"); binaryToDocker != "true" {
-		fmt.Println(logo, " ")
+		const reset = "\033[0m"
+		
+		// Print each line of the logo with a different color from the gradient
+		lines := strings.Split(logo, "\n")
+		for i, line := range lines {
+			color := gradientColors[i%len(gradientColors)]
+			fmt.Println(color, line, reset)
+		}
 		fmt.Printf("version: %v\n\n", version)
 	} else {
 		fmt.Println("Starting keploy in docker environment.")
