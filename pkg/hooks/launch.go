96041c591a798a9c0e0485edf693ef6981996ef7
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2024-02-17 15:44:20 +0900

    use logger Signed-off-by: nnnkkk7 <kurodanaoki0711pana@gmail.com>

--- pkg/hooks/launch.go
+++ pkg/hooks/launch.go
@@ -386,7 +386,7 @@ func (h *Hook) processDockerEnv(appCmd, appContainer, appNetwork string, buildDe
 
 					if containerFound {
 						h.logger.Debug(fmt.Sprintf("the user application container pid: %v", containerPid))
-						inode := getInodeNumber(containerPid)
+						inode := h.getInodeNumber(containerPid)
 						h.logger.Debug("", zap.Any("user inode", inode))
 
 						// send the inode of the container to ebpf hooks to filter the network traffic
@@ -621,13 +621,12 @@ func parseDockerCommand(dockerCmd string) (string, string, error) {
 	return containerName, networkName, nil
 }
 
-func getInodeNumber(pid int) uint64 {
-
+func (h *Hook) getInodeNumber(pid int) uint64 {
 	filepath := filepath.Join("/proc", strconv.Itoa(pid), "ns", "pid")
 
 	f, err := os.Stat(filepath)
 	if err != nil {
-		log.Errorf("%v failed to get the inode number or namespace Id: %w", Emoji, err)
+		h.logger.Error(fmt.Sprintf("%v failed to get the inode number or namespace Id: %v", Emoji, err))
 		return 0
 	}
 	// Dev := (f.Sys().(*syscall.Stat_t)).Dev
