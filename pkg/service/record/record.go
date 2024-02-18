5613b7f0fcb505d5a1a438ce47e98b1a31bbfd14
charankamarapu <kamarapucharan@gmail.com>
2024-02-18 11:12:11 +0000

    chore: add mock-record service
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>
    
    Former-commit-id: 59b222e6800fcc6d42016bd879dfc3994eb1925c

--- pkg/service/record/record.go
+++ pkg/service/record/record.go
@@ -2,6 +2,7 @@ package record
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"go.keploy.io/server/v2/config"
@@ -29,7 +30,7 @@ func NewRecorder(logger *zap.Logger, testDB TestDB, mockDB MockDB, telemetry Tel
 	}
 }
 
-func (r *recorder) record(ctx context.Context) error {
+func (r *recorder) Record(ctx context.Context) error {
 	var runAppError models.AppError
 	var appErrChan = make(chan models.AppError)
 	var incomingFrameChan = make(chan models.Frame)
@@ -67,30 +68,71 @@ func (r *recorder) record(ctx context.Context) error {
 
 	for {
 		select {
-		case err := <-appErrChan:
-			switch err.AppErrorType {
-			case models.ErrKilledByKeploy:
-				r.logger.Info("keploy terminated user application")
+		case appErr := <-appErrChan:
+			switch appErr.AppErrorType {
 			case models.ErrCommandError:
-				r.logger.Error("error in running the user application", zap.Error(err))
+				r.logger.Error("error in running the user application, hence stopping keploy", zap.Error(appErr))
 			case models.ErrUnExpected:
-				r.logger.Warn("user application terminated unexpectedly hence stopping keploy, please check application logs if this behaviour is not expected", zap.Error(err))
+				r.logger.Warn("user application terminated unexpectedly hence stopping keploy, please check application logs if this behaviour is not expected", zap.Error(appErr))
 			default:
-				r.logger.Error("unknown error recieved from application", zap.Error(err))
+				r.logger.Error("unknown error recieved from application, hence stopping keploy", zap.Error(appErr))
 			}
 			hookCancel()
+			incomingCancel()
+			outgoingCancel()
+			return errors.New("failed to execute record due to error in running the user application")
 		case frame := <-incomingFrameChan:
 			r.logger.Info("Incoming frame", zap.Any("frame", frame))
 		case frame := <-outgoingFrameChan:
 			r.logger.Info("Outgoing frame", zap.Any("frame", frame))
 		case err := <-incomingErrChan:
-			r.logger.Error("Error from GetIncoming", zap.Error(err))
+			r.logger.Error("error while fetching incoming frame", zap.Error(err))
 			runAppCancel()
+			hookCancel()
+			outgoingCancel()
+			return errors.New("failed to execute record due to error in fetching incoming frame")
 		case err := <-outgoingErrChan:
-			r.logger.Error("Error from GetOutgoing", zap.Error(err))
+			r.logger.Error("error while fetching outgoing frame", zap.Error(err))
 			runAppCancel()
+			hookCancel()
+			incomingCancel()
+			return errors.New("failed to execute record due to error in fetching outgoing frame")
 		case <-ctx.Done():
 			return nil
 		}
 	}
+}
+
+func (r *recorder) MockRecord(ctx context.Context) error {
+	var outgoingFrameChan = make(chan models.Frame)
+	var outgoingErrChan = make(chan models.OutgoingError)
+
+	hookCtx, hookCancel := context.WithCancel(context.Background())
+	outgoingCtx, outgoingCancel := context.WithCancel(context.Background())
+
+	defer hookCancel()
+	defer outgoingCancel()
+
+	err := r.instrumentation.Hook(hookCtx, models.HookOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to execute mock-record due to error while loading hooks and proxy: %w", err)
+	}
+
+	go func() {
+		outgoingFrameChan, outgoingErrChan = r.instrumentation.GetOutgoing(outgoingCtx, models.OutgoingOptions{})
+	}()
+
+	for {
+		select {
+		case frame := <-outgoingFrameChan:
+			r.logger.Info("Outgoing frame", zap.Any("frame", frame))
+		case err := <-outgoingErrChan:
+			r.logger.Error("error while fetching outgoing frame", zap.Error(err))
+			hookCancel()
+			return errors.New("failed to execute record due to error in fetching outgoing frame")
+		case <-ctx.Done():
+			return nil
+		}
+	}
+
 }
