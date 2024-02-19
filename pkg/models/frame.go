7cb5a75bec1cc855a8a3b6adbb9f79811296c1fc
charankamarapu <kamarapucharan@gmail.com>
2024-02-19 03:34:45 +0000

    chore: add testSetIds
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>
    
    Former-commit-id: 4a9e255851d8abf1e88ee5b8eb908643ebb1e210

--- pkg/models/frame.go
+++ pkg/models/frame.go
@@ -1,4 +1,9 @@
 package models
 
 type Frame struct {
+	Version Version     `json:"version" yaml:"version"`
+	Kind    Kind        `json:"kind" yaml:"kind"`
+	Name    string      `json:"name" yaml:"name"`
+	Spec    interface{} `json:"spec" yaml:"spec"`
+	Curl    string      `json:"curl" yaml:"curl,omitempty"`
 }
