8b2ecaa150984c32e696b329816d56b44ca7c577
Akash Kumar <meakash7902@gmail.com>
2024-02-22 12:45:29 +0530

    address review comments
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- pkg/hooks/loader.go
+++ pkg/hooks/loader.go
@@ -203,8 +203,8 @@ func (h *Hook) UpdateConfigMock(oldMock *models.Mock, newMock *models.Mock) bool
 	isUpdated := h.configMocks.update(oldMock.TestModeInfo, newMock.TestModeInfo, newMock)
 	if isUpdated {
 		h.DeletePersistentMock(oldMock.Name)
+		h.persistentMatchedMocks[newMock.Name] = false
 	}
-	h.persistentMatchedMocks[newMock.Name] = false
 	return isUpdated
 }
 
