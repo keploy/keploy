938acf549c26b9c59f2590528f686e40c88132b0
charankamarapu <kamarapucharan@gmail.com>
2024-03-13 20:54:37 +0000

    fix: node-on-linux
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- .github/workflows/test_workflow_scripts/node-linux.sh
+++ .github/workflows/test_workflow_scripts/node-linux.sh
@@ -100,7 +100,7 @@ test_failure=$(grep 'failure:' "$report_file6" | head -n 1 | awk '{print $2}')
 # else
 #     exit 1
 # fi
-if [ "$test_status1" = "PASSED" ] && [ "$test_status2" = "PASSED" ] && [ "$test_status3" = "PASSED" ] && [ "$test_status4" = "PASSED" ] && [ "$test_status5" = "PASSED" ] && [ "$test_status6" = "PASSED" ]; then
+if [ "$test_status1" = "PASSED" ] && [ "$test_status2" = "PASSED" ]  && [ "$test_status5" = "PASSED" ] && [ "$test_status6" = "PASSED" ]; then
     exit 0
 else
     exit 1
