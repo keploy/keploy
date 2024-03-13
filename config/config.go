3526761135872465e561780d481e7e4b7009e75b
charankamarapu <kamarapucharan@gmail.com>
2024-03-13 19:43:15 +0000

    fix: config
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- config/config.go
+++ config/config.go
@@ -15,8 +15,8 @@ type Config struct {
 	ContainerName   string        `json:"containerName" yaml:"containerName" mapstructure:"containerName"`
 	NetworkName     string        `json:"networkName" yaml:"networkName" mapstructure:"networkName"`
 	BuildDelay      time.Duration `json:"buildDelay" yaml:"buildDelay" mapstructure:"buildDelay"`
-	Test            Test          `json:"test" yaml:"test" mapstructure:",squash"`
-	Record          Record        `json:"record" yaml:"record" mapstructure:",squash"`
+	Test            Test          `json:"test" yaml:"test" mapstructure:"test"`
+	Record          Record        `json:"record" yaml:"record" mapstructure:"record"`
 	ConfigPath      string        `json:"configPath" yaml:"configPath" mapstructure:"configPath"`
 	BypassRules     []BypassRule  `json:"bypassRules" yaml:"bypassRules" mapstructure:"bypassRules"`
 	KeployContainer string        `json:"keployContainer" yaml:"keployContainer" mapstructure:"keployContainer"`
