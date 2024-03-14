18315ba8272e8a774b7d13b2b532257d2511f765
charankamarapu <kamarapucharan@gmail.com>
2024-03-14 08:50:08 +0000

    fix: linter
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- config/config.go
+++ config/config.go
@@ -85,7 +85,7 @@ func SetSelectedTests(conf *Config, testSets []string) {
 	if conf.Test.SelectedTests == nil {
 		conf.Test.SelectedTests = make(map[string][]string)
 	}
-	
+
 	for _, testSet := range testSets {
 		conf.Test.SelectedTests[testSet] = []string{}
 	}
