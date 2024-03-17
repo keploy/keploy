51933950ecff07875455d9dac221d35af3ad54f1
AhmedLotfy02 <ahmadlotfygamersfield@gmail.com>
2024-03-17 17:16:07 +0200

    refactor the print keploy code
    Signed-off-by: AhmedLotfy02 <ahmadlotfygamersfield@gmail.com>

--- cli/root.go
+++ cli/root.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"go.keploy.io/server/v2/cli/provider"
@@ -9,6 +11,18 @@ import (
 	"go.keploy.io/server/v2/utils"
 	"go.uber.org/zap"
 )
+
+const logo string = `
+       ▓██▓▄
+    ▓▓▓▓██▓█▓▄
+     ████████▓▒
+          ▀▓▓███▄      ▄▄   ▄               ▌
+         ▄▌▌▓▓████▄    ██ ▓█▀  ▄▌▀▄  ▓▓▌▄   ▓█  ▄▌▓▓▌▄ ▌▌   ▓
+       ▓█████████▌▓▓   ██▓█▄  ▓█▄▓▓ ▐█▌  ██ ▓█  █▌  ██  █▌ █▓
+      ▓▓▓▓▀▀▀▀▓▓▓▓▓▓▌  ██  █▓  ▓▌▄▄ ▐█▓▄▓█▀ █▓█ ▀█▄▄█▀   █▓█
+       ▓▌                           ▐█▌                   █▌
+        ▓
+`
 
 func Root(ctx context.Context, logger *zap.Logger, svcFactory ServiceFactory, cmdConfigurator CmdConfigurator) *cobra.Command {
 	conf := config.New()
@@ -37,4 +51,50 @@ func Root(ctx context.Context, logger *zap.Logger, svcFactory ServiceFactory, cm
 		rootCmd.AddCommand(c)
 	}
 	return rootCmd
+}
+
+// Print Keploy logo with a gradient color
+func PrintKeployLogo(useColor bool) {
+	// ANSI escape code to reset color
+	const reset = "\033[0m"
+	if useColor {
+		// Print each line of the logo
+		lines := strings.Split(logo, "\n")
+		for i, line := range lines {
+			for j, char := range line {
+				var color string = getLogoColor(i, j)
+
+				// Print each character
+				fmt.Print(color, string(char), reset)
+			}
+			fmt.Println()
+		}
+	} else {
+		fmt.Println(logo)
+	}
+
+}
+
+// Get the color for the logo at position (i, j)
+func getLogoColor(i, j int) string {
+	// Orange to Yellow gradient colors (reversed order)
+	gradientColors := []string{
+		"\033[38;5;202m", // Dark Orange
+		"\033[38;5;208m",
+		"\033[38;5;214m", // Light Orange
+		"\033[38;5;226m", // Light Yellow
+	}
+
+	switch {
+	case i <= 5:
+		return gradientColors[0]
+	case i == 6 && j <= 42:
+		return gradientColors[1]
+	case i == 7 && j <= 49:
+		return gradientColors[2]
+	case j <= 38:
+		return gradientColors[3]
+	default:
+		return gradientColors[0]
+	}
 }
