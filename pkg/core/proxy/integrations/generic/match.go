e0d7a9acbb4fae8d3d7c94a62eff637a2f6f9bfa
charankamarapu <kamarapucharan@gmail.com>
2024-03-28 06:08:45 +0000

    fix: generic matching
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- pkg/core/proxy/integrations/generic/match.go
+++ pkg/core/proxy/integrations/generic/match.go
@@ -34,52 +34,10 @@ func fuzzyMatch(ctx context.Context, reqBuff [][]byte, mockDb integrations.MockM
 				}
 			}
 
-			index := -1
-			for idx, mock := range filteredMocks {
-				if len(mock.Spec.GenericRequests) == len(reqBuff) {
-					matched := true // Flag to track if all requests match
-
-					for requestIndex, reqBuff := range reqBuff {
-
-						bufStr := string(reqBuff)
-						if !util.IsASCIIPrintable(string(reqBuff)) {
-							bufStr = util.EncodeBase64(reqBuff)
-						}
-
-						// Compare the encoded data
-						if mock.Spec.GenericRequests[requestIndex].Message[0].Data != bufStr {
-							matched = false
-							break // Exit the loop if any request doesn't match
-						}
-						if ctx.Err() != nil {
-							return false, nil, ctx.Err()
-						}
-					}
-					if matched {
-						index = idx
-						break
-					}
-					if ctx.Err() != nil {
-						return false, nil, ctx.Err()
-					}
-				}
-			}
-
-			if index != -1 {
-				responseMock := make([]models.GenericPayload, len(filteredMocks[index].Spec.GenericResponses))
-				copy(responseMock, filteredMocks[index].Spec.GenericResponses)
-				originalFilteredMock := *filteredMocks[index]
-				filteredMocks[index].TestModeInfo.IsFiltered = false
-				filteredMocks[index].TestModeInfo.SortOrder = math.MaxInt64
-				isUpdated := mockDb.UpdateUnFilteredMock(&originalFilteredMock, filteredMocks[index])
-				if !isUpdated {
-					continue
-				}
-				return true, responseMock, nil
-			}
+			index := findExactMatch(filteredMocks, reqBuff)
 
 			if index == -1 {
-				index = findBinaryMatch(filteredMocks, reqBuff)
+				index = findBinaryMatch(filteredMocks, reqBuff, 0.9)
 			}
 
 			if index != -1 {
@@ -95,8 +53,10 @@ func fuzzyMatch(ctx context.Context, reqBuff [][]byte, mockDb integrations.MockM
 				return true, responseMock, nil
 			}
 
+			index = findExactMatch(unfilteredMocks, reqBuff)
+
 			if index == -1 {
-				index = findBinaryMatch(unfilteredMocks, reqBuff)
+				index = findBinaryMatch(unfilteredMocks, reqBuff, 0.5)
 			}
 
 			if index != -1 {
@@ -110,9 +70,8 @@ func fuzzyMatch(ctx context.Context, reqBuff [][]byte, mockDb integrations.MockM
 }
 
 // TODO: need to generalize this function for different types of integrations.
-func findBinaryMatch(tcsMocks []*models.Mock, reqBuffs [][]byte) int {
+func findBinaryMatch(tcsMocks []*models.Mock, reqBuffs [][]byte, mxSim float64) int {
 	// TODO: need find a proper similarity index to set a benchmark for matching or need to find another way to do approximate matching
-	mxSim := 0.5
 	mxIdx := -1
 	for idx, mock := range tcsMocks {
 		if len(mock.Spec.GenericRequests) == len(reqBuffs) {
@@ -120,10 +79,7 @@ func findBinaryMatch(tcsMocks []*models.Mock, reqBuffs [][]byte) int {
 				_ = base64.StdEncoding.EncodeToString(reqBuff)
 				encoded, _ := util.DecodeBase64(mock.Spec.GenericRequests[requestIndex].Message[0].Data)
 
-				k := util.AdaptiveK(len(reqBuff), 3, 8, 5)
-				shingles1 := util.CreateShingles(encoded, k)
-				shingles2 := util.CreateShingles(reqBuff, k)
-				similarity := util.JaccardSimilarity(shingles1, shingles2)
+				similarity := fuzzyCheck(encoded, reqBuff)
 
 				if mxSim < similarity {
 					mxSim = similarity
@@ -133,4 +89,39 @@ func findBinaryMatch(tcsMocks []*models.Mock, reqBuffs [][]byte) int {
 		}
 	}
 	return mxIdx
+}
+
+func fuzzyCheck(encoded, reqBuf []byte) float64 {
+	k := util.AdaptiveK(len(reqBuf), 3, 8, 5)
+	shingles1 := util.CreateShingles(encoded, k)
+	shingles2 := util.CreateShingles(reqBuf, k)
+	similarity := util.JaccardSimilarity(shingles1, shingles2)
+	return similarity
+}
+
+func findExactMatch(tcsMocks []*models.Mock, reqBuffs [][]byte) int {
+	for idx, mock := range tcsMocks {
+		if len(mock.Spec.GenericRequests) == len(reqBuffs) {
+			matched := true // Flag to track if all requests match
+
+			for requestIndex, reqBuff := range reqBuffs {
+
+				bufStr := string(reqBuff)
+				if !util.IsASCIIPrintable(string(reqBuff)) {
+					bufStr = util.EncodeBase64(reqBuff)
+				}
+
+				// Compare the encoded data
+				if mock.Spec.GenericRequests[requestIndex].Message[0].Data != bufStr {
+					matched = false
+					break // Exit the loop if any request doesn't match
+				}
+			}
+
+			if matched {
+				return idx
+			}
+		}
+	}
+	return -1
 }
