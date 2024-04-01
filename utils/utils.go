f625961deb8585f9534d93a9b17c863c4c2f4da2
Sky Singh <114267538+Akash-Singh04@users.noreply.github.com>
2024-04-01 23:29:02 +0530

    [feature]: generate GitHub Actions automatically when keploy runs  (#1479)
    * Feat: Automatically Generate Github Actions File on Keploy record
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Addressed review comments and accounted for docker env
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * updated import
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    
    * Removed Depricated package
    
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    
    * Fixed Lint
    
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    
    * Refactored for Keploy v2
    
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    
    * Fixed merge conflicts and fixed keploy folder path
    
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>
    
    ---------
    
    Signed-off-by: Akash <akashsingh2210670@gmail.com>
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -265,6 +265,49 @@ func Recover(logger *zap.Logger) {
 	}
 }
 
+// GenerateGithubActions generates a GitHub Actions workflow file for Keploy
+func GenerateGithubActions(logger *zap.Logger, appCmd string) {
+	// Determine the path based on the alias "keploy"
+	logger.Debug("Generating GitHub Actions workflow file")
+	// Define the content of the GitHub Actions workflow file
+	actionsFileContent := `name: Keploy
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    types: [opened, reopened, synchronize]
+jobs:
+  e2e-test:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: Test-Report
+        uses: keploy/testgpt@main
+        with:
+          working-directory: ./
+          keploy-path: ./
+          command: ` + appCmd + `
+`
+
+	// Define the file path where the GitHub Actions workflow file will be saved
+	filePath := "/githubactions/keploy.yml"
+
+	//create the file path
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		logger.Error("Error creating directory for GitHub Actions workflow file", zap.Error(err))
+		return
+	}
+	// Write the content to the file
+	if err := os.WriteFile(filePath, []byte(actionsFileContent), 0644); err != nil {
+		logger.Error("Error writing GitHub Actions workflow file", zap.Error(err))
+		return
+	}
+
+	logger.Info("GitHub Actions workflow file generated successfully", zap.String("path", filePath))
+}
+
 // GetLatestGitHubRelease fetches the latest version and release body from GitHub releases with a timeout.
 func GetLatestGitHubRelease(ctx context.Context, logger *zap.Logger) (GitHubRelease, error) {
 	// GitHub repository details
