708485c049e675231d9eaa21dc01f79e26a2ad06
dxtym <dilmurod.abdusamadov2004@gmail.com>
2024-05-10 20:59:14 +0500

    fix: move recordtestsuite down
    Signed-off-by: dxtym <dilmurod.abdusamadov2004@gmail.com>

--- pkg/service/record/record.go
+++ pkg/service/record/record.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"sync"
 
 	"time"
 
@@ -67,24 +66,10 @@ func (r *Recorder) Start(ctx context.Context) error {
 	var appID uint64
 	var newTestSetID string
 	var testCount = 0
-	var mutex = &sync.Mutex{}
-	var waitGroup = &sync.WaitGroup{}
 	var mockCountMap = make(map[string]int)
 
 	// defering the stop function to stop keploy in case of any error in record or in case of context cancellation
 	defer func() {
-		select {
-		case <-ctx.Done():
-			waitGroup.Wait()
-			mutex.Lock()
-			r.telemetry.RecordedTestSuite(newTestSetID, testCount, mockCountMap)
-			mutex.Unlock()
-		default:
-			err := utils.Stop(r.logger, stopReason)
-			if err != nil {
-				utils.LogError(r.logger, err, "failed to stop recording")
-			}
-		}
 		runAppCtxCancel()
 		err := runAppErrGrp.Wait()
 		if err != nil {
@@ -98,6 +83,16 @@ func (r *Recorder) Start(ctx context.Context) error {
 		err = errGrp.Wait()
 		if err != nil {
 			utils.LogError(r.logger, err, "failed to stop recording")
+		}
+
+		select {
+		case <-ctx.Done():
+			r.telemetry.RecordedTestSuite(newTestSetID, testCount, mockCountMap)
+		default:
+			err := utils.Stop(r.logger, stopReason)
+			if err != nil {
+				utils.LogError(r.logger, err, "failed to stop recording")
+			}
 		}
 	}()
 
@@ -150,7 +145,6 @@ func (r *Recorder) Start(ctx context.Context) error {
 		return fmt.Errorf(stopReason)
 	}
 
-	waitGroup.Add(1)
 	errGrp.Go(func() error {
 		for testCase := range incomingChan {
 			err := r.testDB.InsertTestCase(ctx, testCase, newTestSetID)
@@ -160,10 +154,7 @@ func (r *Recorder) Start(ctx context.Context) error {
 				}
 				insertTestErrChan <- err
 			} else {
-				mutex.Lock()
 				testCount++
-				mutex.Unlock()
-				waitGroup.Done()
 				r.telemetry.RecordedTestAndMocks()
 			}
 		}
