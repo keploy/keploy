19df68f1f8863bfbe4a43b2f3fe1a46eb6eae353
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2024-05-28 23:54:37 +0530

    refactor: move docker client to platform (#1905)
    * refactor: move dockerClient to platform
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    * fix: make config reference type
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    ---------
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    Former-commit-id: 02533b723e442ce4338314369399b113e481ee75
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -11,33 +11,36 @@ import (
 
 	"go.keploy.io/server/v2/pkg/core/app"
 	"go.keploy.io/server/v2/pkg/models"
+	"go.keploy.io/server/v2/pkg/platform/docker"
 	"go.keploy.io/server/v2/utils"
 	"go.uber.org/zap"
 )
 
 type Core struct {
-	Proxy        // embedding the Proxy interface to transfer the proxy methods to the core object
-	Hooks        // embedding the Hooks interface to transfer the hooks methods to the core object
-	Tester       // embedding the Tester interface to transfer the tester methods to the core object
+	Proxy                      // embedding the Proxy interface to transfer the proxy methods to the core object
+	Hooks                      // embedding the Hooks interface to transfer the hooks methods to the core object
+	Tester                     // embedding the Tester interface to transfer the tester methods to the core object
+	dockerClient docker.Client //embedding the docker client to transfer the docker client methods to the core object
 	logger       *zap.Logger
 	id           utils.AutoInc
 	apps         sync.Map
 	proxyStarted bool
 }
 
-func New(logger *zap.Logger, hook Hooks, proxy Proxy, tester Tester) *Core {
+func New(logger *zap.Logger, hook Hooks, proxy Proxy, tester Tester, client docker.Client) *Core {
 	return &Core{
-		logger: logger,
-		Hooks:  hook,
-		Proxy:  proxy,
-		Tester: tester,
+		logger:       logger,
+		Hooks:        hook,
+		Proxy:        proxy,
+		Tester:       tester,
+		dockerClient: client,
 	}
 }
 
 func (c *Core) Setup(ctx context.Context, cmd string, opts models.SetupOptions) (uint64, error) {
 	// create a new app and store it in the map
 	id := uint64(c.id.Next())
-	a := app.NewApp(c.logger, id, cmd, app.Options{
+	a := app.NewApp(c.logger, id, cmd, c.dockerClient, app.Options{
 		DockerNetwork: opts.DockerNetwork,
 		Container:     opts.Container,
 		DockerDelay:   opts.DockerDelay,
