bcb2d5695862faaa8a4c2a63667724487f427a46
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2024-06-05 18:04:55 +0530

    feat: add support for global passthrough in test mode (#1920)
    * feat: add support for global passthrough in test mode
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    * chore: change flag name and refactor
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    ---------
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    Former-commit-id: 81c0eec203f6612fd138c38b3e81a95c9f1faf5e

--- pkg/core/proxy/options.go
+++ pkg/core/proxy/options.go
@@ -1,5 +1,7 @@
 package proxy
 
+// TODO: what is the need of this? currently it is not being used anywhere.
+
 // Option provides a means to initiate the proxy based on user input.
 type Option struct {
 	Port          uint32
