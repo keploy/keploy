4d4370fff26f62aaae7774727008eb372485c7f2
shivamsouravjha <shivamsouravjha@gmail.com>
2024-06-05 19:05:59 +0530

    chore: mv exit check to ctx done
    Signed-off-by: shivamsouravjha <shivamsouravjha@gmail.com>

--- pkg/core/app/app.go
+++ pkg/core/app/app.go
@@ -490,12 +490,11 @@ func (a *App) run(ctx context.Context) models.AppError {
 
 	err = cmd.Wait()
 
-	if utils.IsDockerKind(a.kind) {
-		a.waitTillExit()
-	}
-
 	select {
 	case <-ctx.Done():
+		if utils.IsDockerKind(a.kind) {
+			a.waitTillExit()
+		}
 		a.logger.Debug("context cancelled, error while waiting for the app to exit", zap.Error(ctx.Err()))
 		return models.AppError{AppErrorType: models.ErrCtxCanceled, Err: ctx.Err()}
 	default:
