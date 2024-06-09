691c25436267a8956daf616b3e8ff890a8d875e4
gouravkrosx <gouravgreatkr@gmail.com>
2024-06-10 02:02:42 +0530

    fix: lint
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- pkg/platform/yaml/configdb/testset/generic.go
+++ pkg/platform/yaml/configdb/testset/generic.go
@@ -13,29 +13,29 @@ import (
 
 // Generic is a generic struct to read and write testset config file
 type Generic[T any] struct {
-	logger *zap.Logger
-	path   string
+	Logger *zap.Logger
+	Path   string
 }
 
 func NewGeneric[T any](logger *zap.Logger, path string) *Generic[T] {
 	return &Generic[T]{
-		logger: logger,
-		path:   path,
+		Logger: logger,
+		Path:   path,
 	}
 }
 
 func (g *Generic[T]) ReadConfig(ctx context.Context, testSetID string) (T, error) {
-	filePath := filepath.Join(g.path, testSetID)
+	filePath := filepath.Join(g.Path, testSetID)
 
 	var config T
-	data, err := yaml.ReadFile(ctx, g.logger, filePath, "config")
+	data, err := yaml.ReadFile(ctx, g.Logger, filePath, "config")
 	if err != nil {
-		utils.LogError(g.logger, err, "failed to read the config from yaml")
+		utils.LogError(g.Logger, err, "failed to read the config from yaml")
 		return config, err
 	}
 
 	if err := yamlLib.Unmarshal(data, &config); err != nil {
-		g.logger.Info("failed to decode the configuration file", zap.Error(err))
+		g.Logger.Info("failed to decode the configuration file", zap.Error(err))
 		return config, err
 	}
 
@@ -43,16 +43,16 @@ func (g *Generic[T]) ReadConfig(ctx context.Context, testSetID string) (T, error
 }
 
 func (g *Generic[T]) WriteConfig(ctx context.Context, testSetID string, config T) error {
-	filePath := filepath.Join(g.path, testSetID)
+	filePath := filepath.Join(g.Path, testSetID)
 
 	data, err := yamlLib.Marshal(config)
 	if err != nil {
-		g.logger.Error("failed to marshal test-set config file", zap.String("testSet", testSetID), zap.Error(err))
+		g.Logger.Error("failed to marshal test-set config file", zap.String("testSet", testSetID), zap.Error(err))
 		return err
 	}
-	err = yaml.WriteFile(ctx, g.logger, filePath, "config", data, false)
+	err = yaml.WriteFile(ctx, g.Logger, filePath, "config", data, false)
 	if err != nil {
-		utils.LogError(g.logger, err, "failed to write test-set configuration in yaml file", zap.String("testSet", testSetID))
+		utils.LogError(g.Logger, err, "failed to write test-set configuration in yaml file", zap.String("testSet", testSetID))
 		return err
 	}
 
