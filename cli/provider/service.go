2e316d0634db8acd61ba481967168467446682e6
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2024-06-10 14:55:27 +0530

    feat: add support for testSet config (#1948)
    * feat: add support for testSet config
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    * fix: lint
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    ---------
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- cli/provider/service.go
+++ cli/provider/service.go
@@ -5,6 +5,11 @@ import (
 	"errors"
 
 	"go.keploy.io/server/v2/config"
+	"go.keploy.io/server/v2/pkg/core"
+	"go.keploy.io/server/v2/pkg/core/hooks"
+	"go.keploy.io/server/v2/pkg/core/proxy"
+	"go.keploy.io/server/v2/pkg/core/tester"
+	"go.keploy.io/server/v2/pkg/platform/docker"
 	"go.keploy.io/server/v2/pkg/platform/telemetry"
 	"go.keploy.io/server/v2/pkg/platform/yaml/configdb/user"
 
@@ -42,6 +47,55 @@ func (n *ServiceProvider) GetTelemetryService(ctx context.Context, config *confi
 	), nil
 }
 
+func (n *ServiceProvider) GetCommonServices(c *config.Config) *CommonInternalService {
+
+	h := hooks.NewHooks(n.logger, c)
+	p := proxy.New(n.logger, h, c)
+	//for keploy test bench
+	t := tester.New(n.logger, h)
+
+	var client docker.Client
+	var err error
+	if utils.IsDockerKind(utils.CmdType(c.CommandType)) {
+		client, err = docker.New(n.logger)
+		if err != nil {
+			utils.LogError(n.logger, err, "failed to create docker client")
+		}
+
+		//parse docker command only in case of docker start or docker run commands
+		if utils.CmdType(c.CommandType) != utils.DockerCompose {
+
+			cont, net, err := docker.ParseDockerCmd(c.Command, utils.CmdType(c.CommandType), client)
+			n.logger.Debug("container and network parsed from command", zap.String("container", cont), zap.String("network", net), zap.String("command", c.Command))
+			if err != nil {
+				utils.LogError(n.logger, err, "failed to parse container name from given docker command", zap.String("cmd", c.Command))
+			}
+			if c.ContainerName != "" && c.ContainerName != cont {
+				n.logger.Warn(fmt.Sprintf("given app container:(%v) is different from parsed app container:(%v), taking parsed value", c.ContainerName, cont))
+			}
+			c.ContainerName = cont
+
+			if c.NetworkName != "" && c.NetworkName != net {
+				n.logger.Warn(fmt.Sprintf("given docker network:(%v) is different from parsed docker network:(%v), taking parsed value", c.NetworkName, net))
+			}
+			c.NetworkName = net
+
+			n.logger.Debug("Using container and network", zap.String("container", c.ContainerName), zap.String("network", c.NetworkName))
+		}
+	}
+
+	instrumentation := core.New(n.logger, h, p, t, client)
+	testDB := testdb.New(n.logger, c.Path)
+	mockDB := mockdb.New(n.logger, c.Path, "")
+	reportDB := reportdb.New(n.logger, c.Path+"/reports")
+	return &CommonInternalService{
+		Instrumentation: instrumentation,
+		YamlTestDB:      testDB,
+		YamlMockDb:      mockDB,
+		YamlReportDb:    reportDB,
+	}
+}
+
 func (n *ServiceProvider) GetService(ctx context.Context, cmd string) (interface{}, error) {
 	tel, err := n.GetTelemetryService(ctx, n.cfg)
 	if err != nil {
@@ -54,7 +108,14 @@ func (n *ServiceProvider) GetService(ctx context.Context, cmd string) (interface
 	case "gen":
 		return utgen.NewUnitTestGenerator(n.cfg.Gen.SourceFilePath, n.cfg.Gen.TestFilePath, n.cfg.Gen.CoverageReportPath, n.cfg.Gen.TestCommand, n.cfg.Gen.TestDir, n.cfg.Gen.CoverageFormat, n.cfg.Gen.DesiredCoverage, n.cfg.Gen.MaxIterations, n.cfg.Gen.Model, n.cfg.Gen.APIBaseURL, n.cfg.Gen.APIVersion, n.cfg, tel, n.logger)
 	case "record", "test", "mock", "normalize":
-		return GetCoreService(ctx, cmd, n.cfg, n.logger, tel)
+		commonServices := n.GetCommonServices(n.cfg)
+		if cmd == "record" {
+			return record.New(n.logger, commonServices.YamlTestDB, commonServices.YamlMockDb, tel, commonServices.Instrumentation, n.cfg), nil
+		}
+		if cmd == "test" || cmd == "normalize" {
+			return replay.NewReplayer(n.logger, commonServices.YamlTestDB, commonServices.YamlMockDb, commonServices.YamlReportDb, tel, commonServices.Instrumentation, n.cfg), nil
+		}
+		return nil, errors.New("invalid command")
 	default:
 		return nil, errors.New("invalid command")
 	}
