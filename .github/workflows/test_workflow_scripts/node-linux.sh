98f937d64836333790d8ebc8f391dc6afdc19dcb
shivamsouravjha <shivamsouravjha@gmail.com>
2024-06-11 17:50:29 +0530

    fix: node linux
    Signed-off-by: shivamsouravjha <shivamsouravjha@gmail.com>

--- .github/workflows/test_workflow_scripts/node-linux.sh
+++ .github/workflows/test_workflow_scripts/node-linux.sh
@@ -9,52 +9,58 @@ npm install
 sed -i "s/mongoDb:27017/localhost:27017/" "src/db/connection.js"
 rm -rf keploy/
 
-# Function to check app started
-check_app_started() {
-    while ! curl -X GET http://localhost:8000/students; do
-        sleep 3
+send_request(){
+    pid=$(pgrep keploy)
+    sleep 10
+    app_started=false
+    while [ "$app_started" = false ]; do
+        if curl -X GET http://localhost:8000/students; then
+            app_started=true
+        fi
+        sleep 3 # wait for 3 seconds before checking again.
     done
-}
-
-# Function to perform API calls
-perform_api_calls() {
+    echo "App started"
+    # Start making curl calls to record the testcases and mocks.
     curl --request POST --url http://localhost:8000/students --header 'content-type: application/json' --data '{"name":"John Doe","email":"john@xyiz.com","phone":"0123456799"}'
     curl --request POST --url http://localhost:8000/students --header 'content-type: application/json' --data '{"name":"Alice Green","email":"green@alice.com","phone":"3939201584"}'
     curl -X GET http://localhost:8000/students
+    # Wait for 10 seconds for keploy to record the tcs and mocks.
+    sleep 10
+    sudo kill $pid
 }
 
 # Record and test sessions in a loop
 for i in {1..2}; do
-    sudo -E env PATH=$PATH ./../../keployv2 record -c 'npm start' --generateGithubActions=false &> record_logs.txt &
-    check_app_started
-    perform_api_calls
+    app_name="nodeApp_${i}"
+    send_request &
+    sudo -E env PATH=$PATH ./../../keployv2 record -c 'npm start' --generateGithubActions=false &> "${app_name}.txt"
+    if grep "WARNING: DATA RACE" "${app_name}.txt"; then
+        echo "Race condition detected in recording, stopping pipeline..."
+        cat "${app_name}.txt"
+        exit 1
+    fi
     sleep 5
-    sudo kill $(pgrep keploy)
-    sleep 5
+    echo "Recorded test case and mocks for iteration ${i}"
 done
 
 # Test modes and result checking
 sudo -E env PATH=$PATH ./../../keployv2 test -c 'npm start' --delay 10 --generateGithubActions=false &> test_logs.txt
-
-grep -q "race condition detected" test_logs.txt && echo "Race condition detected in testing, stopping tests..." && exit 1
+if grep "WARNING: DATA RACE" "test_logs.txt"; then
+    echo "Race condition detected in test, stopping pipeline..."
+    cat "test_logs.txt"
+    exit 1
+fi
 
 sudo -E env PATH=$PATH ./../../keployv2 config --generate
+
 sed -i 's/selectedTests: {}/selectedTests: {"test-set-0": ["test-1", "test-2"]}/' "./keploy.yml"
+
 sudo -E env PATH=$PATH ./../../keployv2 test -c 'npm start' --apiTimeout 30 --delay 10 --generateGithubActions=false &> test_logs2.txt
-
-grep -q "race condition detected" test_logs2.txt && echo "Race condition detected in testing, stopping tests..." && exit 1
-
-# Consolidate test results
-report_file="./keploy/reports/test-run-0/test-set-0-report.yaml"
-test_status1=$(awk '/status:/ {print $2}' $report_file)
-report_file2="./keploy/reports/test-run-0/test-set-1-report.yaml"
-test_status2=$(awk '/status:/ {print $2}' $report_file2)
-report_file5="./keploy/reports/test-run-1/test-set-0-report.yaml"
-test_status5=$(awk '/status:/ {print $2}' $report_file5)
-report_file6="./keploy/reports/test-run-2/test-set-0-report.yaml"
-test_status6=$(awk '/status:/ {print $2}' $report_file6)
-test_total6=$(awk '/total:/ {print $2}' $report_file6)
-test_failure=$(awk '/failure:/ {print $2}' $report_file6)
+if grep "WARNING: DATA RACE" "test_logs2.txt"; then
+    echo "Race condition detected in test, stopping pipeline..."
+    cat "test_logs.txt"
+    exit 1
+fi
 
 all_passed=true
 
