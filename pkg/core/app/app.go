a2122e163ca1a03880e547938391b966bbb0baf0
shivamsouravjha <shivamsouravjha@gmail.com>
2024-06-12 18:23:58 +0530

    fix: ipcontainer race
    Signed-off-by: shivamsouravjha <shivamsouravjha@gmail.com>

--- pkg/core/app/app.go
+++ pkg/core/app/app.go
@@ -45,7 +45,7 @@ type App struct {
 	containerDelay   uint64
 	container        string
 	containerNetwork string
-	containerIPv4    string
+	containerIPv4    chan string
 	keployNetwork    string
 	keployContainer  string
 	keployIPv4       string
@@ -90,7 +90,10 @@ func (a *App) KeployIPv4Addr() string {
 }
 
 func (a *App) ContainerIPv4Addr() string {
-	return a.containerIPv4
+	return <-a.containerIPv4
+}
+func (a *App) SetContainerIPv4Addr(ipAddr string) {
+	a.containerIPv4 <- ipAddr
 }
 
 func (a *App) SetupDocker() error {
@@ -292,7 +295,7 @@ func (a *App) extractMeta(ctx context.Context, e events.Message) (bool, error) {
 		a.logger.Debug("container network not found", zap.Any("containerDetails.NetworkSettings.Networks", info.NetworkSettings.Networks))
 		return false, fmt.Errorf("container network not found: %s", fmt.Sprintf("%+v", info.NetworkSettings.Networks))
 	}
-	a.containerIPv4 = n.IPAddress
+	a.SetContainerIPv4Addr(n.IPAddress)
 	return inode != 0 && n.IPAddress != "", nil
 }
 
