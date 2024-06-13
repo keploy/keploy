122d51e2b3e6af0020add381f3f2da15827150ae
charankamarapu <kamarapucharan@gmail.com>
2024-06-13 18:10:47 +0530

    testing
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -489,8 +489,7 @@ func RunInDocker(ctx context.Context, logger *zap.Logger) error {
 	if runtime.GOOS == "windows" {
 		for i, arg := range os.Args[1:] {
 			// Manually quote each argument for Windows
-			if strings.ContainsAny(arg, " ") {
-				arg = strings.Trim(arg, " ")
+			if strings.ContainsAny(arg, "&()^@=!%:\"')") {
 				fmt.Println("number", i)
 				quotedArgs = append(quotedArgs, `"`+arg+`"`)
 			} else {
