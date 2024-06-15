5ca24ac6ded822d5b29ce77568bdff32c68a1dd7
AhmedLotfy02 <ahmadlotfygamersfield@gmail.com>
2024-06-15 09:14:06 +0300

    [refactor]: the code to output yaml (validated by swagger editor)
    Signed-off-by: AhmedLotfy02 <ahmadlotfygamersfield@gmail.com>

--- pkg/models/openapi.go
+++ pkg/models/openapi.go
@@ -8,58 +8,57 @@ type HTTPSchema2 struct {
 }
 
 type OpenAPI struct {
-	OpenAPI    string                 `json:"openapi"`
-	Info       Info                   `json:"info"`
-	Paths      map[string]PathItem    `json:"paths"`
-	Components map[string]interface{} `json:"components"`
+	OpenAPI    string                 `json:"openapi" yaml:"openapi"`
+	Info       Info                   `json:"info" yaml:"info"`
+	Paths      map[string]PathItem    `json:"paths" yaml:"paths"`
+	Components map[string]interface{} `json:"components" yaml:"components"`
 }
 
 type Info struct {
-	Title   string `json:"title"`
-	Version string `json:"version"`
+	Title   string `json:"title" yaml:"title"`
+	Version string `json:"version" yaml:"version"`
 }
 
 type PathItem struct {
-	Get  *Operation `json:"get,omitempty"`
-	Post *Operation `json:"post,omitempty"`
+	Get  *Operation `json:"get,omitempty" yaml:"get,omitempty"`
+	Post *Operation `json:"post,omitempty" yaml:"post,omitempty"`
 }
 
 type Operation struct {
-	Summary     string                  `json:"summary"`
-	Description string                  `json:"description"`
-	OperationID string                  `json:"operationId"`
-	Parameters  []Parameter             `json:"parameters"`
-	RequestBody *RequestBody            `json:"requestBody,omitempty"`
-	Responses   map[string]ResponseItem `json:"responses"`
+	Summary     string                  `json:"summary" yaml:"summary"`
+	Description string                  `json:"description" yaml:"description"`
+	Parameters  []Parameter             `json:"parameters" yaml:"parameters"`
+	RequestBody *RequestBody            `json:"requestBody,omitempty" yaml:"requestBody,omitempty"`
+	Responses   map[string]ResponseItem `json:"responses" yaml:"responses"`
 }
 
 type Parameter struct {
-	Name     string      `json:"name"`
-	In       string      `json:"in"`
-	Required bool        `json:"required"`
-	Schema   ParamSchema `json:"schema"`
-	Example  string      `json:"example"`
+	Name     string      `json:"name" yaml:"name"`
+	In       string      `json:"in" yaml:"in"`
+	Required bool        `json:"required" yaml:"required"`
+	Schema   ParamSchema `json:"schema" yaml:"schema"`
+	Example  string      `json:"example" yaml:"example"`
 }
 
 type RequestBody struct {
-	Content map[string]MediaType `json:"content"`
+	Content map[string]MediaType `json:"content" yaml:"content"`
 }
 
 type MediaType struct {
-	Schema  Schema `json:"schema"`
-	Example string `json:"example"`
+	Schema  Schema `json:"schema" yaml:"schema"`
+	Example string `json:"example" yaml:"example"`
 }
 
 type ResponseItem struct {
-	Description string               `json:"description"`
-	Content     map[string]MediaType `json:"content"`
+	Description string               `json:"description" yaml:"description"`
+	Content     map[string]MediaType `json:"content" yaml:"content"`
 }
 
 type Schema struct {
-	Type       string                       `json:"type"`
-	Properties map[string]map[string]string `json:"properties"`
+	Type       string                       `json:"type" yaml:"type"`
+	Properties map[string]map[string]string `json:"properties" yaml:"properties"`
 }
 
 type ParamSchema struct {
-	Type string `json:"type"`
+	Type string `json:"type" yaml:"type"`
 }
