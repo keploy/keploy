19f05029e1463bb07b30a113d10e8cd0f98f8216
Yash <khareyash05@gmail.com>
2024-06-22 11:42:05 +0530

    backward compatibility check
    Signed-off-by: Yash <khareyash05@gmail.com>

--- config/config.go
+++ config/config.go
@@ -9,54 +9,54 @@ import (
 
 type Config struct {
 	Path                  string       `json:"path" yaml:"path" mapstructure:"path"`
-	AppID                 string       `json:"appId" yaml:"appId" mapstructure:"app-id"`
+	AppID                 string       `json:"appId" yaml:"appId" mapstructure:"appId"`
 	Command               string       `json:"command" yaml:"command" mapstructure:"command"`
 	Port                  uint32       `json:"port" yaml:"port" mapstructure:"port"`
-	DNSPort               uint32       `json:"dnsPort" yaml:"dnsPort" mapstructure:"dns-port"`
-	ProxyPort             uint32       `json:"proxyPort" yaml:"proxyPort" mapstructure:"proxy-port"`
+	DNSPort               uint32       `json:"dnsPort" yaml:"dnsPort" mapstructure:"dnsPort"`
+	ProxyPort             uint32       `json:"proxyPort" yaml:"proxyPort" mapstructure:"proxyPort"`
 	Debug                 bool         `json:"debug" yaml:"debug" mapstructure:"debug"`
-	DisableTele           bool         `json:"disableTele" yaml:"disableTele" mapstructure:"disable-tele"`
-	DisableANSI           bool         `json:"disableANSI" yaml:"disableANSI" mapstructure:"disable-ansi"`
-	InDocker              bool         `json:"inDocker" yaml:"inDocker" mapstructure:"in_docker"`
-	ContainerName         string       `json:"containerName" yaml:"containerName" mapstructure:"container-name"`
-	NetworkName           string       `json:"networkName" yaml:"networkName" mapstructure:"network-name"`
-	BuildDelay            uint64       `json:"buildDelay" yaml:"buildDelay" mapstructure:"build-delay"`
+	DisableTele           bool         `json:"disableTele" yaml:"disableTele" mapstructure:"disableTele"`
+	DisableANSI           bool         `json:"disableANSI" yaml:"disableANSI" mapstructure:"disableAnsi"`
+	InDocker              bool         `json:"inDocker" yaml:"inDocker" mapstructure:"inDocker"`
+	ContainerName         string       `json:"containerName" yaml:"containerName" mapstructure:"containerName"`
+	NetworkName           string       `json:"networkName" yaml:"networkName" mapstructure:"networkName"`
+	BuildDelay            uint64       `json:"buildDelay" yaml:"buildDelay" mapstructure:"buildDelay"`
 	Test                  Test         `json:"test" yaml:"test" mapstructure:"test"`
 	Record                Record       `json:"record" yaml:"record" mapstructure:"record"`
 	Gen                   UtGen        `json:"gen" yaml:"gen" mapstructure:"gen"`
 	Normalize             Normalize    `json:"normalize" yaml:"normalize" mapstructure:"normalize"`
-	ConfigPath            string       `json:"configPath" yaml:"configPath" mapstructure:"config-path"`
-	BypassRules           []BypassRule `json:"bypassRules" yaml:"bypassRules" mapstructure:"bypassrules"`
-	EnableTesting         bool         `json:"enableTesting" yaml:"enableTesting" mapstructure:"enable-testing"`
-	GenerateGithubActions bool         `json:"generateGithubActions" yaml:"generateGithubActions" mapstructure:"generate-github-actions"`
-	KeployContainer       string       `json:"keployContainer" yaml:"keployContainer" mapstructure:"keploy-container"`
-	KeployNetwork         string       `json:"keployNetwork" yaml:"keployNetwork" mapstructure:"keploy-network"`
-	CommandType           string       `json:"cmdType" yaml:"cmdType" mapstructure:"cmd-type"`
+	ConfigPath            string       `json:"configPath" yaml:"configPath" mapstructure:"configPath"`
+	BypassRules           []BypassRule `json:"bypassRules" yaml:"bypassRules" mapstructure:"bypassRules"`
+	EnableTesting         bool         `json:"enableTesting" yaml:"enableTesting" mapstructure:"enableTesting"`
+	GenerateGithubActions bool         `json:"generateGithubActions" yaml:"generateGithubActions" mapstructure:"generateGithubActions"`
+	KeployContainer       string       `json:"keployContainer" yaml:"keployContainer" mapstructure:"keployContainer"`
+	KeployNetwork         string       `json:"keployNetwork" yaml:"keployNetwork" mapstructure:"keployNetwork"`
+	CommandType           string       `json:"cmdType" yaml:"cmdType" mapstructure:"cmdType"`
 }
 
 type UtGen struct {
-	SourceFilePath     string  `json:"sourceFilePath" yaml:"sourceFilePath" mapstructure:"source-file-path"`
-	TestFilePath       string  `json:"testFilePath" yaml:"testFilePath" mapstructure:"test-file-path"`
-	CoverageReportPath string  `json:"coverageReportPath" yaml:"coverageReportPath" mapstructure:"coverage-report-path"`
-	TestCommand        string  `json:"testCommand" yaml:"testCommand" mapstructure:"test-command"`
-	CoverageFormat     string  `json:"coverageFormat" yaml:"coverageFormat" mapstructure:"coverage-format"`
-	DesiredCoverage    float64 `json:"expectedCoverage" yaml:"expectedCoverage" mapstructure:"expected-coverage"`
-	MaxIterations      int     `json:"maxIterations" yaml:"maxIterations" mapstructure:"max-iterations"`
-	TestDir            string  `json:"testDir" yaml:"testDir" mapstructure:"test-dir"`
-	APIBaseURL         string  `json:"llmBaseUrl" yaml:"llmBaseUrl" mapstructure:"llm-base-url"`
+	SourceFilePath     string  `json:"sourceFilePath" yaml:"sourceFilePath" mapstructure:"sourceFilePath"`
+	TestFilePath       string  `json:"testFilePath" yaml:"testFilePath" mapstructure:"testFilePath"`
+	CoverageReportPath string  `json:"coverageReportPath" yaml:"coverageReportPath" mapstructure:"coverageReportPath"`
+	TestCommand        string  `json:"testCommand" yaml:"testCommand" mapstructure:"testCommand"`
+	CoverageFormat     string  `json:"coverageFormat" yaml:"coverageFormat" mapstructure:"coverageFormat"`
+	DesiredCoverage    float64 `json:"expectedCoverage" yaml:"expectedCoverage" mapstructure:"expectedCoverage"`
+	MaxIterations      int     `json:"maxIterations" yaml:"maxIterations" mapstructure:"maxIterations"`
+	TestDir            string  `json:"testDir" yaml:"testDir" mapstructure:"testDir"`
+	APIBaseURL         string  `json:"llmBaseUrl" yaml:"llmBaseUrl" mapstructure:"llmBaseUrl"`
 	Model              string  `json:"model" yaml:"model" mapstructure:"model"`
-	APIVersion         string  `json:"llmApiVersion" yaml:"llmApiVersion" mapstructure:"llm-api-version"`
+	APIVersion         string  `json:"llmApiVersion" yaml:"llmApiVersion" mapstructure:"llmApiVersion"`
 }
 
 type Record struct {
 	Filters     []Filter      `json:"filters" yaml:"filters" mapstructure:"filters"`
-	RecordTimer time.Duration `json:"recordTimer" yaml:"recordTimer" mapstructure:"record-timer"`
+	RecordTimer time.Duration `json:"recordTimer" yaml:"recordTimer" mapstructure:"recordTimer"`
 	ReRecord    string        `json:"rerecord" yaml:"rerecord" mapstructure:"rerecord"`
 }
 
 type Normalize struct {
-	SelectedTests []SelectedTests `json:"selectedTests" yaml:"selectedTests" mapstructure:"selected-tests"`
-	TestRun       string          `json:"testReport" yaml:"testReport" mapstructure:"test-report"`
+	SelectedTests []SelectedTests `json:"selectedTests" yaml:"selectedTests" mapstructure:"selectedTests"`
+	TestRun       string          `json:"testReport" yaml:"testReport" mapstructure:"testReport"`
 }
 
 type BypassRule struct {
@@ -67,34 +67,34 @@ type BypassRule struct {
 
 type Filter struct {
 	BypassRule `mapstructure:",squash"`
-	URLMethods []string          `json:"urlMethods" yaml:"urlMethods" mapstructure:"url-methods"`
+	URLMethods []string          `json:"urlMethods" yaml:"urlMethods" mapstructure:"urlMethods"`
 	Headers    map[string]string `json:"headers" yaml:"headers" mapstructure:"headers"`
 }
 
 type Test struct {
-	SelectedTests      map[string][]string `json:"selectedTests" yaml:"selectedTests" mapstructure:"selected-tests"`
+	SelectedTests      map[string][]string `json:"selectedTests" yaml:"selectedTests" mapstructure:"selectedTests"`
 	GlobalNoise        Globalnoise         `json:"globalNoise" yaml:"globalNoise" mapstructure:"globalNoise"`
 	Delay              uint64              `json:"delay" yaml:"delay" mapstructure:"delay"`
-	APITimeout         uint64              `json:"apiTimeout" yaml:"apiTimeout" mapstructure:"api-timeout"`
-	Coverage           bool                `json:"coverage" yaml:"coverage" mapstructure:"coverage"`                                 // boolean to capture the coverage in test
-	CoverageReportPath string              `json:"coverageReportPath" yaml:"coverageReportPath" mapstructure:"coverage-report-path"` // directory path to store the coverage files
-	GoCoverage         bool                `json:"goCoverage" yaml:"goCoverage" mapstructure:"go-coverage"`                          // boolean to capture the coverage in test
-	IgnoreOrdering     bool                `json:"ignoreOrdering" yaml:"ignoreOrdering" mapstructure:"ignore-ordering"`
-	MongoPassword      string              `json:"mongoPassword" yaml:"mongoPassword" mapstructure:"mongo-password"`
+	APITimeout         uint64              `json:"apiTimeout" yaml:"apiTimeout" mapstructure:"apiTimeout"`
+	Coverage           bool                `json:"coverage" yaml:"coverage" mapstructure:"coverage"`                               // boolean to capture the coverage in test
+	CoverageReportPath string              `json:"coverageReportPath" yaml:"coverageReportPath" mapstructure:"coverageReportPath"` // directory path to store the coverage files
+	GoCoverage         bool                `json:"goCoverage" yaml:"goCoverage" mapstructure:"goCoverage"`                         // boolean to capture the coverage in test
+	IgnoreOrdering     bool                `json:"ignoreOrdering" yaml:"ignoreOrdering" mapstructure:"ignoreOrdering"`
+	MongoPassword      string              `json:"mongoPassword" yaml:"mongoPassword" mapstructure:"mongoPassword"`
 	Language           string              `json:"language" yaml:"language" mapstructure:"language"`
-	RemoveUnusedMocks  bool                `json:"removeUnusedMocks" yaml:"removeUnusedMocks" mapstructure:"remove-unused-mocks"`
-	FallBackOnMiss     bool                `json:"fallBackOnMiss" yaml:"fallBackOnMiss" mapstructure:"fallBack-on-miss"`
-	BasePath           string              `json:"basePath" yaml:"basePath" mapstructure:"base-path"`
+	RemoveUnusedMocks  bool                `json:"removeUnusedMocks" yaml:"removeUnusedMocks" mapstructure:"removeUnusedMocks"`
+	FallBackOnMiss     bool                `json:"fallBackOnMiss" yaml:"fallBackOnMiss" mapstructure:"fallBackOnMiss"`
+	BasePath           string              `json:"basePath" yaml:"basePath" mapstructure:"basePath"`
 	Mocking            bool                `json:"mocking" yaml:"mocking" mapstructure:"mocking"`
 }
 
 type Globalnoise struct {
 	Global   GlobalNoise  `json:"global" yaml:"global" mapstructure:"global"`
-	Testsets TestsetNoise `json:"test-sets" yaml:"test-sets" mapstructure:"test-sets"`
+	Testsets TestsetNoise `json:"testSets" yaml:"testSets" mapstructure:"testSets"`
 }
 
 type SelectedTests struct {
-	TestSet string   `json:"testSet" yaml:"testSet" mapstructure:"test-set"`
+	TestSet string   `json:"testSet" yaml:"testSet" mapstructure:"testSet"`
 	Tests   []string `json:"tests" yaml:"tests" mapstructure:"tests"`
 }
 
