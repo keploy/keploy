17f78b42814673bb407b6a67beda1194c214e748
Lorenzo Castelli <lorenzo@castellimail.it>
2024-06-27 17:17:25 +0200

    clenup color scheme for disabled ANSI in logs
    Signed-off-by: Lorenzo Castelli <lorenzo@castellimail.it>

--- pkg/models/const.go
+++ pkg/models/const.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/fatih/color"
@@ -26,39 +27,89 @@ var orangeColorSGR = []color.Attribute{38, 5, 208}
 
 var BaseTime = time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
 
-var HighlightString = color.New(orangeColorSGR...).SprintFunc()
-var HighlightPassingString = color.New(color.FgGreen).SprintFunc()
-var HighlightFailingString = color.New(color.FgRed).SprintFunc()
-var HighlightGrayString = color.New(color.FgHiBlack).SprintFunc()
+var IsAnsiDisabled = false
 
-var PassingColorScheme = pp.ColorScheme{
-	String:          pp.Green,
-	StringQuotation: pp.Green | pp.Bold,
-	FieldName:       pp.White,
-	Integer:         pp.Blue | pp.Bold,
-	StructName:      pp.NoColor,
-	Bool:            pp.Cyan | pp.Bold,
-	Float:           pp.Magenta | pp.Bold,
-	EscapedChar:     pp.Magenta | pp.Bold,
-	PointerAdress:   pp.Blue | pp.Bold,
-	Nil:             pp.Cyan | pp.Bold,
-	Time:            pp.Blue | pp.Bold,
-	ObjectLength:    pp.Blue,
+var HighlightString = func(a ...interface{}) string {
+	if IsAnsiDisabled {
+		return fmt.Sprint(a)
+	}
+	return color.New(orangeColorSGR...).SprintFunc()(a)
 }
 
-var FailingColorScheme = pp.ColorScheme{
-	Bool:            pp.Cyan | pp.Bold,
-	Integer:         pp.Blue | pp.Bold,
-	Float:           pp.Magenta | pp.Bold,
-	String:          pp.Red,
-	StringQuotation: pp.Red | pp.Bold,
-	EscapedChar:     pp.Magenta | pp.Bold,
-	FieldName:       pp.Yellow,
-	PointerAdress:   pp.Blue | pp.Bold,
-	Nil:             pp.Cyan | pp.Bold,
-	Time:            pp.Blue | pp.Bold,
-	StructName:      pp.White,
-	ObjectLength:    pp.Blue,
+var HighlightPassingString = func(a ...interface{}) string {
+	if IsAnsiDisabled {
+		return fmt.Sprint(a)
+	}
+	return color.New(color.FgGreen).SprintFunc()(a)
+}
+
+var HighlightFailingString = func(a ...interface{}) string {
+	if IsAnsiDisabled {
+		return fmt.Sprint(a)
+	}
+	return color.New(color.FgRed).SprintFunc()(a)
+}
+
+var HighlightGrayString = func(a ...interface{}) string {
+	if IsAnsiDisabled {
+		return fmt.Sprint(a)
+	}
+	return color.New(color.FgHiBlack).SprintFunc()(a)
+}
+
+var defaultColorScheme = pp.ColorScheme{
+	Bool:            pp.NoColor,
+	Integer:         pp.NoColor,
+	Float:           pp.NoColor,
+	String:          pp.NoColor,
+	StringQuotation: pp.NoColor,
+	EscapedChar:     pp.NoColor,
+	FieldName:       pp.NoColor,
+	PointerAdress:   pp.NoColor,
+	Nil:             pp.NoColor,
+	Time:            pp.NoColor,
+	StructName:      pp.NoColor,
+	ObjectLength:    pp.NoColor,
+}
+
+var GetPassingColorScheme = func() pp.ColorScheme {
+	if IsAnsiDisabled {
+		return defaultColorScheme
+	}
+	return pp.ColorScheme{
+		String:          pp.Green,
+		StringQuotation: pp.Green | pp.Bold,
+		FieldName:       pp.White,
+		Integer:         pp.Blue | pp.Bold,
+		StructName:      pp.NoColor,
+		Bool:            pp.Cyan | pp.Bold,
+		Float:           pp.Magenta | pp.Bold,
+		EscapedChar:     pp.Magenta | pp.Bold,
+		PointerAdress:   pp.Blue | pp.Bold,
+		Nil:             pp.Cyan | pp.Bold,
+		Time:            pp.Blue | pp.Bold,
+		ObjectLength:    pp.Blue,
+	}
+}
+
+var GetFailingColorScheme = func() pp.ColorScheme {
+	if IsAnsiDisabled {
+		return defaultColorScheme
+	}
+	return pp.ColorScheme{
+		Bool:            pp.Cyan | pp.Bold,
+		Integer:         pp.Blue | pp.Bold,
+		Float:           pp.Magenta | pp.Bold,
+		String:          pp.Red,
+		StringQuotation: pp.Red | pp.Bold,
+		EscapedChar:     pp.Magenta | pp.Bold,
+		FieldName:       pp.Yellow,
+		PointerAdress:   pp.Blue | pp.Bold,
+		Nil:             pp.Cyan | pp.Bold,
+		Time:            pp.Blue | pp.Bold,
+		StructName:      pp.White,
+		ObjectLength:    pp.Blue,
+	}
 }
 
 // MySQL constants
