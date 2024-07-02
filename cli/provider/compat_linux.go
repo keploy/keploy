521ec269e39606cd291c51d34a89aceb4d896527
slayerjain <shubhamkjain@outlook.com>
2024-07-02 19:50:16 +0530

    fix: revise linux kernel compatibility to 5.10
    Signed-off-by: slayerjain <shubhamkjain@outlook.com>

--- cli/provider/compat_linux.go
+++ cli/provider/compat_linux.go
@@ -10,17 +10,18 @@ import (
 )
 
 func isCompatible(logger *zap.Logger) error {
-	//check if the version of the kernel is above 5.15 for eBPF support
-	isValid := kernel.CheckKernelVersion(5, 15, 0)
+	//check if the version of the kernel is above 5.10 for eBPF support
+	isValid := kernel.CheckKernelVersion(5, 10, 0)
 	if !isValid {
 		c, err := kernel.GetKernelVersion()
 		if err != nil {
 			logger.Error("Error getting kernel version", zap.Error(err))
 			return err
 		}
-		errMsg := "detected linux kernel version" + c.String() + ". Keploy requires linux kernel version 5.15 or above. Please upgrade your kernel or docker version.\n"
+		errMsg := "detected linux kernel version" + c.String() + ". Keploy requires linux kernel version 5.10 or above. Please upgrade your kernel or docker version.\n"
 		logger.Error(errMsg)
 		return errors.New(errMsg)
 	}
+	// TODO check for cgroup v2 support
 	return nil
 }
