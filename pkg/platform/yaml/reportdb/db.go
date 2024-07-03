5d2f29eb215769030cf5724afe2cd463c33cf08b
slayerjain <shubhamkjain@outlook.com>
2024-07-03 17:04:27 +0530

    feat: new experience to get coverage report (#1785)
    Signed-off-by: Yash <khareyash05@gmail.com>

--- pkg/platform/yaml/reportdb/db.go
+++ pkg/platform/yaml/reportdb/db.go
@@ -109,3 +109,21 @@ func (fe *TestReport) InsertReport(ctx context.Context, testRunID string, testSe
 	}
 	return nil
 }
+
+func (fe *TestReport) UpdateReport(ctx context.Context, testRunID string, coverageReport any) error {
+	reportPath := filepath.Join(fe.Path, testRunID)
+
+	data := []byte{}
+	d, err := yamlLib.Marshal(&coverageReport)
+	if err != nil {
+		return fmt.Errorf("%s failed to marshal document to yaml. error: %s", utils.Emoji, err.Error())
+	}
+	data = append(data, d...)
+
+	err = yaml.WriteFile(ctx, fe.Logger, reportPath, "coverage", data, false)
+	if err != nil {
+		utils.LogError(fe.Logger, err, "failed to write the coverage report to yaml", zap.Any("session", filepath.Base(reportPath)))
+		return err
+	}
+	return nil
+}
