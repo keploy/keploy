407fddb6e3eede754508f99362e839f5f038dc35
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2024-07-05 13:01:10 +0530

    feat: add support for basePath in macOS (#2032)
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    Signed-off-by: Yash <khareyash05@gmail.com>

--- cli/provider/core_service_others.go
+++ cli/provider/core_service_others.go
@@ -7,10 +7,50 @@ import (
 	"errors"
 
 	"go.keploy.io/server/v2/config"
+	"go.keploy.io/server/v2/pkg/core"
+	"go.keploy.io/server/v2/pkg/models"
 	"go.keploy.io/server/v2/pkg/platform/telemetry"
+	"go.keploy.io/server/v2/pkg/platform/yaml/configdb/testset"
+	mockdb "go.keploy.io/server/v2/pkg/platform/yaml/mockdb"
+	reportdb "go.keploy.io/server/v2/pkg/platform/yaml/reportdb"
+	testdb "go.keploy.io/server/v2/pkg/platform/yaml/testdb"
+	"go.keploy.io/server/v2/pkg/service/replay"
 	"go.uber.org/zap"
 )
 
-func Get(ctx context.Context, cmd string, cfg *config.Config, logger *zap.Logger, tel *telemetry.Telemetry) (interface{}, error) {
+type CommonInternalService struct {
+	commonPlatformServices
+	Instrumentation *core.Core
+}
+
+func Get(ctx context.Context, cmd string, c *config.Config, logger *zap.Logger, tel *telemetry.Telemetry) (interface{}, error) {
+	commonServices, err := GetCommonServices(ctx, c, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	replaySvc := replay.NewReplayer(logger, commonServices.YamlTestDB, commonServices.YamlMockDb, commonServices.YamlReportDb, commonServices.YamlTestSetDB, tel, commonServices.Instrumentation, c)
+
+	if cmd == "test" || cmd == "normalize" || cmd == "templatize" {
+		return replaySvc, nil
+	}
+
 	return nil, errors.New("command not supported in non linux os")
+}
+
+func GetCommonServices(_ context.Context, c *config.Config, logger *zap.Logger) (*CommonInternalService, error) {
+	instrumentation := core.New(logger)
+	testDB := testdb.New(logger, c.Path)
+	mockDB := mockdb.New(logger, c.Path, "")
+	reportDB := reportdb.New(logger, c.Path+"/reports")
+	testSetDb := testset.New[*models.TestSet](logger, c.Path)
+	return &CommonInternalService{
+		commonPlatformServices{
+			YamlTestDB:    testDB,
+			YamlMockDb:    mockDB,
+			YamlReportDb:  reportDB,
+			YamlTestSetDB: testSetDb,
+		},
+		instrumentation,
+	}, nil
 }
