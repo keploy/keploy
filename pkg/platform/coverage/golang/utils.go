5007af1f400fad36087d0af30b043589082d00ed
Akash Kumar <meakash7902@gmail.com>
2024-07-11 13:12:50 +0530

    fix: test -cover flag only when binary is provided which implies one argument in command
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- pkg/platform/coverage/golang/utils.go
+++ pkg/platform/coverage/golang/utils.go
@@ -2,21 +2,27 @@ package golang
 
 import (
 	"debug/elf"
-	"fmt"
-	"os"
+	"slices"
 	"strings"
 
 	"go.keploy.io/server/v2/utils"
 	"go.uber.org/zap"
 )
 
-// CheckGoBinaryForCoverFlag checks if the given Go binary has the coverage flag enabled
+// checkForCoverFlag checks if the given Go binary has the coverage flag enabled
 // TODO: use native approach till https://github.com/golang/go/issues/67366 gets resolved
-func checkGoBinaryForCoverFlag(logger *zap.Logger, cmd string) bool {
+func checkForCoverFlag(logger *zap.Logger, cmd string) bool {
+	cmdFields := strings.Fields(cmd)
+	if cmdFields[0] == "go" && len(cmdFields) > 1 {
+		if slices.Contains(cmdFields, "-cover") {
+			return true
+		}
+		logger.Warn("cover flag not found in command, skipping coverage calculation")
+		return false
+	}
 	file, err := elf.Open(cmd)
 	if err != nil {
-		utils.LogError(logger, err, "failed to open file")
-		fmt.Fprintf(os.Stderr, "Failed to open file: %v\n", err)
+		utils.LogError(logger, err, "failed to open file, skipping coverage calculation")
 		return false
 	}
 	defer func() {
@@ -27,7 +33,7 @@ func checkGoBinaryForCoverFlag(logger *zap.Logger, cmd string) bool {
 
 	symbols, err := file.Symbols()
 	if err != nil {
-		utils.LogError(logger, err, "failed to read symbols")
+		utils.LogError(logger, err, "failed to read symbols, skipping coverage calculation")
 		return false
 	}
 
@@ -37,5 +43,6 @@ func checkGoBinaryForCoverFlag(logger *zap.Logger, cmd string) bool {
 			return true
 		}
 	}
+	logger.Warn("go binary was not build with -cover flag", zap.String("file", cmd))
 	return false
 }
