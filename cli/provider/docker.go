dfc285c2d76a4580eb8c8477e42105874ad2d5f1
Yash Khare <khareyash05@gmail.com>
2024-07-15 12:26:24 +0530

    Update docker.go

--- cli/provider/docker.go
+++ cli/provider/docker.go
@@ -143,6 +143,9 @@ func getAlias(ctx context.Context, logger *zap.Logger) (string, error) {
 		ttyFlag = " "
 	}
 
+	logger.info("TTYflag will be here",zap.Any("here",ttyFlag))
+	logger.info("os will be here",zap.Any("here",osName))
+
 	switch osName {
 	case "linux":
 		alias := "sudo docker container run --name keploy-v2 " + envs + "-e BINARY_TO_DOCKER=true -p 16789:16789 --privileged --pid=host" + ttyFlag + "-v " + os.Getenv("PWD") + ":" + os.Getenv("PWD") + " -w " + os.Getenv("PWD") + " -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock -v " + os.Getenv("HOME") + "/.keploy-config:/root/.keploy-config -v " + os.Getenv("HOME") + "/.keploy:/root/.keploy --rm " + img
