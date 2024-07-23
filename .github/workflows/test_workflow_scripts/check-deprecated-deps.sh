23ab46be4b18b38bfb50b01977327428fb0733e7
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-07-24 01:33:43 +0530

    shell script added
    Signed-off-by: sratslla <shubhgaur.cse24@jecrc.ac.in>

---
+++ .github/workflows/test_workflow_scripts/check-deprecated-deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run `go list -m -u all` to list all dependencies.
+output=$(go list -m -u all)
+
+found_deprecated=false
+
+while IFS= read -r line; do
+    if [[ "$line" == *"deprecated"* ]]; then
+        echo "Deprecated dependency found: $line"
+        found_deprecated=true
+    fi
+done <<< "$output"
+
+if [ "$found_deprecated" = true ]; then
+    echo "Exiting with failure due to deprecated dependencies."
+    exit 1
+fi
