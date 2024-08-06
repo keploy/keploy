bfed4fb7e54dc4b88bd39df99a1ccb58ffe8e2ee
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-08-06 19:47:56 +0530

    resolve conflicts
    Signed-off-by: sratslla <shubhgaur.cse24@jecrc.ac.in>

--- config/config.go
+++ config/config.go
@@ -83,6 +83,7 @@ type Test struct {
 	SelectedTests       map[string][]string `json:"selectedTests" yaml:"selectedTests" mapstructure:"selectedTests"`
 	GlobalNoise         Globalnoise         `json:"globalNoise" yaml:"globalNoise" mapstructure:"globalNoise"`
 	Delay               uint64              `json:"delay" yaml:"delay" mapstructure:"delay"`
+	Host                string              `json:"host" yaml:"host" mapstructure:"host"`
 	APITimeout          uint64              `json:"apiTimeout" yaml:"apiTimeout" mapstructure:"apiTimeout"`
 	SkipCoverage        bool                `json:"skipCoverage" yaml:"skipCoverage" mapstructure:"skipCoverage"`                   // boolean to capture the coverage in test
 	CoverageReportPath  string              `json:"coverageReportPath" yaml:"coverageReportPath" mapstructure:"coverageReportPath"` // directory path to store the coverage files
