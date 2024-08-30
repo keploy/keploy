c55431f54985a9bb2703d5c3c5c4b5a31cf2df59
shivamsouravjha <shivamsouravjha@gmail.com>
2024-08-30 15:22:07 +0530

    add: support for jacoco format
    Signed-off-by: shivamsouravjha <shivamsouravjha@gmail.com>

--- pkg/service/utgen/coverage.go
+++ pkg/service/utgen/coverage.go
@@ -5,6 +5,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -29,10 +30,10 @@ func NewCoverageProcessor(reportPath, srcpath, format string) *CoverageProcessor
 
 // ProcessCoverageReport verifies the report and parses it based on its type
 func (cp *CoverageProcessor) ProcessCoverageReport(latestTime int64) (*models.CoverageResult, error) {
-	err := cp.VerifyReportUpdate(latestTime)
-	if err != nil {
-		return nil, err
-	}
+	// err := cp.VerifyReportUpdate(latestTime)
+	// if err != nil {
+	// 	return nil, err
+	// }
 	return cp.ParseCoverageReport()
 }
 
@@ -67,6 +68,8 @@ func (cp *CoverageProcessor) ParseCoverageReport() (*models.CoverageResult, erro
 	switch cp.Format {
 	case "cobertura":
 		return cp.ParseCoverageReportCobertura()
+	case "jacoco":
+		return cp.ParseCoverageReportJacoco()
 	case "lcov":
 		return nil, fmt.Errorf("parsing for %s coverage reports is not implemented yet", cp.Format)
 	default:
@@ -130,6 +133,98 @@ func (cp *CoverageProcessor) ParseCoverageReportCobertura() (*models.CoverageRes
 		Packages: []models.Package{
 			{
 				Classes: filteredClasses,
+			},
+		},
+	}
+
+	// Encode the filtered coverage report to XML
+	var filteredBuf bytes.Buffer
+	xmlEncoder := xml.NewEncoder(&filteredBuf)
+	xmlEncoder.Indent("", "  ")
+	if err := xmlEncoder.Encode(filteredCov); err != nil {
+		return nil, err
+	}
+
+	coverageResult := &models.CoverageResult{
+		LinesCovered:  linesCovered,
+		LinesMissed:   linesMissed,
+		Coverage:      coveragePercentage,
+		Files:         filesToCover,
+		ReportContent: filteredBuf.String(),
+	}
+
+	return coverageResult, nil
+}
+
+func (cp *CoverageProcessor) ParseCoverageReportJacoco() (*models.CoverageResult, error) {
+
+	filesToCover := make([]string, 0)
+	// Open the XML file
+	xmlFile, err := os.Open(cp.ReportPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err := xmlFile.Close(); err != nil {
+			return
+		}
+	}()
+
+	// Decode the XML file into a Coverage struct
+	var jacoco models.Jacoco
+	if err := xml.NewDecoder(xmlFile).Decode(&jacoco); err != nil {
+		return nil, err
+	}
+
+	// Find coverage for the specified file
+	var linesCovered, linesMissed []int
+	var totalLines, coveredLines int
+	var filteredSourceFiles []models.JacocoSourceFile
+
+	for _, pkg := range jacoco.Packages {
+		for _, src := range pkg.SourceFiles {
+			if cp.SrcPath == "." {
+				filesToCover = append(filesToCover, src.Name)
+			}
+			if strings.HasSuffix(src.Name, cp.SrcPath) {
+				for _, line := range src.Lines {
+					totalLines++
+					missedInstructions, errMissed := strconv.Atoi(line.MissedInstructions)
+					coveredInstructions, errCovered := strconv.Atoi(line.CoveredInstructions)
+					if errMissed != nil || errCovered != nil {
+						// Handle conversion error
+						continue
+					}
+					if coveredInstructions > 0 {
+						coveredLines++
+						lineNumber, err := strconv.Atoi(line.Number)
+						if err == nil {
+							linesCovered = append(linesCovered, lineNumber)
+						}
+					} else if missedInstructions > 0 {
+						// Use missedInstructions to check if a line has any missed instructions
+						lineNumber, err := strconv.Atoi(line.Number)
+						if err == nil {
+							linesMissed = append(linesMissed, lineNumber)
+						}
+					}
+				}
+				filteredSourceFiles = append(filteredSourceFiles, src)
+				break
+			}
+		}
+	}
+	var coveragePercentage float64
+	if totalLines > 0 {
+		coveragePercentage = float64(len(linesCovered)) / float64(totalLines)
+	}
+
+	// Reconstruct the coverage report with only the filtered class
+	filteredCov := models.Jacoco{
+		Packages: []models.JacocoPackage{
+			{
+				SourceFiles: filteredSourceFiles,
 			},
 		},
 	}
