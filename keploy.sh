8d3342e8532677ed0dea3a97c7f34dc8cf0e4847
shivamsouravjha <2019145@iiitdmj.ac.in>
2024-09-02 21:12:58 +0530

    update: one click
    Signed-off-by: shivamsouravjha <2019145@iiitdmj.ac.in>

--- keploy.sh
+++ keploy.sh
@@ -3,6 +3,7 @@
 installKeploy (){
     version="latest"
     IS_CI=false
+    NO_ROOT=false
     for arg in "$@"
     do
         case $arg in
@@ -19,14 +20,62 @@ installKeploy (){
                     return 1 
                 fi
             ;;
+            -noRoot)
+                NO_ROOT=true
+                shift
+            ;;
             *)
             ;;
         esac
     done
-
     if [ "$version" != "latest" ]; then
         echo "Installing Keploy version: $version......"
     fi
+
+    move_keploy_binary() {
+        # Check if NO_ROOT is set to true
+        if [ "$NO_ROOT" = "true" ]; then
+            # Move without sudo
+            target_dir="$HOME/.keploy/bin"
+            source_dir="/tmp/keploy"  # Default source directory
+
+            # Create the target directory in the user's home directory
+            mkdir -p "$target_dir"
+            if [ $? -ne 0 ]; then
+                echo "Error: Failed to create directory $target_dir"
+                exit 1
+            fi
+
+            # Check if the OS is macOS (Darwin) to set the correct source path
+            OS_NAME=$(uname)  # Get the operating system name
+            if [ "$OS_NAME" = "Darwin" ]; then
+                source_dir="/tmp/keploy/keploy"  # Set source directory to the binary inside the extracted folder
+            fi
+
+            # Move the keploy binary to the user's home directory bin
+            if [ -f "$source_dir" ]; then
+                mv "$source_dir" "$target_dir/keploy"
+                if [ $? -ne 0 ]; then
+                    echo "Error: Failed to move the keploy binary from $source_dir to $target_dir"
+                    exit 1
+                fi
+            else
+                echo "Error: $source_dir does not exist."
+                exit 1
+            fi
+
+            # Make sure the binary is executable
+            chmod +x "$target_dir/keploy"
+            if [ $? -ne 0 ]; then
+                echo "Error: Failed to make the keploy binary executable"
+                exit 1
+            fi
+            set_alias
+        else
+            # Move with sudo
+            sudo mkdir -p /usr/local/bin && sudo mv /tmp/keploy /usr/local/bin/keploy
+        fi
+    }
 
     install_keploy_darwin_all() {
         if [ "$version" != "latest" ]; then
@@ -39,7 +88,7 @@ installKeploy (){
         rm -rf /tmp/keploy
         mkdir -p /tmp/keploy
         curl --silent --location "$download_url" | tar xz -C /tmp/keploy/
-        sudo mkdir -p /usr/local/bin && sudo mv /tmp/keploy/keploy /usr/local/bin/keploy
+        move_keploy_binary
         delete_keploy_alias
     }
 
@@ -50,8 +99,8 @@ installKeploy (){
             download_url="https://github.com/keploy/keploy/releases/latest/download/keploy_linux_arm64.tar.gz"
         fi
         curl --silent --location "$download_url" | tar xz --overwrite -C /tmp 
-        sudo mkdir -p /usr/local/bin && sudo mv /tmp/keploy /usr/local/bin/keploy
-        set_alias 'sudo -E env PATH="$PATH" keploy'
+        move_keploy_binary
+        set_alias
     }
 
 
@@ -62,8 +111,8 @@ installKeploy (){
             download_url="https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz"
         fi
         curl --silent --location "$download_url" | tar xz --overwrite -C /tmp
-        sudo mkdir -p /usr/local/bin && sudo mv /tmp/keploy /usr/local/bin/keploy
-        set_alias 'sudo -E env PATH="$PATH" keploy'
+        move_keploy_binary
+        set_alias
     }
 
     append_to_rc() {
@@ -78,36 +127,75 @@ installKeploy (){
 
     # Get the alias to set and set it
     set_alias() {
-        ALIAS_CMD="alias keploy='$1'"
+        OS_NAME=$(uname)
         current_shell="$(basename "$SHELL")"
-        if [[ "$current_shell" = "zsh" || "$current_shell" = "-zsh" ]]; then
-            if [ -f ~/.zshrc ]; then
-                if grep -q "alias keploy=" ~/.zshrc; then
-                    if [ "$OS_NAME" = "Darwin" ]; then
-                        sed -i '' '/alias keploy/d' ~/.zshrc
-                    else
-                        sed -i '/alias keploy/d' ~/.zshrc
-                    fi
+        if [ "$OS_NAME" = "Darwin" ]; then
+            # macOS case
+            if [ "$NO_ROOT" = "true" ]; then
+                # Just update the PATH in .zshrc or .bashrc, no alias needed
+                PATH_CMD="export PATH=\"\$HOME/.keploy/bin:\$PATH\""
+                if [[ "$current_shell" = "zsh" || "$current_shell" = "-zsh" ]]; then
+                    append_to_rc "$PATH_CMD" "$HOME/.zshrc"
+                elif [[ "$current_shell" = "bash" || "$current_shell" = "-bash" ]]; then
+                    append_to_rc "$PATH_CMD" "$HOME/.bashrc"
+                else
+                    append_to_rc "$PATH_CMD" "$HOME/.profile"
                 fi
-                append_to_rc "$ALIAS_CMD" ~/.zshrc
             else
-                alias keploy="$1"
-            fi
-        elif [[ "$current_shell" = "bash" || "$current_shell" = "-bash" ]]; then
-            if [ -f ~/.bashrc ]; then
-                if grep -q "alias keploy=" ~/.bashrc; then
-                    if [ "$OS_NAME" = "Darwin" ]; then
-                        sed -i '' '/alias keploy/d' ~/.bashrc
-                    else
-                        sed -i '/alias keploy/d' ~/.bashrc
-                    fi
-                fi
-                append_to_rc "$ALIAS_CMD" ~/.bashrc
-            else
-                alias keploy="$1"
+                return  # Exit the function and do nothing
             fi
         else
-            alias keploy="$1"
+            if [ "$NO_ROOT" = "true" ]; then
+                ALIAS_CMD="alias keploy='$HOME/.keploy/bin/keploy'"
+                if [[ "$current_shell" = "zsh" || "$current_shell" = "-zsh" ]]; then
+                    if grep -q "alias keploy=" "$HOME/.zshrc"; then
+                        sed -i '/alias keploy/d' "$HOME/.zshrc"
+                    fi
+                    append_to_rc "$ALIAS_CMD" "$HOME/.zshrc"
+                elif [[ "$current_shell" = "bash" || "$current_shell" = "-bash" ]]; then
+                    if grep -q "alias keploy=" "$HOME/.bashrc"; then
+                        sed -i '/alias keploy/d' "$HOME/.bashrc"
+                    fi
+                    append_to_rc "$ALIAS_CMD" "$HOME/.bashrc"
+                else\"$PATH\"
+                    if grep -q "alias keploy=" "$HOME/.profile"; then
+                        sed -i '/alias keploy/d' "$HOME/.profile"
+                    fi
+                    append_to_rc "$ALIAS_CMD" "$HOME/.profile"
+                fi
+            else
+                ALIAS_CMD="alias keploy='sudo -E env PATH="$PATH" keploy'"
+                # Handle zsh or bash for non-macOS systems
+                if [[ "$current_shell" = "zsh" || "$current_shell" = "-zsh" ]]; then
+                    if [ -f "$HOME/.zshrc" ]; then
+                        if grep -q "alias keploy=" "$HOME/.zshrc"; then
+                            sed -i '/alias keploy/d' "$HOME/.zshrc"
+                        fi
+                        append_to_rc "$ALIAS_CMD" ~/.zshrc
+                    else
+                        alias keploy="$ALIAS_CMD"
+                    fi
+                elif [[ "$current_shell" = "bash" || "$current_shell" = "-bash" ]]; then
+                    if [ -f "$HOME/.bashrc" ]; then
+                        if grep -q "alias keploy=" "$HOME/.bashrc"; then
+                            sed -i '/alias keploy/d' "$HOME/.bashrc"
+                        fi
+                        append_to_rc "$ALIAS_CMD" ~/.bashrc
+                    else
+                        alias keploy="$ALIAS_CMD"
+                    fi
+                else
+                    if [ -f "$HOME/.profile" ]; then
+                        if grep -q "alias keploy=" "$HOME/.profile"; then
+                            sed -i '/alias keploy/d' "$HOME/.profile"
+                        fi
+                        append_to_rc "$ALIAS_CMD" ~/.profile
+                    else
+                        alias keploy="$ALIAS_CMD"
+                    fi
+                fi
+
+            fi
         fi
     }
 
@@ -144,7 +232,12 @@ installKeploy (){
         tmp_files=("LICENSE" "README.md" "READMEes-Es.md" "README-UnitGen.md")
         for file in "${tmp_files[@]}"; do
             if [ -f "/tmp/$file" ]; then
-                sudo rm -rf "/tmp/$file"
+                if [ "$NO_ROOT" = "true" ]; then
+                    rm -rf "/tmp/$file"
+                else
+                    sudo rm -rf "/tmp/$file"
+                fi
+                
             fi
         done
     }
