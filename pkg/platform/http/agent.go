786955db185e73596325ae65a245d93400007830
Sarthak160 <rocksarthak45@gmail.com>
2024-10-09 01:56:00 +0530

    refactor: remove delay
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>

--- pkg/platform/http/agent.go
+++ pkg/platform/http/agent.go
@@ -604,7 +604,6 @@ func (a *AgentClient) Initcontainer(ctx context.Context, logger *zap.Logger, opt
 			return err
 		}
 	}
-	fmt.Println("INIT NAME", initFile.Name())
 	// Use the temporary file path in the Docker command
 	cmd := fmt.Sprintf("docker run --network=%s --name keploy-init --rm -v%s:/initStop.sh alpine /initStop.sh", a.conf.NetworkName, initFile.Name())
 
@@ -625,7 +624,6 @@ func (a *AgentClient) Initcontainer(ctx context.Context, logger *zap.Logger, opt
 		return nil
 	})
 
-	time.Sleep(7 * time.Second)
 	// Get the PID of the container's first process
 	inspect, err := a.dockerClient.ContainerInspect(ctx, "keploy-init")
 	if err != nil {
