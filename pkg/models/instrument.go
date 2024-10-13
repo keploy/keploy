d523404960e7d2b133bc9f3d3c95aab57611060a
Sarthak160 <rocksarthak45@gmail.com>
2024-10-14 01:54:45 +0530

    refator: ignore dstcfg from marshalling
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>

--- pkg/models/instrument.go
+++ pkg/models/instrument.go
@@ -18,10 +18,10 @@ type OutgoingOptions struct {
 	Rules         []config.BypassRule
 	MongoPassword string
 	// TODO: role of SQLDelay should be mentioned in the comments.
-	SQLDelay       time.Duration // This is the same as Application delay.
-	FallBackOnMiss bool          // this enables to pass the request to the actual server if no mock is found during test mode.
-	Mocking        bool          // used to enable/disable mocking
-	DstCfg         *ConditionalDstCfg
+	SQLDelay       time.Duration      // This is the same as Application delay.
+	FallBackOnMiss bool               // this enables to pass the request to the actual server if no mock is found during test mode.
+	Mocking        bool               // used to enable/disable mocking
+	DstCfg         *ConditionalDstCfg `json:"-"`
 }
 
 type ConditionalDstCfg struct {
