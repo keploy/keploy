7635bfe1f0a670f43cc8176f101635ab27d65e1b
Yaten <yaten598@gmail.com>
2024-10-22 23:35:09 +0530

    feat: improved install command
    Signed-off-by: Yaten <yaten598@gmail.com>

--- pkg/platform/coverage/csharp/utils.go
+++ pkg/platform/coverage/csharp/utils.go
@@ -5,10 +5,35 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func downloadDotnetCoverage(ctx context.Context) error {
-	args := []string{
+	// Consruct arguments for command to check if dotnet-coverage is already installed or not.
+	check_args := []string{
+		"dotnet",
+		"tool",
+		"list",
+		"-g",
+	}
+
+	checkCmd := exec.CommandContext(ctx, check_args[0], check_args[1:]...)
+	checkCmd.Stdout = os.Stdout
+	checkCmd.Stderr = os.Stderr
+
+	if err := checkCmd.Run(); err != nil {
+		return fmt.Errorf("failed to check for existing dotnet-coverage: %w", err)
+	} else {
+		if strings.Contains(checkCmd.String(), "dotnet-coverage") {
+			fmt.Println("dotnet-coverage is already installed")
+			return nil
+		}
+	}
+
+	fmt.Println("dotnet-coverage not found. Installing...")
+
+	// Construct the command arguments to install dotnet-coverage
+	installArgs := []string{
 		"dotnet",
 		"tool",
 		"install",
@@ -16,12 +41,12 @@ func downloadDotnetCoverage(ctx context.Context) error {
 		"dotnet-coverage",
 	}
 
-	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	installCmd := exec.CommandContext(ctx, installArgs[0], installArgs[1:]...)
+	installCmd.Stdout = os.Stdout
+	installCmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("failed to install dotnet-coverage: %w", err)
+	if err := installCmd.Run(); err != nil {
+		return fmt.Errorf("failed to install dotnet-coverage. Ensure .NET SDK is installed and try again: %w", err)
 	}
 
 	return nil
