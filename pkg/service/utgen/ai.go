6a7c8e7154d663fce79e41a86b346f17bb2de4d1
Shivam Sourav Jha <60891544+shivamsouravjha@users.noreply.github.com>
2024-11-07 12:30:46 +0530

    chore: add omit empty for max_completion_tokens (#2400)
    * chore: update call for ut
    
    Signed-off-by: shivamsouravjha <2019145@iiitdmj.ac.in>
    
    * remove: print statement
    
    Signed-off-by: shivamsouravjha <2019145@iiitdmj.ac.in>
    
    * chore: add iteration count to ai call
    
    Signed-off-by: shivamsouravjha <2019145@iiitdmj.ac.in>
    
    * chore: add omit empty for max_completion_tokens
    
    Signed-off-by: shivamsouravjha <2019145@iiitdmj.ac.in>
    
    ---------
    
    Signed-off-by: shivamsouravjha <2019145@iiitdmj.ac.in>

--- pkg/service/utgen/ai.go
+++ pkg/service/utgen/ai.go
@@ -39,7 +39,7 @@ type CompletionParams struct {
 	Model               string    `json:"model"`
 	Messages            []Message `json:"messages"`
 	MaxTokens           int       `json:"max_tokens,omitempty"`
-	MaxCompletionTokens int       `json:"max_completion_tokens"`
+	MaxCompletionTokens int       `json:"max_completion_tokens,omitempty"`
 	Stream              *bool     `json:"stream,omitempty"`
 	Temperature         float32   `json:"temperature,omitempty"`
 }
