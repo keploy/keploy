632ca9fa63dd506d91e291c69cbc7523977170f6
ayush3160 <kshitij3160@gmail.com>
2024-11-18 23:08:01 +0530

    check python application logs
    Signed-off-by: ayush3160 <kshitij3160@gmail.com>

--- .github/workflows/test_workflow_scripts/python-linux.sh
+++ .github/workflows/test_workflow_scripts/python-linux.sh
@@ -33,6 +33,7 @@ send_request(){
         if curl -X GET http://127.0.0.1:8000/; then
             app_started=true
         fi
+        cat "flaskApp_1.txt"
         sleep 3 # wait for 3 seconds before checking again.
     done
     echo "App started"
