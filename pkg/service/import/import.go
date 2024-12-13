4f83fc295528705af9b3ae82f33d8cda114ade97
Ayush Sharma <kshitij3160@gmail.com>
2024-12-13 16:06:44 +0530

    fix: import for collection without folders
    Signed-off-by: Ayush Sharma <kshitij3160@gmail.com>

--- pkg/service/import/import.go
+++ pkg/service/import/import.go
@@ -23,8 +23,13 @@ type PostmanCollection struct {
 		Name      string `json:"name"`
 		Schema    string `json:"schema"`
 	} `json:"info"`
-	Items     []PostmanItem            `json:"item"`
+	Items     json.RawMessage          `json:"item"`
 	Variables []map[string]interface{} `json:"variable"`
+}
+
+type ItemsContainer struct {
+	PostmanItems  []PostmanItem
+	TestDataItems []TestData
 }
 
 type PostmanItem struct {
@@ -62,6 +67,30 @@ type PostmanResponse struct {
 	Header          []map[string]string `json:"header"`
 }
 
+func (ic *ItemsContainer) UnmarshalJSON(data []byte) error {
+	var postmanItems []PostmanItem
+	if err := json.Unmarshal(data, &postmanItems); err == nil {
+		ic.PostmanItems = postmanItems
+	}
+
+	var items []TestData
+
+	if err := json.Unmarshal(data, &items); err != nil {
+		return err
+	}
+
+	ic.TestDataItems = items
+
+	return nil
+}
+
+func (ic ItemsContainer) MarshalJSON() ([]byte, error) {
+	if len(ic.PostmanItems) > 0 {
+		return json.Marshal(ic.PostmanItems)
+	}
+	return json.Marshal(ic.TestDataItems)
+}
+
 const Schema = "https://schema.getpostman.com/json/collection/v2.0.0/collection.json"
 
 func Import(ctx context.Context, logger *zap.Logger, path string) error {
@@ -79,7 +108,16 @@ func Import(ctx context.Context, logger *zap.Logger, path string) error {
 		return fmt.Errorf("failed to read file: %w", err)
 	}
 
-	var postmanCollection PostmanCollection
+	var postmanCollection struct {
+		Info struct {
+			PostmanID string `json:"_postman_id"`
+			Name      string `json:"name"`
+			Schema    string `json:"schema"`
+		} `json:"info"`
+		Items     ItemsContainer           `json:"item"`
+		Variables []map[string]interface{} `json:"variable"`
+	}
+
 	if err := json.Unmarshal(postmanCollectionBytes, &postmanCollection); err != nil {
 		return fmt.Errorf("failed to unmarshal json: %w", err)
 	}
@@ -88,13 +126,84 @@ func Import(ctx context.Context, logger *zap.Logger, path string) error {
 		logger.Warn("Postman Collection schema mismatch: expected v2.0.0", zap.String("schema", postmanCollection.Info.Schema))
 	}
 
-	for index, item := range postmanCollection.Items {
+	if len(postmanCollection.Items.PostmanItems) > 0 {
+		for index, item := range postmanCollection.Items.PostmanItems {
+			if item.Name == "" {
+				item.Name = fmt.Sprintf("test-set-%d", index+1)
+			}
 
-		if item.Name == "" {
-			item.Name = fmt.Sprintf("test-set-%d", index+1)
+			testSetName := item.Name
+
+			cwd, err := os.Getwd()
+
+			if err != nil {
+				return fmt.Errorf("failed to get current working directory: %w", err)
+			}
+
+			testSetPath := filepath.Join(cwd, "keploy", testSetName)
+
+			testsPath := filepath.Join(testSetPath, "tests")
+
+			globalVariables := make(map[string]string)
+
+			for _, variable := range postmanCollection.Variables {
+
+				if variable["disabled"] != nil && variable["disabled"].(bool) {
+					continue
+				}
+
+				variableKey, ok := variable["key"].(string)
+				if !ok {
+					logger.Error("global variable key is not a string", zap.Any("key", variable["key"]))
+					continue
+				}
+
+				variableValue, ok := variable["value"].(string)
+				if !ok {
+					logger.Error("global variable value is not a string", zap.Any("value", variable["value"]))
+					continue
+				}
+
+				globalVariables[variableKey] = variableValue
+			}
+
+			// Reiterating again if global variables values are also dependent on other global variables
+			for key, value := range globalVariables {
+				globalVariables[key] = ReplaceTemplateVars(value, globalVariables)
+			}
+
+			testCounter := 0
+
+			for _, testItem := range item.Item {
+				if len(testItem.Response) == 0 {
+					continue
+				}
+
+				err := WriteTestData(ctx, logger, testItem, testsPath, globalVariables, &testCounter)
+
+				if err != nil {
+					return fmt.Errorf("failed to write test data: %w", err)
+				}
+
+			}
 		}
+	}
 
-		testSetName := item.Name
+	if len(postmanCollection.Items.TestDataItems) > 0 {
+
+		maxTestSetNumber := 0
+		files, err := os.ReadDir(filepath.Join("keploy"))
+		if err == nil {
+			for _, file := range files {
+				if file.IsDir() && strings.HasPrefix(file.Name(), "test-set-") {
+					var testSetNumber int
+					_, err := fmt.Sscanf(file.Name(), "test-set-%d", &testSetNumber)
+					if err == nil && testSetNumber > maxTestSetNumber {
+						maxTestSetNumber = testSetNumber
+					}
+				}
+			}
+		}
 
 		cwd, err := os.Getwd()
 
@@ -102,11 +211,11 @@ func Import(ctx context.Context, logger *zap.Logger, path string) error {
 			return fmt.Errorf("failed to get current working directory: %w", err)
 		}
 
+		testSetName := fmt.Sprintf("test-set-%d", maxTestSetNumber+1)
+
 		testSetPath := filepath.Join(cwd, "keploy", testSetName)
 
 		testsPath := filepath.Join(testSetPath, "tests")
-
-		testCounter := 0
 
 		globalVariables := make(map[string]string)
 
@@ -136,58 +245,70 @@ func Import(ctx context.Context, logger *zap.Logger, path string) error {
 			globalVariables[key] = ReplaceTemplateVars(value, globalVariables)
 		}
 
-		for _, testItem := range item.Item {
-			// If there is no response, skip the test will need to check if this should be the desired behavior
+		testCounter := 0
+
+		for _, testItem := range postmanCollection.Items.TestDataItems {
 			if len(testItem.Response) == 0 {
 				continue
 			}
 
-			requestSchema := ConstructRequest(testItem.Request, globalVariables)
+			err := WriteTestData(ctx, logger, testItem, testsPath, globalVariables, &testCounter)
 
-			for _, response := range testItem.Response {
-
-				testName := fmt.Sprintf("test-%d", testCounter+1)
-
-				if response.OriginalRequest != nil {
-					requestSchema = ConstructRequest(*response.OriginalRequest, globalVariables)
-				}
-
-				responseSchema := ConstructResponse(response)
-
-				testCase := &yaml.NetworkTrafficDoc{
-					Version: models.GetVersion(),
-					Kind:    models.Kind("HTTP"),
-					Name:    testItem.Name,
-				}
-
-				err := testCase.Spec.Encode(&models.HTTPSchema{
-					Request:  requestSchema,
-					Response: responseSchema,
-				})
-
-				if err != nil {
-					return fmt.Errorf("failed to encode test case: %w", err)
-				}
-
-				testCase.Curl = pkg.MakeCurlCommand(requestSchema)
-
-				testResultBytes, err := yamlLib.Marshal(testCase)
-				if err != nil {
-					return fmt.Errorf("failed to marshal test result: %w", err)
-				}
-
-				err = yaml.WriteFile(ctx, logger, testsPath, testName, testResultBytes, false)
-
-				if err != nil {
-					return fmt.Errorf("failed to write test result: %w", err)
-				}
-
-				testCounter++
+			if err != nil {
+				return fmt.Errorf("failed to write test data: %w", err)
 			}
+
 		}
 	}
 
 	fmt.Println("✅ Postman Collection Successfully Imported To Keploy Tests 🎉")
+
+	return nil
+}
+
+func WriteTestData(ctx context.Context, logger *zap.Logger, testItem TestData, testsPath string, globalVariables map[string]string, testCounter *int) error {
+	requestSchema := ConstructRequest(testItem.Request, globalVariables)
+
+	for _, response := range testItem.Response {
+
+		testName := fmt.Sprintf("test-%d", *testCounter+1)
+
+		if response.OriginalRequest != nil {
+			requestSchema = ConstructRequest(*response.OriginalRequest, globalVariables)
+		}
+
+		responseSchema := ConstructResponse(response)
+
+		testCase := &yaml.NetworkTrafficDoc{
+			Version: models.GetVersion(),
+			Kind:    models.Kind("HTTP"),
+			Name:    testItem.Name,
+		}
+
+		err := testCase.Spec.Encode(&models.HTTPSchema{
+			Request:  requestSchema,
+			Response: responseSchema,
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to encode test case: %w", err)
+		}
+
+		testCase.Curl = pkg.MakeCurlCommand(requestSchema)
+
+		testResultBytes, err := yamlLib.Marshal(testCase)
+		if err != nil {
+			return fmt.Errorf("failed to marshal test result: %w", err)
+		}
+
+		err = yaml.WriteFile(ctx, logger, testsPath, testName, testResultBytes, false)
+
+		if err != nil {
+			return fmt.Errorf("failed to write test result: %w", err)
+		}
+
+		(*testCounter)++
+	}
 
 	return nil
 }
