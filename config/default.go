4983e3d0c2d38e684c0c794de7e9b322370fefa5
Vibhor Phalke <vibgitcode27@gmail.com>
2024-12-21 16:35:39 +0530

    Revert unnecessary changes and retain 'skipCoverage' update in Keploy config
    Signed-off-by: Vibhor Phalke <vibgitcode27@gmail.com>

--- config/default.go
+++ config/default.go
@@ -10,6 +10,8 @@ import (
 // defaultConfig is a variable to store the default configuration of the Keploy CLI. It is not a constant because enterprise need update the default configuration.
 var defaultConfig = `
 path: ""
+appId: 0
+appName: ""
 command: ""
 port: 0
 proxyPort: 16789
@@ -35,12 +37,20 @@ test:
   mongoPassword: "default@123"
   language: ""
   removeUnusedMocks: false
+  basePath: ""
+  mocking: true
+  disableLineCoverage: false
+  fallbackOnMiss: false
+  disableMockUpload: true
 record:
   recordTimer: 0s
   filters: []
+contract:
+  driven: "consumer"
+  servicesMapping: {}
+  self: "s1"
 configPath: ""
 bypassRules: []
-cmdType: "native"
 `
 
 func GetDefaultConfig() string {
@@ -55,6 +65,8 @@ const InternalConfig = `
 enableTesting: false
 keployContainer: "keploy-v2"
 keployNetwork: "keploy-network"
+inDocker: false
+cmdType: "native"
 `
 
 var config = &Config{}
