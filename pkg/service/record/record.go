538844a2945ace1d61d47dd74a0f3ed1c39c0a47
charankamarapu <kamarapucharan@gmail.com>
2024-12-24 13:38:34 +0530

    fix: run docker compose app as child process
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- pkg/service/record/record.go
+++ pkg/service/record/record.go
@@ -222,7 +222,7 @@ func (r *Recorder) Start(ctx context.Context, reRecord bool) error {
 			if appErr.Err == nil && appErr.AppErrorType == "" && utils.FindDockerCmd(r.config.Command) == utils.DockerCompose {
 				stopReason = "docker-compose exited successfully, hence stopping keploy"
 				return nil
-			} 
+			}
 			stopReason = "unknown error recieved from application, hence stopping keploy"
 		}
 
