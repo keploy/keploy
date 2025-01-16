fcaf65bb4bf2cb787ca8e9cf08e9f5c8bb225f4c
Sarthak160 <rocksarthak45@gmail.com>
2025-01-16 18:27:59 +0530

    chore: remove pprof
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 	//pprof for debugging
 	"net/http"
-	_ "net/http/pprof"
+	// _ "net/http/pprof"
 )
 
 // version is the version of the server and will be injected during build by ldflags, same with dsn
@@ -30,14 +30,14 @@ var gitHubClientID = "Iv23liFBvIVhL29i9BAp"
 
 func main() {
 	// Uncomment the following code to enable pprof for debugging
-	go func() {
-		fmt.Println("Starting pprof server for debugging...")
-		err := http.ListenAndServe("0.0.0.0:6060", nil)
-		if err != nil {
-			fmt.Println("Failed to start the pprof server for debugging", err)
-			return
-		}
-	}()
+	// go func() {
+	// 	fmt.Println("Starting pprof server for debugging...")
+	// 	err := http.ListenAndServe("0.0.0.0:6060", nil)
+	// 	if err != nil {
+	// 		fmt.Println("Failed to start the pprof server for debugging", err)
+	// 		return
+	// 	}
+	// }()
 	setVersion()
 	ctx := utils.NewCtx()
 	start(ctx)
