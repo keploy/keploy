a5e3b7c8eb4e794ff7f35660acb294fe2eab55fe
dhruv <dhruvj797@gmail.com>
2025-03-21 17:30:30 +0530

    added test workflow for http-pokeapi
    Signed-off-by: dhruv <dhruvj797@gmail.com>

---
+++ .github/workflows/test_workflow_scripts/golang-http-linux.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+source ./../../.github/workflows/test_workflow_scripts/test-iid.sh
+
+# Checkout a different branch
+git fetch origin
+git checkout native-linux
+
+# Check if there is a keploy-config file, if there is, delete it.
+if [ -f "./keploy.yml" ]; then
+    rm ./keploy.yml
+fi
+
+rm -rf keploy/
+
+# Generate the keploy-config file.
+sudo ./../../keployv2 config --generate
+
+# Update the global noise to updated_at.
+config_file="./keploy.yml"
+sed -i 's/global: {}/global: {"body": {"updated_at":[]}}/' "$config_file"
+
+# Build go binary
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o http-pokeapi
+
+send_request() {
+    local index=$1  
+
+    sleep 10
+    app_started=false
+    while [ "$app_started" = false ]; do
+        if curl -X GET http://localhost:8080/api/locations; then
+            app_started=true
+        fi
+        sleep 3
+    done
+    
+    echo "App started"
+    response=$(curl -s -X GET http://localhost:8080/api/locations)
+
+    # Extract any location from the reponse
+    location=$(echo "$response" | jq -r '.location[$index]')
+
+    response=$(curl -s -X GET http://localhost:8080/api/locations/$location)
+
+    # Extract any pokemon from the response
+    pokemon=$(echo "$response" | jq -r '.[$index]')
+
+    curl -s -X GET http://localhost:8080/api/locations/pokemon/$pokemon
+
+    # Wait for 10 seconds for Keploy to record the tcs and mocks.
+    sleep 10
+    pid=$(pgrep keploy)
+    echo "$pid Keploy PID"
+    echo "Killing Keploy"
+    sudo kill $pid
+}
+
+for i in {1..2}; do
+    app_name="http-pokeapi_${i}"
+    send_request i &
+    sudo -E env PATH="$PATH" ./../../keployv2 record -c "./http-pokeapi" --generateGithubActions=false &> "${app_name}.txt"
+    if grep "ERROR" "${app_name}.txt"; then
+        echo "Error found in pipeline..."
+        cat "${app_name}.txt"
+        exit 1
+    fi
+    if grep "WARNING: DATA RACE" "${app_name}.txt"; then
+      echo "Race condition detected in recording, stopping pipeline..."
+      cat "${app_name}.txt"
+      exit 1
+    fi
+    sleep 5
+    wait
+    echo "Recorded test case and mocks for iteration ${i}"
+done
+
+# Start the go-http app in test mode.
+sudo -E env PATH="$PATH" ./../../keployv2 test -c "./http-pokeapi" --delay 7 --generateGithubActions=false &> test_logs.txt
+
+if grep "ERROR" "test_logs.txt"; then
+    echo "Error found in pipeline..."
+    cat "test_logs.txt"
+    exit 1
+fi
+
+if grep "WARNING: DATA RACE" "test_logs.txt"; then
+    echo "Race condition detected in test, stopping pipeline..."
+    cat "test_logs.txt"
+    exit 1
+fi
+
+all_passed=true
+
+# Get the test results from the testReport file.
+for i in {0..1}
+do
+    # Define the report file for each test set
+    report_file="./keploy/reports/test-run-0/test-set-$i-report.yaml"
+
+    # Extract the test status
+    test_status=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
+
+    # Print the status for debugging
+    echo "Test status for test-set-$i: $test_status"
+
+    # Check if any test set did not pass
+    if [ "$test_status" != "PASSED" ]; then
+        all_passed=false
+        echo "Test-set-$i did not pass."
+        break # Exit the loop early as all tests need to pass
+    fi
+done
+
+# Check the overall test status and exit accordingly
+if [ "$all_passed" = true ]; then
+    echo "All tests passed"
+    exit 0
+else
+    cat "test_logs.txt"
+    exit 1
+fi
