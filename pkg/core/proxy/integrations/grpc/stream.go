8f924f06d9834b461f7f5cca65c4fb6650496748
gouravkrosx <gouravgreatkr@gmail.com>
2025-04-09 15:30:58 +0530

    fix: lint
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- pkg/core/proxy/integrations/grpc/stream.go
+++ pkg/core/proxy/integrations/grpc/stream.go
@@ -4,12 +4,8 @@ package grpc
 
 import (
 	"context"
-	"encoding/binary"
-	"fmt"
 	"sync"
 	"time"
-
-	"github.com/protocolbuffers/protoscope"
 
 	"go.keploy.io/server/v2/pkg"
 	"go.keploy.io/server/v2/pkg/models"
@@ -138,46 +134,4 @@ func (sic *StreamInfoCollection) ResetStream(streamID uint32) {
 	defer sic.mutex.Unlock()
 
 	delete(sic.StreamInfo, streamID)
-}
-
-func createLengthPrefixedMessageFromPayload(data []byte) models.GrpcLengthPrefixedMessage {
-	msg := models.GrpcLengthPrefixedMessage{}
-
-	// If the body is not length prefixed, we return the default value.
-	if len(data) < 5 {
-		return msg
-	}
-
-	// The first byte is the compression flag.
-	msg.CompressionFlag = uint(data[0])
-
-	// The next 4 bytes are message length.
-	msg.MessageLength = binary.BigEndian.Uint32(data[1:5])
-
-	// The payload could be empty. We only parse it if it is present.
-	if len(data) >= 5 {
-		// Use protoscope to decode the message.
-		msg.DecodedData = protoscope.Write(data[5:], protoscope.WriterOptions{})
-	}
-
-	return msg
-}
-
-func createPayloadFromLengthPrefixedMessage(msg models.GrpcLengthPrefixedMessage) ([]byte, error) {
-	scanner := protoscope.NewScanner(msg.DecodedData)
-	encodedData, err := scanner.Exec()
-	if err != nil {
-		return nil, fmt.Errorf("could not encode grpc msg using protoscope: %v", err)
-	}
-
-	// Note that the encoded length is present in the msg, but it is also equal to the len of encodedData.
-	// We should give the preference to the length of encodedData, since the mocks might have been altered.
-
-	// Reserve 1 byte for compression flag, 4 bytes for length capture.
-	payload := make([]byte, 1+4)
-	payload[0] = uint8(msg.CompressionFlag)
-	binary.BigEndian.PutUint32(payload[1:5], uint32(len(encodedData)))
-	payload = append(payload, encodedData...)
-
-	return payload, nil
 }
