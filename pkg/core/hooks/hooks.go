04b717f5752d8e681e3cf1546b25c2d765384ab3
Hermione Dadheech <hermionedadheech@gmail.com>
2025-04-09 18:47:58 +0530

    fix
    Signed-off-by: Hermione Dadheech <hermionedadheech@gmail.com>

--- pkg/core/hooks/hooks.go
+++ pkg/core/hooks/hooks.go
@@ -137,7 +137,7 @@ func (h *Hooks) load(ctx context.Context, opts core.HookCfg) error {
 		var ve *ebpf.VerifierError
 		if errors.As(err, &ve) {
 			errString := strings.Join(ve.Log, "\n")
-			h.logger.Debug("verifier log: ", zap.String("err", errString))
+			h.logger.Info("verifier log: ", zap.String("err", errString))
 		}
 		utils.LogError(h.logger, err, "failed to load eBPF objects")
 		return err
