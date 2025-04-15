59a2bf1442ff0954a3e5a0565e61037e066bf786
Per0x1de-1337 <baviskarhd99@gmail.com>
2025-04-15 23:24:22 +0530

    handle errors
    Signed-off-by: Per0x1de-1337 <baviskarhd99@gmail.com>

--- pkg/core/proxy/proxy.go
+++ pkg/core/proxy/proxy.go
@@ -261,7 +261,7 @@ func (p *Proxy) start(ctx context.Context, readyChan chan<- error) error {
 					return
 				}
 				utils.LogError(p.logger, err, "failed to accept connection to the proxy")
-				errCh <- nil
+				errCh <- err
 				return
 			}
 			clientConnCh <- conn
@@ -269,7 +269,7 @@ func (p *Proxy) start(ctx context.Context, readyChan chan<- error) error {
 		select {
 		case <-ctx.Done():
 			return nil
-		case <-errCh:
+		case err:= <-errCh:
 			return err
 		// handle the client connection
 		case clientConn := <-clientConnCh:
