b315f9abd0f172fed192322be0ee1d885ac84703
Akash Kumar <meakash7902@gmail.com>
2025-04-21 22:16:04 +0530

    fix: attempting sort order fix
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- pkg/core/proxy/mockmanager.go
+++ pkg/core/proxy/mockmanager.go
@@ -32,9 +32,7 @@ func NewMockManager(filtered, unfiltered *TreeDb, logger *zap.Logger) *MockManag
 func (m *MockManager) SetFilteredMocks(mocks []*models.Mock) {
 	m.filtered.deleteAll()
 	for index, mock := range mocks {
-		if mock.TestModeInfo.SortOrder == 0 {
-			mock.TestModeInfo.SortOrder = index
-		}
+		mock.TestModeInfo.SortOrder = index
 		mock.TestModeInfo.ID = index
 		m.filtered.insert(mock.TestModeInfo, mock)
 	}
@@ -43,9 +41,7 @@ func (m *MockManager) SetFilteredMocks(mocks []*models.Mock) {
 func (m *MockManager) SetUnFilteredMocks(mocks []*models.Mock) {
 	m.unfiltered.deleteAll()
 	for index, mock := range mocks {
-		if mock.TestModeInfo.SortOrder == 0 {
-			mock.TestModeInfo.SortOrder = index
-		}
+		mock.TestModeInfo.SortOrder = index
 		mock.TestModeInfo.ID = index
 		m.unfiltered.insert(mock.TestModeInfo, mock)
 	}
