5e32278e7c984827392942c96a0ae0e524931d7d
Akash Kumar <91385321+AkashKumar7902@users.noreply.github.com>
2025-05-05 14:15:08 +0530

    fix: wrong mock matching which results in testcase failure (#2678)
    * fix: wrong mock matching
    
    Before each testcase, we fetch the copy of mocks in our DS, due to this we are taking into account that some mocks may be already used, and this creates problem.
    
    Current mock matching flow, is to first filter out mocks based on req-resp timestamp, so if two requests are made asynchronously then the same set of mocks would be matched. Since after each testcase, we are not taking into account the consumed mocks, we may get a match to a mock which has already been used in a previous testcase, this may lead to failed testcase.
    To solve this, I used the already existing totalConsumedMocks set to filter out unused mocks before matching algorithm.
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    
    * postgres-parser: fixed the preparedStatementMatch logic to consider packet types which doesn't pass checkIfPs function
    
    the correct mock should be able to match in preparedStatementMatch, but it is not because the pgrequest packet headers is not of the form B E B E B E ..... , basically, we are checking if Bind(B) is trying to refer to a previously cached query (by referring it with an identifier), In other words, if Bind is not preceeded by a Parse(P), if B is not preceeded with a P then we can conclude that the B is trying to refer to an identifier which was previously cached in the previous pg request. that's the reason I believe what checkIfPS is trying to validate.
    
    but in preparedStatementMatch main for loop (match) logic, we are only letting pgrequests with PacketTypes which has all Bind statements trying to refer to a previously cached request (B E B E B E),
    
    But technically, if a single bind statement is trying to refer to the previously cached identifier we should let the pgrequests pass (move to the match logic loop), Like B E P B E should not be stopped at the gates of PreparedStatementMatch.
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    ---------
    
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>
    Co-authored-by: Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
    Co-authored-by: Yash Khare <khareyash05@gmail.com>

--- pkg/service/replay/service.go
+++ pkg/service/replay/service.go
@@ -17,7 +17,7 @@ type Instrumentation interface {
 	// SetMocks Allows for setting mocks between test runs for better filtering and matching
 	SetMocks(ctx context.Context, id uint64, filtered []*models.Mock, unFiltered []*models.Mock) error
 	// GetConsumedMocks to log the names of the mocks that were consumed during the test run of failed test cases
-	GetConsumedMocks(ctx context.Context, id uint64) ([]string, error)
+	GetConsumedMocks(ctx context.Context, id uint64) ([]models.MockState, error)
 	// Run is blocking call and will execute until error
 	Run(ctx context.Context, id uint64, opts models.RunOptions) models.AppError
 
@@ -52,7 +52,7 @@ type TestDB interface {
 type MockDB interface {
 	GetFilteredMocks(ctx context.Context, testSetID string, afterTime time.Time, beforeTime time.Time) ([]*models.Mock, error)
 	GetUnFilteredMocks(ctx context.Context, testSetID string, afterTime time.Time, beforeTime time.Time) ([]*models.Mock, error)
-	UpdateMocks(ctx context.Context, testSetID string, mockNames map[string]bool) error
+	UpdateMocks(ctx context.Context, testSetID string, mockNames map[string]models.MockState) error
 }
 
 type ReportDB interface {
@@ -91,11 +91,3 @@ type InstrumentState struct {
 	AppID      uint64
 	HookCancel context.CancelFunc
 }
-
-type MockAction string
-
-// MockAction constants define the possible actions that can be taken on a mocking.
-const (
-	Start  MockAction = "start"
-	Update MockAction = "update"
-)
