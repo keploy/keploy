09525d4978464d35baac4986074fc7cbb1e654c8
Hermione Dadheech <hermionedadheech@gmail.com>
2025-06-05 17:24:42 +0530

    feat: add AND/OR match logic to request filters in isFiltered
    Signed-off-by: Hermione Dadheech <hermionedadheech@gmail.com>

--- pkg/core/hooks/conn/util.go
+++ pkg/core/hooks/conn/util.go
@@ -60,42 +60,56 @@ func isFiltered(logger *zap.Logger, req *http.Request, opts models.IncomingOptio
 	passThrough := proxyHttp.IsPassThrough(logger, req, uint(dstPort), headerOpts)
 
 	for _, filter := range opts.Filters {
-		if len(filter.URLMethods) > 0 {
-			urlMethodMatch := false
-			for _, method := range filter.URLMethods {
-				if method == req.Method {
-					urlMethodMatch = true
-					break
-				}
-			}
-			passThrough = urlMethodMatch
-			if !passThrough {
-				continue
-			}
-		}
-		if len(filter.Headers) > 0 {
-			headerMatch := false
-			for filterHeaderKey, filterHeaderValue := range filter.Headers {
-				regex, err := regexp.Compile(filterHeaderValue)
-				if err != nil {
-					utils.LogError(logger, err, "failed to compile the header regex")
-					continue
-				}
-				if req.Header.Get(filterHeaderKey) != "" {
-					for _, value := range req.Header.Values(filterHeaderKey) {
-						headerMatch = regex.MatchString(value)
-						if headerMatch {
-							break
-						}
-					}
-				}
-				passThrough = headerMatch
-				if passThrough {
-					break
-				}
+	matchType := strings.ToUpper(filter.MatchType)
+
+	urlMethodMatch := len(filter.URLMethods) == 0
+	if len(filter.URLMethods) > 0 {
+		for _, method := range filter.URLMethods {
+			if method == req.Method {
+				urlMethodMatch = true
+				break
 			}
 		}
 	}
+
+	headerMatch := len(filter.Headers) == 0
+	if len(filter.Headers) > 0 {
+		for filterHeaderKey, filterHeaderValue := range filter.Headers {
+			regex, err := regexp.Compile(filterHeaderValue)
+			if err != nil {
+				utils.LogError(logger, err, "failed to compile the header regex")
+				continue
+			}
+			for _, value := range req.Header.Values(filterHeaderKey) {
+				if regex.MatchString(value) {
+					headerMatch = true
+					break
+				}
+			}
+			if headerMatch {
+				break
+			}
+		}
+	}
+
+	// Apply AND / OR logic
+	switch matchType {
+	case "AND":
+		if urlMethodMatch && headerMatch {
+			passThrough = true
+		}
+	case "OR":
+		if urlMethodMatch || headerMatch {
+			passThrough = true
+		}
+	default:
+		// default to OR if unspecified
+		if urlMethodMatch || headerMatch {
+			passThrough = true
+		}
+	}
+}
+
 
 	return passThrough
 }
