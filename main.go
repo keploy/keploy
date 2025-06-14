7a2e456a534b8599595df9a336cc735f878065b9
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-06-14 20:02:46 +0530

    Update main.go
    Signed-off-by: Arnav Kapoor <kapoorarnav43@gmail.com>

--- main.go
+++ main.go
@@ -34,7 +34,6 @@ func main() {
 	}()
 
 	setVersion()
-
 	ctx := utils.NewCtx()
 	err := start(ctx)
 	if err != nil {
@@ -61,39 +60,45 @@ func start(ctx context.Context) error {
 	defer func() {
 		if err := utils.DeleteFileIfNotExists(logger, "keploy-logs.txt"); err != nil {
 			utils.LogError(logger, err, "Failed to delete Keploy Logs")
+			return
 		}
 		if err := utils.DeleteFileIfNotExists(logger, "docker-compose-tmp.yaml"); err != nil {
 			utils.LogError(logger, err, "Failed to delete Temporary Docker Compose")
+			return
 		}
 	}()
 	defer utils.Recover(logger)
 
+	// The 'umask' command is commonly used in various operating systems to regulate the permissions of newly created files.
+	// These 'umask' values subtract from the permissions assigned by the process, effectively lowering the permissions.
+	// For example, if a file is created with permissions '777' and the 'umask' is '022', the resulting permissions will be '755',
+	// reducing certain permissions for security purposes.
+	// Setting 'umask' to '0' ensures that 'keploy' can precisely control the permissions of the files it creates.
+	// However, it's important to note that this approach may not work in scenarios involving mounted volumes,
+	// as the 'umask' is set by the host system, and cannot be overridden by 'keploy' or individual processes.
 	oldMask := utils.SetUmask()
 	defer utils.RestoreUmask(oldMask)
 
 	if dsn != "" {
 		utils.SentryInit(logger, dsn)
+		//logger = utils.ModifyToSentryLogger(ctx, logger, sentry.CurrentHub().Client(), configDb)
 	}
-
 	conf := config.New()
 	conf.APIServerURL = apiServerURI
 	conf.GitHubClientID = gitHubClientID
-
 	userDb := userDb.New(logger, conf)
 	conf.InstallationID, err = userDb.GetInstallationID(ctx)
 	if err != nil {
 		utils.LogError(logger, err, "Failed to get installation ID")
 		return err
 	}
-
 	auth := auth.New(conf.APIServerURL, conf.InstallationID, logger, conf.GitHubClientID)
 	svcProvider := provider.NewServiceProvider(logger, conf, auth)
 	cmdConfigurator := provider.NewCmdConfigurator(logger, conf)
 	rootCmd := cli.Root(ctx, logger, svcProvider, cmdConfigurator)
-
 	if err := rootCmd.Execute(); err != nil {
 		if strings.HasPrefix(err.Error(), "unknown command") || strings.HasPrefix(err.Error(), "unknown shorthand") {
-			fmt.Println("Error:", err.Error())
+			fmt.Println("Error: ", err.Error())
 			fmt.Println("Run 'keploy --help' for usage.")
 		}
 		return err
