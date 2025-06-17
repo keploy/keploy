fee5343e3192fc03cc611aa1ea9503fd684e7486
shiiyan <shiiyan79@gmail.com>
2025-06-17 22:03:50 +0900

    fix ask for confirmation to exit gracefully
    Signed-off-by: shiiyan <shiiyan79@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -286,27 +286,26 @@ var ConfigGuide = `
 
 // AskForConfirmation asks the user for confirmation. A user must type in "yes" or "no" and
 // then press enter. It has fuzzy matching, so "y", "Y", "yes", "YES", and "Yes" all count as
-// confirmations. If the input is not recognized, it will ask again. The function does not return
-// until it gets a valid response from the user.
+// confirmations. If the input is not recognized, exit gracefully as "no".
 func AskForConfirmation(s string) (bool, error) {
 	reader := bufio.NewReader(os.Stdin)
 
-	for {
-		fmt.Printf("%s [y/n]: ", s)
+	fmt.Printf("%s [y/n]: ", s)
 
-		response, err := reader.ReadString('\n')
-		if err != nil {
-			return false, err
-		}
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		return false, err
+	}
 
-		response = strings.ToLower(strings.TrimSpace(response))
+	response = strings.ToLower(strings.TrimSpace(response))
 
-		switch response {
-		case "y", "yes":
-			return true, nil
-		case "n", "no":
-			return false, nil
-		}
+	switch response {
+	case "y", "yes":
+		return true, nil
+	case "n", "no":
+		return false, nil
+	default:
+		return false, nil
 	}
 }
 
