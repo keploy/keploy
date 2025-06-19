b8069f13703e96638a26fec32105243cd8c41ea3
shiiyan <36617009+shiiyan@users.noreply.github.com>
2025-06-19 20:44:24 +0900

    fix: bug keploy config --generate gets stuck on invalid input when keploy.yml exists (#2772)
    * fix ask for confirmation to exit gracefully
    
    Signed-off-by: shiiyan <shiiyan79@gmail.com>
    ---------
    
    Signed-off-by: shiiyan <shiiyan79@gmail.com>
    Co-authored-by: Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
    Signed-off-by: Priyanshu Singh <s.priyanshu.coder@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -286,27 +286,36 @@ var ConfigGuide = `
 
 // AskForConfirmation asks the user for confirmation. A user must type in "yes" or "no" and
 // then press enter. It has fuzzy matching, so "y", "Y", "yes", "YES", and "Yes" all count as
-// confirmations. If the input is not recognized, it will ask again. The function does not return
-// until it gets a valid response from the user.
-func AskForConfirmation(s string) (bool, error) {
+// confirmations. If the input is not recognized or interrupted, exit gracefully as "no".
+func AskForConfirmation(ctx context.Context, s string) (bool, error) {
 	reader := bufio.NewReader(os.Stdin)
 
-	for {
-		fmt.Printf("%s [y/n]: ", s)
+	fmt.Printf("%s [y/n]: ", s)
 
+	respCh := make(chan string, 1)
+	errCh := make(chan error, 1)
+
+	go func() {
 		response, err := reader.ReadString('\n')
 		if err != nil {
-			return false, err
+			errCh <- err
+		} else {
+			respCh <- response
 		}
+	}()
 
+	select {
+	case <-ctx.Done():
+		// Cobra caught SIGINT (Ctrl+C) and cancelled its root context
+		return false, nil
+	case err := <-errCh:
+		return false, err
+	case response := <-respCh:
 		response = strings.ToLower(strings.TrimSpace(response))
-
-		switch response {
-		case "y", "yes":
+		if response == "y" || response == "yes" {
 			return true, nil
-		case "n", "no":
-			return false, nil
 		}
+		return false, nil
 	}
 }
 
