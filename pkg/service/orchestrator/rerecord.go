706b624d0732619aa9c8ccdb78959b088b201d4b
Sarthak160 <rocksarthak45@gmail.com>
2025-07-30 20:01:47 +0530

    chore: change the curl command log for re-record
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>

--- pkg/service/orchestrator/rerecord.go
+++ pkg/service/orchestrator/rerecord.go
@@ -291,7 +291,7 @@ func (o *Orchestrator) replayTests(ctx context.Context, testSet string) (bool, e
 			continue // Proceed with the next command
 		}
 
-		o.logger.Info("Re-recorded the testcase successfully", zap.String("curl", tc.Curl), zap.Any("response", (resp)))
+		o.logger.Info("Re-recorded the testcase successfully for the given testset", zap.String("testset", testSet), zap.String("testcase", tc.Name))
 	}
 
 	if simErr {
