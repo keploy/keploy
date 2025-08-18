0334439d1875c076483d9d678cf237e16868ede9
Kapish <upadhyaykapish@gmail.com>
2025-08-18 13:20:03 +0000

    chore: setting inode for keploy agent
    Signed-off-by: Kapish <upadhyaykapish@gmail.com>

--- pkg/core/hooks/hooks.go
+++ pkg/core/hooks/hooks.go
@@ -529,8 +529,7 @@ func (h *Hooks) load(ctx context.Context, opts core.HookCfg) error {
 	}
 
 	agentInfo.DNSPort = int32(h.dnsPort)
-	agentInfo.KeployAgentInode = clientInfo.KeployClientInode // Reuse the inode you already fetched
-	// agentInfo.KeployAgentNsPid = clientInfo.KeployClientNsPid
+	agentInfo.KeployAgentInode = clientInfo.KeployClientInode 
 	if opts.IsDocker {
 		clientInfo.IsDockerApp = uint32(1)
 	} else {
