4e16e625f6a7d09ea4c324736ee0f080de74c7da
Asish Kumar <officialasishkumar@gmail.com>
2025-08-21 16:10:47 +0530

    fix: go linter error
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- pkg/core/hooks/hooks.go
+++ pkg/core/hooks/hooks.go
@@ -118,7 +118,7 @@ func (h *Hooks) Load(ctx context.Context, id uint64, opts core.HookCfg) error {
 	g.Go(func() error {
 		defer utils.Recover(h.logger)
 		<-ctx.Done()
-		// Already done after every test set run, so we don't need to unload at the end since it is already done 
+		// Already done after every test set run, so we don't need to unload at the end since it is already done
 		// h.unLoad(ctx, opts)
 
 		//deleting in order to free the memory in case of rerecord.
