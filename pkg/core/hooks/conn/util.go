c635681dcfe8a2e9ea355445da84f2e6aa3a10cc
Sarthak <50234097+Sarthak160@users.noreply.github.com>
2025-08-27 02:13:28 +0530

    fix: skip reading req-body in case of no buffer (#2916)
    * fix: skip reading body in case of GET request
    
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>
    
    * fix: skip reading body in case of GET request
    
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>
    
    * fix: skip reading body in case of GET request
    
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>
    
    * Add generated tests by Keploy for PR #2916
    
    Signed-off-by: Keploy Bot <keploy-bot@keploy.io>
    
    * Revert "Add generated tests by Keploy for PR #2916\n\nSigned-off-by: Keploy Bot <keploy-bot@keploy.io>"
    
    This reverts commit c895c450ad6985ad6c27c041a8b39cef89529f5d
    
    ---------
    
    Signed-off-by: Sarthak160 <rocksarthak45@gmail.com>
    Signed-off-by: Keploy Bot <keploy-bot@keploy.io>
    Co-authored-by: Keploy Bot <keploy-bot@keploy.io>
    Signed-off-by: Priyanshu Singh <s.priyanshu.coder@gmail.com>

--- pkg/core/hooks/conn/util.go
+++ pkg/core/hooks/conn/util.go
@@ -162,17 +162,21 @@ func isFiltered(logger *zap.Logger, req *http.Request, opts models.IncomingOptio
 //}
 
 func Capture(_ context.Context, logger *zap.Logger, t chan *models.TestCase, req *http.Request, resp *http.Response, reqTimeTest time.Time, resTimeTest time.Time, opts models.IncomingOptions) {
-	reqBody, err := io.ReadAll(req.Body)
-	if err != nil {
-		utils.LogError(logger, err, "failed to read the http request body", zap.Int64("of size", int64(len(reqBody))), zap.String("body", base64.StdEncoding.EncodeToString(reqBody)))
-		return
-	}
 
-	if req.Header.Get("Content-Encoding") != "" {
-		reqBody, err = pkg.Decompress(logger, req.Header.Get("Content-Encoding"), reqBody)
+	var reqBody []byte
+	if req.Body != nil { // Read
+		var err error
+		reqBody, err = io.ReadAll(req.Body)
 		if err != nil {
-			utils.LogError(logger, err, "failed to decompress the request body", zap.Int64("of size", int64(len(reqBody))), zap.String("body", base64.StdEncoding.EncodeToString(reqBody)))
-			return
+			logger.Warn("failed to read the http request body", zap.Any("metadata", utils.GetReqMeta(req)), zap.Int64("of size", int64(len(reqBody))), zap.String("body", base64.StdEncoding.EncodeToString(reqBody)), zap.Error(err))
+		}
+
+		if req.Header.Get("Content-Encoding") != "" {
+			reqBody, err = pkg.Decompress(logger, req.Header.Get("Content-Encoding"), reqBody)
+			if err != nil {
+				utils.LogError(logger, err, "failed to decode the http request body", zap.Any("metadata", utils.GetReqMeta(req)))
+				return
+			}
 		}
 	}
 
