89015b000cfaf14cbeb54bfa0cd6dce564b85985
Prabal <prabaltripathiofficial@gmail.com>
2025-08-27 13:30:15 +0000

    chore: Fix Race Condition in Java-Linux Pipeline
    Signed-off-by: Prabal <prabaltripathiofficial@gmail.com>

--- pkg/core/proxy/integrations/postgres/v1/match.go
+++ pkg/core/proxy/integrations/postgres/v1/match.go
@@ -301,14 +301,14 @@ OuterLoop:
 
 			if matched {
 				logger.Debug("Matched mock", zap.String("mock", matchedMock.Name))
-				originalMatchedMock := *matchedMock
-				matchedMock.TestModeInfo.IsFiltered = false
-				matchedMock.TestModeInfo.SortOrder = pkg.GetNextSortNum()
-				updated := mockDb.UpdateUnFilteredMock(&originalMatchedMock, matchedMock)
+				finalMock := matchedMock.DeepCopy()
+				finalMock.TestModeInfo.IsFiltered = false
+                finalMock.TestModeInfo.SortOrder = pkg.GetNextSortNum()
+				updated := mockDb.UpdateUnFilteredMock(matchedMock, matchedMock)
 				if !updated {
 					logger.Debug("failed to update matched mock", zap.Error(err))
 				}
-				return true, matchedMock.Spec.PostgresResponses, nil
+				return true, finalMock.Spec.PostgresResponses, nil
 			}
 			return false, nil, nil
 		}
