b79d22769998800a74a7d40453bf50b1c4c1234c
Prabal <prabaltripathiofficial@gmail.com>
2025-08-29 11:21:52 +0000

    chore: trying to fix docker compose

--- .github/workflows/test_workflow_scripts/golang-docker-compose.sh
+++ .github/workflows/test_workflow_scripts/golang-docker-compose.sh
@@ -8,6 +8,8 @@ docker compose build
 # Remove any preexisting keploy tests and mocks.
 sudo rm -rf keploy/
 
+mkdir -p keploy/reports
+
 # Generate the keploy-config file.
 sudo -E env PATH=$PATH $RECORD_BIN config --generate
 
@@ -58,7 +60,7 @@ send_request(){
 for i in {1..2}; do
     container_name="echoApp"
     send_request &
-    sudo -E env PATH=$PATH $RECORD_BIN record -c "docker compose up" --container-name "$container_name" --generateGithubActions=false &> "${container_name}.txt"
+    sudo -E env PATH=$PATH $RECORD_BIN record -c "docker compose up" --container-name "$container_name" --generateGithubActions=false --language go &> "${container_name}.txt"
 
     if grep "WARNING: DATA RACE" "${container_name}.txt"; then
         echo "Race condition detected in recording, stopping pipeline..."
@@ -82,7 +84,7 @@ echo "Services stopped - Keploy should now use mocks for dependency interactions
 
 # Start keploy in test mode.
 test_container="echoApp"
-sudo -E env PATH=$PATH $REPLAY_BIN test -c 'docker compose up' --containerName "$test_container" --apiTimeout 60 --delay 20 --generate-github-actions=false --debug &> "${test_container}.txt"
+sudo -E env PATH=$PATH $REPLAY_BIN test -c 'docker compose up' --containerName "$test_container" --apiTimeout 60 --delay 20 --generate-github-actions=false --debug --language go &> "${test_container}.txt"
 
 if grep "ERROR" "${test_container}.txt"; then
     echo "Error found in pipeline..."
