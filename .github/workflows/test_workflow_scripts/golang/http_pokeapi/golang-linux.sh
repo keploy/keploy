bf55492463a72d0453d6b8365c961c48483a8581
Asish Kumar <officialasishkumar@gmail.com>
2025-09-16 07:47:01 +0000

    feat: add more testcases
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- .github/workflows/test_workflow_scripts/golang/http_pokeapi/golang-linux.sh
+++ .github/workflows/test_workflow_scripts/golang/http_pokeapi/golang-linux.sh
@@ -29,38 +29,37 @@ config_file="./keploy.yml"
 sed -i 's/global: {}/global: {"body": {"updated_at":[]}}/' "$config_file"
 
 send_request() {
-    local index=$1  
+    # Run 50 test cases per set
+    for index in {0..49}; do
+        sleep 10
+        app_started=false
+        while [ "$app_started" = false ]; do
+            if curl -X GET http://localhost:8080/api/locations; then
+                app_started=true
+            fi
+            sleep 3
+        done
+        
+        echo "App started for test case $index"
+        
+        response=$(curl -s -X GET http://localhost:8080/api/locations)
 
-    sleep 10
-    app_started=false
-    while [ "$app_started" = false ]; do
-        if curl -X GET http://localhost:8080/api/locations; then
-            app_started=true
-        fi
-        sleep 3
+        # Extract any location from the response
+        location=$(echo "$response" | jq -r ".location[$index]")
+        
+        response=$(curl -s -X GET http://localhost:8080/api/locations/$location)
+
+        # Extract any pokemon from the response
+        pokemon=$(echo "$response" | jq -r ".[$index]")
+        
+        curl -s -X GET http://localhost:8080/api/pokemon/$pokemon
+
+        curl -s -X GET http://localhost:8080/api/greet
+        curl -s -X GET http://localhost:8080/api/greet?format=html
+        curl -s -X GET http://localhost:8080/api/greet?format=xml
     done
-    
-    echo "App started"
-    
-    response=$(curl -s -X GET http://localhost:8080/api/locations)
 
-    # Extract any location from the reponse
-    location=$(echo "$response" | jq -r ".location[$index]")
-    
-    response=$(curl -s -X GET http://localhost:8080/api/locations/$location)
-
-    # Extract any pokemon from the response
-    pokemon=$(echo "$response" | jq -r ".[$index]")
-    
-    curl -s -X GET http://localhost:8080/api/pokemon/$pokemon
-
-    curl -s -X GET http://localhost:8080/api/greet
-
-    curl -s -X GET http://localhost:8080/api/greet?format=html
-
-    curl -s -X GET http://localhost:8080/api/greet?format=xml
-
-    # Wait for 10 seconds for Keploy to record the tcs and mocks.
+    # Wait for Keploy to record the tcs and mocks.
     sleep 10
     pid=$(pgrep keploy)
     echo "$pid Keploy PID"
@@ -68,7 +67,8 @@ send_request() {
     sudo kill $pid
 }
 
-for i in {1..2}; do
+# Run 100 test sets
+for i in {1..100}; do
     app_name="http-pokeapi_${i}"
     send_request $i &
     sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "./http-pokeapi" --generateGithubActions=false &> "${app_name}.txt"
@@ -105,26 +105,20 @@ fi
 all_passed=true
 
 # Get the test results from the testReport file.
-for i in {0..1}
+for i in {0..99}
 do
-    # Define the report file for each test set
     report_file="./keploy/reports/test-run-0/test-set-$i-report.yaml"
 
-    # Extract the test status
     test_status=$(grep 'status:' "$report_file" | head -n 1 | awk '{print $2}')
-
-    # Print the status for debugging
     echo "Test status for test-set-$i: $test_status"
 
-    # Check if any test set did not pass
     if [ "$test_status" != "PASSED" ]; then
         all_passed=false
         echo "Test-set-$i did not pass."
-        break # Exit the loop early as all tests need to pass
+        break
     fi
 done
 
-# Check the overall test status and exit accordingly
 if [ "$all_passed" = true ]; then
     echo "All tests passed"
     exit 0
