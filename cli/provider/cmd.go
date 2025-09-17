fa0f250967a4a27f510f7d805a8f5e435afc1b18
Ayush Sharma <kshitij3160@gmail.com>
2025-09-17 14:21:12 +0530

    feat: change flag name to amend testset
    Signed-off-by: Ayush Sharma <kshitij3160@gmail.com>

--- cli/provider/cmd.go
+++ cli/provider/cmd.go
@@ -244,7 +244,7 @@ func (c *CmdConfigurator) AddFlags(cmd *cobra.Command) error {
 		}
 
 		cmd.Flags().Bool("global-passthrough", false, "Allow all outgoing calls to be mocked if set to true")
-		cmd.Flags().Bool("create-test-set", false, "For creating a fresh test-set for each test-set during rerecording. By default it is false")
+		cmd.Flags().Bool("amend-testset", false, "For updating the current test-set for each test-set during rerecording. By default it is false")
 		cmd.Flags().StringP("path", "p", ".", "Path to local directory where generated testcases/mocks are stored")
 		cmd.Flags().Uint32("proxy-port", c.cfg.ProxyPort, "Port used by the Keploy proxy server to intercept the outgoing dependency calls")
 		cmd.Flags().Uint32("dns-port", c.cfg.DNSPort, "Port used by the Keploy DNS server to intercept the DNS queries")
@@ -739,6 +739,11 @@ func (c *CmdConfigurator) ValidateFlags(ctx context.Context, cmd *cobra.Command)
 		}
 	case "record", "test", "rerecord":
 
+		if cmd.Name() == "rerecord" {
+			updateTestSet, _ := cmd.Flags().GetBool("amend-testset")
+			c.cfg.ReRecord.CreateTestSet = !updateTestSet
+		}
+
 		if cmd.Parent() != nil && cmd.Parent().Name() == "contract" {
 			path, err := cmd.Flags().GetString("path")
 			if err != nil {
@@ -1049,9 +1054,6 @@ func (c *CmdConfigurator) ValidateFlags(ctx context.Context, cmd *cobra.Command)
 				}
 			}
 		}
-
-		createTestSet, _ := cmd.Flags().GetBool("create-test-set") //this flag is only for rerecord command
-		c.cfg.ReRecord.CreateTestSet = createTestSet               //hence ignoring error
 
 		globalPassthrough, _ := cmd.Flags().GetBool("global-passthrough")
 		c.cfg.Record.GlobalPassthrough = globalPassthrough //hence ignoring error
