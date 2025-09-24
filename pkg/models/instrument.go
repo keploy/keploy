33321221cb5c734bd271a140de64dccb984abda8
Kapish Upadhyay <upadhyaykapish@gmail.com>
2025-09-24 23:30:10 +0530

    chore: removed persister
    Signed-off-by: Kapish Upadhyay <upadhyaykapish@gmail.com>

--- pkg/models/instrument.go
+++ pkg/models/instrument.go
@@ -9,13 +9,7 @@ import (
 )
 
 // TestCasePersister defines the function signature for saving a TestCase.
-// By placing it in the models package, both the core and proxy packages can
-// reference it without creating a circular dependency.
 type TestCasePersister func(ctx context.Context, testCase *TestCase) error
-
-// type ProxyDependencies struct {
-// 	Persister TestCasePersister
-// }
 
 type HookOptions struct {
 	Rules         []config.BypassRule
