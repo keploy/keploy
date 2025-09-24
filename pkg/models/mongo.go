5e502f8bb0d644ec6b5cb3b55a9e995620b37885
Ayush Sharma <kshitij3160@gmail.com>
2025-09-24 16:18:37 +0530

    chore: add the unmarshal bson for mongo back
    Signed-off-by: Ayush Sharma <kshitij3160@gmail.com>

--- pkg/models/mongo.go
+++ pkg/models/mongo.go
@@ -2,8 +2,11 @@ package models
 
 import (
 	"encoding/gob"
+	"encoding/json"
+	"errors"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/wiremessage"
 	"gopkg.in/yaml.v3"
 )
@@ -75,4 +78,215 @@ func init() {
 	gob.Register(&MongoOpMessage{})
 	gob.Register(&MongoOpQuery{})
 	gob.Register(&MongoOpReply{})
+}
+
+// UnmarshalBSON implements bson.Unmarshaler for mongoRequests because of interface typeof field
+func (mr *MongoRequest) UnmarshalBSON(data []byte) error {
+
+	// duplicate struct to avoid infinite recursion
+	type MongoRequestAlias struct {
+		Header    *MongoHeader `bson:"header,omitempty"`
+		Message   bson.Raw     `bson:"message,omitempty"`
+		ReadDelay int64        `bson:"read_delay,omitempty"`
+	}
+	var aux MongoRequestAlias
+
+	if err := bson.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// assign the unmarshalled data to the original data
+	mr.Header = aux.Header
+	mr.ReadDelay = aux.ReadDelay
+
+	// unmarshal the message into the correct type
+	switch mr.Header.Opcode {
+	case wiremessage.OpMsg:
+		var msg MongoOpMessage
+		if err := bson.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	case wiremessage.OpQuery:
+		var msg MongoOpQuery
+		if err := bson.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	default:
+		return errors.New("failed to unmarshal unknown opcode")
+	}
+	return nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler for mongoRequests because of interface typeof field
+func (mr *MongoRequest) UnmarshalJSON(data []byte) error {
+	// duplicate struct to avoid infinite recursion
+	type MongoRequestAlias struct {
+		Header    *MongoHeader    `json:"header"`
+		Message   json.RawMessage `json:"message"`
+		ReadDelay int64           `json:"read_delay"`
+	}
+	var aux MongoRequestAlias
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// assign the unmarshalled data to the original data
+	mr.Header = aux.Header
+	mr.ReadDelay = aux.ReadDelay
+
+	// unmarshal the message into the correct type
+	switch mr.Header.Opcode {
+	case wiremessage.OpMsg:
+		var msg MongoOpMessage
+		if err := json.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	case wiremessage.OpQuery:
+		var msg MongoOpQuery
+		if err := json.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	default:
+		return errors.New("failed to unmarshal unknown opcode")
+	}
+
+	return nil
+}
+
+// MarshalJSON implements json.Marshaler for mongoRequests because of interface typeof field
+func (mr *MongoRequest) MarshalJSON() ([]byte, error) {
+	// duplicate struct to avoid infinite recursion
+	type MongoRequestAlias struct {
+		Header    *MongoHeader    `json:"header"`
+		Message   json.RawMessage `json:"message"`
+		ReadDelay int64           `json:"read_delay"`
+	}
+
+	aux := MongoRequestAlias{
+		Header:    mr.Header,
+		Message:   json.RawMessage(nil),
+		ReadDelay: mr.ReadDelay,
+	}
+
+	if mr.Message != nil {
+		// Marshal the message interface{} into JSON
+		msgJSON, err := json.Marshal(mr.Message)
+		if err != nil {
+			return nil, err
+		}
+		aux.Message = msgJSON
+	}
+
+	return json.Marshal(aux)
+}
+
+// UnmarshalBSON implements bson.Unmarshaler for mongoResponses because of interface typeof field
+func (mr *MongoResponse) UnmarshalBSON(data []byte) error {
+	// duplicate struct to avoid infinite recursion
+	type MongoResponseAlias struct {
+		Header    *MongoHeader `bson:"header,omitempty"`
+		Message   bson.Raw     `bson:"message,omitempty"`
+		ReadDelay int64        `bson:"read_delay,omitempty"`
+	}
+	var aux MongoResponseAlias
+
+	if err := bson.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// assign the unmarshalled data to the original data
+	mr.Header = aux.Header
+	mr.ReadDelay = aux.ReadDelay
+
+	// unmarshal the message into the correct type
+	switch mr.Header.Opcode {
+	case wiremessage.OpMsg:
+		var msg MongoOpMessage
+		if err := bson.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	case wiremessage.OpReply:
+		var msg MongoOpReply
+		if err := bson.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	default:
+		return errors.New("failed to unmarshal unknown opcode")
+	}
+
+	return nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler for mongoResponses because of interface typeof field
+func (mr *MongoResponse) UnmarshalJSON(data []byte) error {
+	// duplicate struct to avoid infinite recursion
+	type MongoResponseAlias struct {
+		Header    *MongoHeader    `json:"header"`
+		Message   json.RawMessage `json:"message"`
+		ReadDelay int64           `json:"read_delay"`
+	}
+	var aux MongoResponseAlias
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// assign the unmarshalled data to the original data
+	mr.Header = aux.Header
+	mr.ReadDelay = aux.ReadDelay
+
+	// unmarshal the message into the correct type
+	switch mr.Header.Opcode {
+	case wiremessage.OpMsg:
+		var msg MongoOpMessage
+		if err := json.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	case wiremessage.OpReply:
+		var msg MongoOpReply
+		if err := json.Unmarshal(aux.Message, &msg); err != nil {
+			return err
+		}
+		mr.Message = &msg
+	default:
+		return errors.New("failed to unmarshal unknown opcode")
+	}
+
+	return nil
+
+}
+
+// MarshalJSON implements json.Marshaler for mongoResponses because of interface typeof field
+func (mr *MongoResponse) MarshalJSON() ([]byte, error) {
+	// duplicate struct to avoid infinite recursion
+	type MongoResponseAlias struct {
+		Header    *MongoHeader    `json:"header"`
+		Message   json.RawMessage `json:"message"`
+		ReadDelay int64           `json:"read_delay"`
+	}
+
+	aux := MongoResponseAlias{
+		Header:    mr.Header,
+		Message:   json.RawMessage(nil),
+		ReadDelay: mr.ReadDelay,
+	}
+
+	if mr.Message != nil {
+		// Marshal the message interface{} into JSON
+		msgJSON, err := json.Marshal(mr.Message)
+		if err != nil {
+			return nil, err
+		}
+		aux.Message = msgJSON
+	}
+
+	return json.Marshal(aux)
 }
