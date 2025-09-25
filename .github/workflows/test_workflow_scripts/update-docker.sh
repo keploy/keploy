c210cebeefeb77184a00b835fe211608fed33520
gouravkrosx <gouravgreatkr@gmail.com>
2025-09-25 11:31:26 +0530

    fix: pipeline
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- .github/workflows/test_workflow_scripts/update-docker.sh
+++ .github/workflows/test_workflow_scripts/update-docker.sh
@@ -16,6 +16,9 @@ update_dockerfile() {
     
     # Ensure the go mod download command uses the SSH mount.
     sed -i 's/RUN go mod download/RUN --mount=type=ssh go mod download/' "$DOCKERFILE_PATH"
+    
+    # Add go mod tidy after go mod download
+    sed -i '/RUN --mount=type=ssh go mod download/a RUN go mod tidy' "$DOCKERFILE_PATH"
 }
 
 # Function to build the Docker image
