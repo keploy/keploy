e21c27fb00fdc566771c8ce59a8077fa50d30920
Kapish Upadhyay <upadhyaykapish@gmail.com>
2025-10-13 13:32:26 +0530

    chore: removed unsed models
    Signed-off-by: Kapish Upadhyay <upadhyaykapish@gmail.com>

--- pkg/agent/hooks/structs/structs.go
+++ pkg/agent/hooks/structs/structs.go
@@ -3,15 +3,6 @@ package structs
 
 type BpfSpinLock struct{ Val uint32 }
 
-// struct dest_info_t
-// {
-//     u32 ip_version;
-//     u32 dest_ip4;
-//     u32 dest_ip6[4];
-//     u32 dest_port;
-//     u32 kernelPid;
-// };
-
 type DestInfo struct {
 	IPVersion uint32
 	DestIP4   uint32
@@ -20,40 +11,16 @@ type DestInfo struct {
 	KernelPid uint32
 }
 
-// struct proxy_info
-// {
-//     u32 ip4;
-//     u32 ip6[4];
-//     u32 port;
-// };
-
 type ProxyInfo struct {
 	IP4  uint32
 	IP6  [4]uint32
 	Port uint32
 }
 
-// struct app_info
-// {
-//     u32 keploy_client_ns_pid;
-//     u64 keploy_client_inode;
-//     u64 app_inode;
-//     u32 mode;
-//     u32 is_docker_app;
-//     u32 is_keploy_client_registered; // whether the client is registered or not
-//     s32 pass_through_ports[PASS_THROUGH_ARRAY_SIZE];
-// };
-
 type ClientInfo struct {
-	// KeployClientNsPid uint32 // 4 bytes
-	Mode uint32 // 4 bytes
-	// IsDockerApp       uint32 // 4 bytes
-	ClientNSPID uint32
-
-	// IsKeployClientRegistered uint32    // 4 bytes
+	Mode             uint32 // 4 bytes
+	ClientNSPID      uint32
 	PassThroughPorts [10]int32 // 40 bytes
-
-	// AppInode                 uint64
 }
 
 type AgentInfo struct {
