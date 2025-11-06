871b245abd182fa456c14680551798f86ab35d68
Kapish Upadhyay <upadhyaykapish@gmail.com>
2025-11-06 22:30:24 +0530

    feat: V4
    Signed-off-by: Kapish Upadhyay <upadhyaykapish@gmail.com>

--- pkg/platform/docker/util.go
+++ pkg/platform/docker/util.go
@@ -120,7 +120,7 @@ func getAlias(ctx context.Context, logger *zap.Logger, opts models.SetupOptions)
 			fmt.Sprintf("%d", opts.AgentPort) + ":" + fmt.Sprintf("%d", opts.AgentPort) +
 			" -p " + fmt.Sprintf("%d", opts.ProxyPort) + ":" + fmt.Sprintf("%d", opts.ProxyPort) + appPortsStr +
 			" --privileged " + Volumes + "-v " + os.Getenv("PWD") + ":" + os.Getenv("PWD") + " -w " + os.Getenv("PWD") +
-			" -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock " +
+			" -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf " +
 			" --rm " + img + " --client-pid " + fmt.Sprintf("%d", opts.ClientNSPID) + " --mode " + string(opts.Mode)
 
 		if opts.EnableTesting {
