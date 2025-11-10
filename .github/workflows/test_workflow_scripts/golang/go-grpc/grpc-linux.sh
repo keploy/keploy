a4310b6a400b913d1dfad35cb91a6034695e5e7c
Akash Kumar <meakash7902@gmail.com>
2025-11-10 09:53:25 +0000

    fix: use debug flag
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- .github/workflows/test_workflow_scripts/golang/go-grpc/grpc-linux.sh
+++ .github/workflows/test_workflow_scripts/golang/go-grpc/grpc-linux.sh
@@ -193,7 +193,7 @@ if [ "$MODE" = "incoming" ]; then
     
     start_section "record_incoming" "🔴 Recording incoming gRPC calls..."
     ./grpc-client &> client_incoming.log &
-    sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "./grpc-server" $BIG_PAYLOAD_FLAG --generateGithubActions=false 2>&1 | tee record_incoming.log &
+    sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "./grpc-server" $BIG_PAYLOAD_FLAG --generateGithubActions=false --debug 2>&1 | tee record_incoming.log &
     wait_for_port 50051
     sleep 5
     send_requests
@@ -220,7 +220,7 @@ elif [ "$MODE" = "outgoing" ]; then
     start_section "record_outgoing" "🔴 Recording outgoing gRPC calls..."
     ./grpc-server &> server_outgoing.log &
     wait_for_port 50051
-    sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "./grpc-client" $BIG_PAYLOAD_FLAG --generateGithubActions=false 2>&1 | tee record_outgoing.log &
+    sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "./grpc-client" $BIG_PAYLOAD_FLAG --generateGithubActions=false --debug 2>&1 | tee record_outgoing.log &
     send_requests
     sleep 15 # Allow time for traces to be recorded
     kill_keploy_process
