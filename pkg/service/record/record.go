269b052fc45c07f99f06e63057a201f29904e64e
kapish <upadhyaykapish@gmail.com>
2025-11-18 18:56:08 +0000

    fix: gofmt fix
    Signed-off-by: kapish <upadhyaykapish@gmail.com>

--- pkg/service/record/record.go
+++ pkg/service/record/record.go
@@ -155,7 +155,7 @@ func (r *Recorder) Start(ctx context.Context, reRecordCfg models.ReRecordCfg) er
 	}
 
 	// Instrument will setup the environment and start the hooks and proxy
-	err := r.instrumentation.Setup(setupCtx, r.config.Command, models.SetupOptions{Container: r.config.ContainerName, DockerDelay: r.config.BuildDelay, Mode: models.MODE_RECORD, CommandType: r.config.CommandType, EnableTesting: false, GlobalPassthrough: r.config.Record.GlobalPassthrough, BuildDelay: r.config.BuildDelay, PassThroughPorts: passPortsUint,  ConfigPath: r.config.ConfigPath})
+	err := r.instrumentation.Setup(setupCtx, r.config.Command, models.SetupOptions{Container: r.config.ContainerName, DockerDelay: r.config.BuildDelay, Mode: models.MODE_RECORD, CommandType: r.config.CommandType, EnableTesting: false, GlobalPassthrough: r.config.Record.GlobalPassthrough, BuildDelay: r.config.BuildDelay, PassThroughPorts: passPortsUint, ConfigPath: r.config.ConfigPath})
 
 	if err != nil {
 		stopReason = "failed setting up the environment"
