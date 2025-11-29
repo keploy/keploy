103d5421551678bfc05f3f1f3c7f17b00795e586
kapish <upadhyaykapish@gmail.com>
2025-11-29 07:04:17 +0000

    chore: corrected keploy stopping logic
    Signed-off-by: kapish <upadhyaykapish@gmail.com>

--- .github/workflows/test_workflow_scripts/fuzzer/grpc/golang-linux.sh
+++ .github/workflows/test_workflow_scripts/fuzzer/grpc/golang-linux.sh
@@ -123,7 +123,7 @@ if [ "$MODE" = "incoming" ]; then
 
   # Start server with keploy in record mode
   if [[ "$RECORD_SRC" == "latest" ]]; then
-  sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "$FUZZER_SERVER_BIN" --bigPayload 2>&1 | tee record_incoming.txt &
+  sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "$FUZZER_SERVER_BIN" 2>&1 | tee record_incoming.txt &
   else
   sudo -E env PATH="$PATH" "$RECORD_BIN" record -c "$FUZZER_SERVER_BIN" 2>&1 | tee record_incoming.txt &
   fi
@@ -152,10 +152,12 @@ if [ "$MODE" = "incoming" ]; then
 
  echo "Stopping keploy record and server"
 
-REC_PID="$(pgrep -n -f 'keploy record' || true)"
-echo "$REC_PID Keploy PID"
-echo "Killing keploy"
-sudo kill -INT "$REC_PID" 2>/dev/null || true
+
+
+  REC_PID=$(pgrep keploy | sort -n | head -1)
+  echo "$REC_PID Keploy PID"
+  echo "Killing keploy"
+  sudo kill -INT "$REC_PID" 2>/dev/null || true
 
  sleep 5
 
@@ -243,11 +245,10 @@ elif [ "$MODE" = "outgoing" ]; then
  sleep 10
 
 
-REC_PID="$(pgrep -n -f 'keploy record' || true)"
-echo "$REC_PID Keploy PID"
-echo "Killing keploy"
-sudo kill -INT "$REC_PID" 2>/dev/null || true
-
+ REC_PID=$(pgrep keploy | sort -n | head -1)
+ echo "$REC_PID Keploy PID"
+ echo "Killing keploy"
+ sudo kill -INT "$REC_PID" 2>/dev/null || true
  sleep 5
 
  echo "Ensuring fuzzer client is stopped..."
