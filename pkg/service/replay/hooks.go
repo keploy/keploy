a1fed96b3d217e346968fbc21a908b3fb7dde917
kapish <upadhyaykapish@gmail.com>
2025-12-01 11:49:31 +0000

    chore: updated beforesimulaterequest fields
    Signed-off-by: kapish <upadhyaykapish@gmail.com>

--- pkg/service/replay/hooks.go
+++ pkg/service/replay/hooks.go
@@ -233,7 +233,7 @@ func (h *Hooks) BeforeTestSetRun(ctx context.Context, testSetID string) error {
 
 func (h *Hooks) AfterTestRun(_ context.Context, testRunID string, testSetIDs []string, coverage models.TestCoverage) error {
 	h.logger.Debug("AfterTestRun hook executed", zap.String("testRunID", testRunID), zap.Any("testSetIDs", testSetIDs), zap.Any("coverage", coverage))
-	
+
 	if err := h.instrumentation.AfterTestRun(testRunID, testSetIDs, coverage); err != nil {
 		h.logger.Error("failed to call before test run hook", zap.Error(err))
 	}
