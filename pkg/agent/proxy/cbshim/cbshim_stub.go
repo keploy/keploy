72df34b76eec34b097b8d98cb2464acda32a92c1
Ayush Sharma <ayush.sharma@keploy.io>
2026-06-02 14:19:46 +0100

    style(cbshim): gofmt cbshim_stub.go
    Re-align Counters fields and one-line stub method signatures the way
    gofmt wants them. CI's `Check gofmt` step caught this.
    
    Signed-off-by: Ayush Sharma <ayush.sharma@keploy.io>

--- pkg/agent/proxy/cbshim/cbshim_stub.go
+++ pkg/agent/proxy/cbshim/cbshim_stub.go
@@ -24,28 +24,28 @@ type LibpqRange struct {
 }
 
 type Counters struct {
-	TotalFires   uint64
-	TGIDMatched  uint64
-	LibpqFires   uint64
-	LookupHit    uint64
-	LookupMiss   uint64
-	WriteOK      uint64
-	WriteFail    uint64
+	TotalFires  uint64
+	TGIDMatched uint64
+	LibpqFires  uint64
+	LookupHit   uint64
+	LookupMiss  uint64
+	WriteOK     uint64
+	WriteFail   uint64
 }
 
-func New(_ *zap.Logger) (*CBShim, error)                                            { return &CBShim{}, nil }
-func (c *CBShim) Close() error                                                       { return nil }
-func (c *CBShim) RegisterPID(_ uint32) error                                         { return nil }
-func (c *CBShim) UnregisterPID(_ uint32)                                             {}
-func (c *CBShim) IsPIDRegistered(_ uint32) bool                                      { return false }
-func (c *CBShim) RegisterLibpqRanges(_ uint32, _ []LibpqRange) error                 { return nil }
-func (c *CBShim) AttachToLibcrypto(_ string) error                                   { return nil }
-func (c *CBShim) AttachToProcess(_ int) error                                        { return nil }
-func (c *CBShim) AttachToProcessTree(_ int) error                                    { return nil }
-func (c *CBShim) WatchProcessTree(_ context.Context, _ int)                          {}
-func (c *CBShim) DetachFromProcessTree(_ int)                                        {}
-func (c *CBShim) RegisterMITM(_ string, _ []byte)                                    {}
-func (c *CBShim) RegisterReal(_ string, _ []byte, _ x509.SignatureAlgorithm)         {}
-func (c *CBShim) CleanupConnection(_ string)                                         {}
-func (c *CBShim) Publish(_, _ []byte, _ x509.SignatureAlgorithm) error               { return nil }
-func (c *CBShim) Counters() Counters                                                 { return Counters{} }
+func New(_ *zap.Logger) (*CBShim, error)                                     { return &CBShim{}, nil }
+func (c *CBShim) Close() error                                               { return nil }
+func (c *CBShim) RegisterPID(_ uint32) error                                 { return nil }
+func (c *CBShim) UnregisterPID(_ uint32)                                     {}
+func (c *CBShim) IsPIDRegistered(_ uint32) bool                              { return false }
+func (c *CBShim) RegisterLibpqRanges(_ uint32, _ []LibpqRange) error         { return nil }
+func (c *CBShim) AttachToLibcrypto(_ string) error                           { return nil }
+func (c *CBShim) AttachToProcess(_ int) error                                { return nil }
+func (c *CBShim) AttachToProcessTree(_ int) error                            { return nil }
+func (c *CBShim) WatchProcessTree(_ context.Context, _ int)                  {}
+func (c *CBShim) DetachFromProcessTree(_ int)                                {}
+func (c *CBShim) RegisterMITM(_ string, _ []byte)                            {}
+func (c *CBShim) RegisterReal(_ string, _ []byte, _ x509.SignatureAlgorithm) {}
+func (c *CBShim) CleanupConnection(_ string)                                 {}
+func (c *CBShim) Publish(_, _ []byte, _ x509.SignatureAlgorithm) error       { return nil }
+func (c *CBShim) Counters() Counters                                         { return Counters{} }
